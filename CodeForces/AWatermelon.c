051014e704062f2c8973d6a2cbe988b4813f46e3
mdsiaofficial <mdsiaofficial@gmail.com>
2022-06-30 08:27:23 +0600

    afds

--- CodeForces/AWatermelon.c
+++ CodeForces/AWatermelon.c
@@ -3,16 +3,29 @@
 int main()
 
 {
-    int weight;
-    scanf("%d", &weight);
-
-    if(weight%2==0){
-        printf("YES\n");
-    }
-    else{
+    int n;
+    scanf("%d", &n);
+    if(n <= 2 || n%2 != 0){
         printf("NO\n");
     }
+    else{
+        printf("YES\n");
+    }
+
+
+/* 
+    int weight;
+    scanf("%d", &weight);
+    if(weight>=1 && weight<=100){
     
+        if(weight%2==0){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    } 
+    */
     
     return 0;
 }
