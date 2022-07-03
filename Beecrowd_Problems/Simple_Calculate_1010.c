a378b18292db27cdcc10f2e6b0dbc0e053f326da
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-03 23:41:08 +0600

    sadfasdf

--- Beecrowd_Problems/Simple_Calculate_1010.c
+++ Beecrowd_Problems/Simple_Calculate_1010.c
@@ -5,8 +5,8 @@ int main() {
     int product1, unit1, product2, unit2;
     float price1, price2;
     
-    scanf("%d%d%f", &product1, &unit1, price1);
-    scanf("%d%d%f", &product2, &unit2, price2);
+    scanf("%d%d%f %d%d%f", &product1, &unit1, &price1, &product2, &unit2, &price2);
+    //scanf("%d%d%f", &product2, &unit2, price2);
     
     float totalprice = (unit1*price1) + (unit2*price2);
 
