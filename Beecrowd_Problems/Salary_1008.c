187c83989f9c6a6881908ca1263b1ed78ee80118
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-03 22:17:06 +0600

    adsf

--- Beecrowd_Problems/Salary_1008.c
+++ Beecrowd_Problems/Salary_1008.c
@@ -2,6 +2,15 @@
 
 int main() {
     
-    
+    int EmNum, hour;
+    float perhour;
+
+    scanf("%d %d %f", &EmNum, &hour, &perhour);
+
+    float salary = hour*perhour;
+
+    printf("NUMBER = %d\n", EmNum);
+    printf("SALARY = U$ %f\n", salary);
+
 	return 0;
 }
