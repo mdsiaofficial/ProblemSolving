1fdc7c54cc79532b0da3fa8135673447019d701e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-03 23:54:34 +0600

    FDAS

--- Beecrowd_Problems/Sphere_1011.c
+++ Beecrowd_Problems/Sphere_1011.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
- 
+#include <math.h>
+
 int main() {
+
+    int r;
+    scanf("%d", &r);
+
+    double volume = (4.0/3.0)*pi*pow(r,3);
     
+    printf("VOLUME = %.3f\n", volume);
 
     return 0;
 }
