89314407919ffdce3c928d0ecbae459c031bfae5
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-03 22:28:42 +0600

    df

--- Beecrowd_Problems/Salary_1008.c
+++ Beecrowd_Problems/Salary_1008.c
@@ -8,9 +8,9 @@ int main() {
     scanf("%d %d %f", &EmNum, &hour, &perhour);
     //hour = (float)hour;
 
-    
-    float salary = hour*perhour;
-    printf("%d", hour);
+    float salary = (float)hour*perhour;
+    //float salary = hour*perhour;
+    //printf("%d", hour);
 
     printf("NUMBER = %d\n", EmNum);
     printf("SALARY = U$ %.2f\n", salary);
