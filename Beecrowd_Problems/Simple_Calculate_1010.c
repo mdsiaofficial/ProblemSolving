27d96b8e7f56ffcfb1a6d8794ff4cca02e00a37a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-03 23:37:51 +0600

    fsda

--- Beecrowd_Problems/Simple_Calculate_1010.c
+++ Beecrowd_Problems/Simple_Calculate_1010.c
@@ -5,7 +5,12 @@ int main() {
     int product1, unit1, product2, unit2;
     float price1, price2;
     
-    scanf
+    scanf("%d%d%f", &product1, &unit1, price1);
+    scanf("%d%d%f", &product2, &unit2, price2);
+    
+    float totalprice = (unit1*price1) + (unit2*price2);
+
+    printf("VALOR A PAGAR: R$ %.2f\n", totalprice);
 
  
     return 0;
