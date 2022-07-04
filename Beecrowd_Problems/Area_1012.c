cc3448dfd8acbf70ace23b28f2c1ead60c449049
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-04 08:46:21 +0600

    afds

--- Beecrowd_Problems/Area_1012.c
+++ Beecrowd_Problems/Area_1012.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-
+    
+    const double pi = 3.14159; //Contant value of pi
     double A, B, C;
+
     scanf("%lf%lf%lf", &A, &B, &C);
 
-    double Area_Triangle = 
+    double Area_Triangle = (1/2)*A*C;
+    // area of triangle
+    double Area_Circle = pi*pow(C,2);
+
+    double Area_Trapezium = (1/2)*(A+B)*C;
+
+    double Area_Square = pow(B,2);
+
+    double Area_Rectangle = A*B;
+
+
+    printf("%lf\n", Area_Triangle);
+    printf("%lf\n", Area_Circle);
+    printf("%lf\n", Area_Triangle);
+    printf("%lf\n", Area_Triangle);
+    printf("%lf\n", Area_Triangle);
 
 	return 0;
 }
