fd0a849b2b60540b0bdc7cdad6c420e17bff2f4c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-04 23:40:32 +0600

    fd

--- Beecrowd_Problems/The_Greatest_1013.c
+++ Beecrowd_Problems/The_Greatest_1013.c
@@ -3,5 +3,13 @@
 
 int main() {
     
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+    int d = (a+b+ abs (a-b))/2;
+    int e = (d+c+ abs (d-c))/2;
+
+    printf("%d eh o maior\n", e);
+
 	return 0;
 }
