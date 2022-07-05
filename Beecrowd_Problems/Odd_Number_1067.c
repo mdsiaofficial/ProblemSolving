1b0c0a4f996a31fd4edc3b201dd830054fc887fa
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-05 09:41:32 +0600

    dfsa

---
+++ Beecrowd_Problems/Odd_Number_1067.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int X;
+    // input value
+    for(int i=1; i!=X; i++){
+        if (i%2!= 0){
+            printf("%d", i);
+        }
+    }
+
+	return 0;
+}
