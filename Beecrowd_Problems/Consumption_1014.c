1a4923e1c1ed3b90ddbc893353677738a0ef8e7a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-05 08:15:42 +0600

    adf

--- Beecrowd_Problems/Consumption_1014.c
+++ Beecrowd_Problems/Consumption_1014.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    
+    int X;
+    float Y;
+    scanf("%d %.1f", &X, &Y);
+
+    float Avrg_Consumption = (X/Y);
+
+    printf("%.3f km/l\n", Avrg_Consumption);
 
 	return 0;
 }
