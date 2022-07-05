0c48b0641b7cbd5c0a7d7051c421dd80aae7dce4
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-05 23:24:40 +0600

    dfs

--- Beecrowd_Problems/Distance_Between_Two_Points.c
+++ Beecrowd_Problems/Distance_Between_Two_Points.c
@@ -5,15 +5,15 @@ int main() {
     double x1,y1,x2,y2;
 
     scanf("%lf%lf %lf%lf", &x1, &y1, &x2, &y2);
-    // double dx = (x2-x1);
-    // double dy = (y2-y1);
+    double dx = (x2-x1);
+    double dy = (y2-y1);
 
     //double dx2 = pow(dx,2);
     // dy2 = pow(dy,2);
 
     //double distance=sqrt(dx2+dy2);
-    //double distance=sqrt( pow(dx,2) + pow(dy,2) );
-    double distance=sqrt( pow( (x2-x1), 2) + pow( (y2-y1),2) );
+    double distance=sqrt( pow(dx,2) + pow(dy,2) );
+    //double distance=sqrt( pow( (x2-x1), 2) + pow( (y2-y1),2) );
     printf("%.4lf\n", distance);
 
 
