36832f7531db45e07eaada57a88a718e90b001bf
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-05 23:20:04 +0600

    dfg

--- Beecrowd_Problems/Distance_Between_Two_Points.c
+++ Beecrowd_Problems/Distance_Between_Two_Points.c
@@ -8,8 +8,8 @@ int main() {
     double dx = (x2-x1);
     double dy = (y2-y1);
 
-    double dx2 = pow(dx,2)
-    double dy2 = pow(dy,2)
+    double dx2 = pow(dx,2);
+    double dy2 = pow(dy,2);
 
     double distance=sqrt(dx2+dy2);
 
