ce94eb06efe1beca0e3b4747e05f1e7cc1010792
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-06 08:05:19 +0600

    dsafds

---
+++ Beecrowd_Problems/Distance_1016.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+    
+    int distance;
+    // Distance between two car
+    int perminute = 30/60;
+    int time = 
+    scanf("%d", &distance);
+
+    time=distance*perminute;
+
+    printf("%d minutos", time);
+
+    return 0;
+}
