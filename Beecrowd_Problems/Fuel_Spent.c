924ef83ee0b2167dc38f4ed999aa39b266afb0f8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-07 11:31:14 +0600

    dsfa

--- Beecrowd_Problems/Fuel_Spent.c
+++ Beecrowd_Problems/Fuel_Spent.c
@@ -8,6 +8,12 @@ int main()
     int time, avrg_speed;
 
     scanf("%d %d", &time, & avrg_speed);
-    
+
+    float distance= time*avrg_speed;
+
+    float fuelperkm = (1/12);
+
+    float fuelspent = distance*fuelperkm;
+    printf("%.3f", fuelspent);
     return 0;
 }
