080f6354320f4faf2774465e326e07c652e230cb
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-07 12:19:18 +0600

    dasfs

--- Beecrowd_Problems/Fuel_Spent_1017.cpp
+++ Beecrowd_Problems/Fuel_Spent_1017.cpp
@@ -9,14 +9,12 @@ int main(){
     int time, avrg_speed;
 
     cin>>time>>avrg_speed;
-    //scanf("%d %d", &time, & avrg_speed);
 
     float distance= (float)time*(float)avrg_speed;
-
     float fuelperkm = 0.0833333333;
-
     float fuelspent = distance*fuelperkm;
-    //printf("%.3f\n", fuelspent);
+
     cout<<fixed<<setprecision(3) <<fuelspent<< endl;
+
     return 0;
 }
