616ff9b8615e4dd582fb532fe66222bfe4c31420
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-07 11:57:22 +0600

    adsfsd

--- Beecrowd_Problems/Fuel_Spent_1017.cpp
+++ Beecrowd_Problems/Fuel_Spent_1017.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 //#include<bits/stdc++.h>
-#include <iomanip>
+#include <iomanip> //this library need to use setprecision
+
 using namespace std;
 
 int main(){
@@ -8,14 +9,14 @@ int main(){
     int time, avrg_speed;
 
     cin>>time>>avrg_speed;
-    scanf("%d %d", &time, & avrg_speed);
+    //scanf("%d %d", &time, & avrg_speed);
 
     float distance= (float)time*(float)avrg_speed;
 
     float fuelperkm = 0.0833333333;
 
     float fuelspent = distance*fuelperkm;
-    printf("%.3f\n", fuelspent);
-    cout<< setprecision(3) <<fuelspent<< endl;
+    //printf("%.3f\n", fuelspent);
+    cout<<fixed<<setprecision(3) <<fuelspent<< endl;
     return 0;
 }
