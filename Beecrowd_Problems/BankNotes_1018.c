42873c25e1605f8a42a218a264833a64164c39d1
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-08 23:31:33 +0600

    gfd

--- Beecrowd_Problems/BankNotes_1018.c
+++ Beecrowd_Problems/BankNotes_1018.c
@@ -8,32 +8,32 @@ int main(void) {
  
     int b=a/100;
     int c=a%100;
-    printf("%d nota(s) de R$ 100,00\n",b,c);
+    printf("%d nota(s) de R$ 100,00\n",b);
  
     int d = c/50;
     int e = c%50;
-    printf("%d nota(s) de R$ 50,00\n",d,e);
+    printf("%d nota(s) de R$ 50,00\n",d);
  
     int f = e/20;
     int g = e%20;
-    printf("%d nota(s) de R$ 20,00\n",f,g);
+    printf("%d nota(s) de R$ 20,00\n",f);
  
     int h = g/10;
     int i = g%10;
-    printf("%d nota(s) de R$ 10,00\n",h,i);
+    printf("%d nota(s) de R$ 10,00\n",h);
  
     int j = i/5;
     int k = i%5;
-    printf("%d nota(s) de R$ 5,00\n",j,k);
+    printf("%d nota(s) de R$ 5,00\n",j);
  
  
     int l = k/2;
     int m = k%2;
-    printf("%d nota(s) de R$ 2,00\n",l,m);
+    printf("%d nota(s) de R$ 2,00\n",l);
  
     int n = m/1;
     int o = m%1;
-    printf("%d nota(s) de R$ 1,00\n",n,o);
+    printf("%d nota(s) de R$ 1,00\n",n);
  
  
 	return 0;
