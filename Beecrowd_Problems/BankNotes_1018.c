32ebb37e4dd3b2ec4cef3091b34c597e0bdbb4e7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-08 23:29:19 +0600

    dfsg

--- Beecrowd_Problems/BankNotes_1018.c
+++ Beecrowd_Problems/BankNotes_1018.c
@@ -2,38 +2,38 @@
  
 int main(void) {
 
-    
+
 	int a;
     scanf("%d",&a);
  
     int b=a/100;
     int c=a%100;
-    printf("100 tk - %d Remaining %d\n",b,c);
+    printf("%d nota(s) de R$ 100,00\n",b,c);
  
     int d = c/50;
     int e = c%50;
-    printf("50 tk  - %d Remaining %d\n",d,e);
+    printf("%d nota(s) de R$ 50,00\n",d,e);
  
     int f = e/20;
     int g = e%20;
-    printf("20 tk note - %d  Remaining: %d\n",f,g);
+    printf("%d nota(s) de R$ 20,00\n",f,g);
  
     int h = g/10;
     int i = g%10;
-    printf(" 10 tk %d Remaining %d\n",h,i);
+    printf("%d nota(s) de R$ 10,00\n",h,i);
  
     int j = i/5;
     int k = i%5;
-    printf("5 tk  %d Remaining %d\n",j,k);
+    printf("%d nota(s) de R$ 5,00\n",j,k);
  
  
     int l = k/2;
     int m = k%2;
-    printf("2 tk - %d  Remaining %d\n",l,m);
+    printf("%d nota(s) de R$ 2,00\n",l,m);
  
     int n = m/1;
     int o = m%1;
-    printf("1 tk - %d Remaining  %d\n",n,o);
+    printf("%d nota(s) de R$ 1,00\n",n,o);
  
  
 	return 0;
