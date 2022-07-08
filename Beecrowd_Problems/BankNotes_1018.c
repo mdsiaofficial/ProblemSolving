27bfbf126a26eedc6593606fb9bde76992cd6036
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-08 23:36:50 +0600

    ffds

--- Beecrowd_Problems/BankNotes_1018.c
+++ Beecrowd_Problems/BankNotes_1018.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
  
 int main(void) {
+    int notes, aux;
+
+    scanf("%d", &notes);
+
+    printf("%d\n", notes);
+    printf("%d nota(s) de R$ 100,00\n", notes/100);
+    aux = (notes%100);
 
 
+    printf("%d nota(s) de R$ 50,00\n", aux/50);
+    aux = (aux%50);
+
+    printf("%d nota(s) de R$ 20,00\n", aux/20);                                                                                                                        
+    aux = (aux%20);
+
+    printf("%d nota(s) de R$ 10,00\n", aux/10);
+    aux = (aux%10);
+
+    printf("%d nota(s) de R$ 5,00\n", aux/5);
+    aux = (aux%5);
+
+    printf("%d nota(s) de R$ 2,00\n", aux/2);
+    aux = (aux%2);
+
+    printf("%d nota(s) de R$ 1,00\n", aux/1);
+
+/* 
 	int a;
     scanf("%d",&a);
  
@@ -35,6 +60,6 @@ int main(void) {
     int o = m%1;
     printf("%d nota(s) de R$ 1,00\n",n);
  
- 
+  */
 	return 0;
 }
