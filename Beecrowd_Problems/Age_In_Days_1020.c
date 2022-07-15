aab7914922ba2a8d62a6e06931b05e120b2a6068
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-15 22:20:37 +0600

    dg

--- Beecrowd_Problems/Age_In_Days_1020.c
+++ Beecrowd_Problems/Age_In_Days_1020.c
@@ -3,9 +3,14 @@
 int main(void) {
 
 
+    int n;
+    scanf("%d",&n);
+    int year = n/365;
+    int L = n%365;
+    int month = L/30;
+    int day = L%30;
 
-
-    printf("%d:%d:%d\n",);
+    printf("%d:%d:%d\n",year,month,day);
     
 
 	return 0;
