72c6f7c80bfad45f5a00571053dbe7b00154f9fa
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-16 22:55:47 +0600

    fg

--- Beecrowd_Problems/Banknotes_And_Coins.c
+++ Beecrowd_Problems/Banknotes_And_Coins.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
+#include <math.h>
  
 int main(void) {
     
-    int notes, aux;
+    double notes, aux;
 
-    scanf("%d", &notes);
+    scanf("%f", &notes);
 /* 
     printf("NOTAS:\n");
-    printf("%d nota(s) de R$ 100,00\n", notes/100);
-    aux = (notes%100);
+    printf("%lf nota(s) de R$ 100,00\n", notes/100);
+    //aux = (notes%100);
+    aux = remainder(notes,100);
 
-    printf("%d nota(s) de R$ 50,00\n", aux/50);
-    aux = (aux%50);
+    printf("%lf nota(s) de R$ 50,00\n", aux/50);
+    //aux = (aux%50);
+    aux = remainder(notes,50);
 
-    printf("%d nota(s) de R$ 20,00\n", aux/20);                                                                                                                        
-    aux = (aux%20);
+    printf("%lf nota(s) de R$ 20,00\n", aux/20);                                                                                                                        
+    //aux = (aux%20);
+    aux = remainder(notes,20);
 
-    printf("%d nota(s) de R$ 10,00\n", aux/10);
-    aux = (aux%10);
+    printf("%lf nota(s) de R$ 10,00\n", aux/10);
+    //aux = (aux%10);
+    aux = remainder(notes,10);
 
-    printf("%d nota(s) de R$ 5,00\n", aux/5);
-    aux = (aux%5);
+    printf("%lf nota(s) de R$ 5,00\n", aux/5);
+    //aux = (aux%5);
+    aux = remainder(notes,5);
 
-    printf("%d nota(s) de R$ 2,00\n", aux/2);
-    aux = (aux%2);
+    printf("%lf nota(s) de R$ 2,00\n", aux/2);
+    //aux = (aux%2);
+    aux = remainder(notes,2);
 
     printf("MOEDAS:\n");
-    printf("%d nota(s) de R$ 1,00\n", aux/1);
+    printf("%lf nota(s) de R$ 1,00\n", aux/1);
  */
 
-    aux=notes%100;
-    printf("%d",aux);
+    //aux=notes%100.0;
+    aux = remainder(notes,100);
+    printf("%lf\n",aux);
 
-    printf("1 moeda(s) de R$ 0.50");
+    aux=notes/50;
+
+
+    printf("%lf moeda(s) de R$ 0.50\n", aux);
 	return 0;
 }
