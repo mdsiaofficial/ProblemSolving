a6ee4288ebb3f68f73c880cf7b3d65f5ca42daf2
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-17 11:17:34 +0600

    dsaf

--- Beecrowd_Problems/Banknotes_And_Coins.c
+++ Beecrowd_Problems/Banknotes_And_Coins.c
@@ -3,46 +3,9 @@
  
 int main(void) {
     
-    double notes, aux;
+    float notes, aux;
+    scanf("%f", &notes);
 
-    scanf("%lf", &notes);
-/* 
-    printf("NOTAS:\n");
-    printf("%lf nota(s) de R$ 100,00\n", notes/100);
-    //aux = (notes%100);
-    aux = remainder(notes,100);
-
-    printf("%lf nota(s) de R$ 50,00\n", aux/50);
-    //aux = (aux%50);
-    aux = remainder(notes,50);
-
-    printf("%lf nota(s) de R$ 20,00\n", aux/20);                                                                                                                        
-    //aux = (aux%20);
-    aux = remainder(notes,20);
-
-    printf("%lf nota(s) de R$ 10,00\n", aux/10);
-    //aux = (aux%10);
-    aux = remainder(notes,10);
-
-    printf("%lf nota(s) de R$ 5,00\n", aux/5);
-    //aux = (aux%5);
-    aux = remainder(notes,5);
-
-    printf("%lf nota(s) de R$ 2,00\n", aux/2);
-    //aux = (aux%2);
-    aux = remainder(notes,2);
-
-    printf("MOEDAS:\n");
-    printf("%lf nota(s) de R$ 1,00\n", aux/1);
- */
-
-    //aux=notes%100.0;
-    aux = remainder(notes,100.0);
-    printf("%lf\n",aux);
-
-    aux=notes/50;
-
-
-    printf("%lf moeda(s) de R$ 0.50\n", aux);
+    printf("Note: %f\n", notes);
 	return 0;
 }
