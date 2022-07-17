ff355f97f23966abdd4cbbe0dda9884b874ad852
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-17 11:39:18 +0600

    dsf

---
+++ Beecrowd_Problems/Simple_Factorial_1153.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main(void) {
+    
+    int N;
+    scanf("%d",&N);
+    //int factorial
+    for(int i=1;i<N;i++){
+        int factorial = factorial*i;
+    }
+    printf("%d\n",factorial);
+
+	return 0;
+}
