37cabe7c6ef157f4f5da179211ab438fe7325d07
mdsiaofficial <mdsiaofficial@gmail.com>
2022-07-17 11:42:37 +0600

    fd

--- Beecrowd_Problems/Simple_Factorial_1153.c
+++ Beecrowd_Problems/Simple_Factorial_1153.c
@@ -4,12 +4,16 @@
 int main(void) {
     
     int N;
+
     scanf("%d",&N);
-    int fact;
-    for(int i=1; i<N; i++){
+
+    int fact=1;
+    
+    for(int i=1; i<=N; i++){
         fact = fact*i;
     }
     printf("%d\n",fact);
+
 
 	return 0;
 }
