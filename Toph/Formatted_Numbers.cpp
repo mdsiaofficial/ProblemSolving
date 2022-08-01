29431b8e614141db1979bacd0cb17abfc868160d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-01 08:50:13 +0600

    dsf

--- Toph/Formatted_Numbers.cpp
+++ Toph/Formatted_Numbers.cpp
@@ -2,21 +2,29 @@
 using namespace std;
 
 int main(){
+
+    
     int n;
     char h=',';
     cin>>n;
+    string s= 
     /* int n1=n%1000;
 
     int n12=n/1000;
 
     int n2=n12%1;
  */
-    
-    cout<<n2<<h<<n1;
+    cout<<ch<<endl;
+
+    //cout<<n2<<h<<n1;
 
    /*  for(int i=0;i<n1;i++){
         
     } */
+
+
+
+
 
     return 0;
 }
