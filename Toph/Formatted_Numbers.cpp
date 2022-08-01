0e978d481ee5c8b04593f99b863acc350c33a104
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-01 08:37:31 +0600

    asdf

---
+++ Toph/Formatted_Numbers.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    char h=',';
+    cin>>n;
+    /* int n1=n%1000;
+
+    int n12=n/1000;
+
+    int n2=n12%1;
+ */
+    
+    cout<<n2<<h<<n1;
+
+   /*  for(int i=0;i<n1;i++){
+        
+    } */
+
+    return 0;
+}
