552e778e74990f94c4fd12fa0c7df8d113d31313
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-06 21:14:18 +0600

    dgfdgf

--- CSESFI/Weird_Algorithm.c
+++ CSESFI/Weird_Algorithm.c
@@ -2,5 +2,26 @@
  
 int main(void) {
     
+    int n;
+    scanf("%d",&n);
+
+
+    if(n!=1){
+
+        if(n%==2){
+
+        n=n/2;
+        printf("%d\n",n);
+        
+        }else{
+
+        n=(n*3)+1;
+        printf("%d\n",n);
+
+        }
+    }
+    
+
+
 	return 0;
 }
