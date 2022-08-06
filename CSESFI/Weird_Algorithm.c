1ae615df301644613f02441ba9e3c3755800859f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-06 21:20:36 +0600

    bvn

--- CSESFI/Weird_Algorithm.c
+++ CSESFI/Weird_Algorithm.c
@@ -5,18 +5,18 @@ int main(void) {
     int n;
     scanf("%d",&n);
 
+    printf("%d ",n);
+    if(n!=1){
 
-    while(n!=1){
-
-        if(n%2=0){
+        if(n%2==0){
 
         n=n/2;
-        printf("%d \n",n);
+        printf("%d ",n);
 
         }else{
 
         n=(n*3)+1;
-        printf("%d \n",n);
+        printf("%d ",n);
 
         }
     }
