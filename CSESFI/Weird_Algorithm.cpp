2e64adc540e2de9b157d69caad608cf7112afc59
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-06 21:33:29 +0600

    dfd

--- CSESFI/Weird_Algorithm.cpp
+++ CSESFI/Weird_Algorithm.cpp
@@ -8,12 +8,17 @@ int main() {
     cout <<n<<" ";
 
     while(n>1){
+
         if(n%2==0){
+            
             n=n/2;
             cout <<n<<" ";
+
         }else{
+
             n=(n*3)+1;
             cout <<n<<" ";
+
         }
     }
 	return 0;
