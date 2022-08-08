0c7ca6a72cb7bc89c5e36548068485d8fb5a4fed
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-08 13:15:01 +0600

    f

--- CSESFI/Missing_Number.cpp
+++ CSESFI/Missing_Number.cpp
@@ -4,10 +4,19 @@ using namespace std;
 int main() {
 	
     long long int n;
+    long long int t[];
     cin >> n;
-    
+
+    for (long long int i = 0; i < n-1; i++) {
+        cin>>t[];
+    }
+
+    for(long long int j=0; j<n; j++){
+        cout << t[j] << " ";
+    }
+  /*   
     int sumofnthnumber= (n*(n+1))/2;
     cout<<sumofnthnumber;
-
+ */
 	return 0;
 }
