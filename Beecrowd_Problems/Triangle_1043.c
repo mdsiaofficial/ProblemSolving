3f2f98c29567249283a6a266acaaa4e0efc8ec40
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-09 20:39:01 +0600

    gf

--- Beecrowd_Problems/Triangle_1043.c
+++ Beecrowd_Problems/Triangle_1043.c
@@ -5,6 +5,11 @@ int main(void) {
 
     scanf("%f%f%f", &a,&b,&c);
 
-    
+    if(a+b>c || b+c>a || c+a>b){
+        printf("Perimetro = %.1f\n", a+b+c);
+    }else{
+        printf("Area = %.1f", ((a+b)/2)*c);
+        
+    }
 	return 0;
 }
