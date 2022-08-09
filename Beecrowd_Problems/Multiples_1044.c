5857f45af21e005fe158c11474627d8b11b4c5b7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-09 20:22:13 +0600

    dfssd

---
+++ Beecrowd_Problems/Multiples_1044.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void) {
+    int n,n1;
+    scanf("%d%d", &n,&n1);
+    if(n1%n==0){
+        printf("Sao Multiplos\n");
+    }else{
+        printf("Nao sao Multiplos\n");
+    }
+
+
+	return 0;
+}
