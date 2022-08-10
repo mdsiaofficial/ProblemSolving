01c9ec3a01f2950ecb4c8aa33bb6fc06396d594d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-10 13:20:52 +0600

    DS

--- Beecrowd_Problems/Bhaskaras_Formula_1036.c
+++ Beecrowd_Problems/Bhaskaras_Formula_1036.c
@@ -7,10 +7,12 @@ int main(void) {
     scanf("%lf%lf%lf", &a,&b,&c);
 
     double del = (b*b)-4*a*c;
+    
 
     if(del > 0) {
-        double r1= (sqrt(del)-b)/2*a;
-        double r2= (-sqrt(del)-b)/2*a;
+        del = sqrt(del);
+        double r1= (del-b)/(2*a);
+        double r2= (-del-b)/(2*a);
         printf("R1 = %.5lf\nR2 = %.5lf\n",r1,r2);
     }else{
 
