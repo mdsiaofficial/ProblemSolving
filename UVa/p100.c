579202c2827f6dfa21b85c2fa9abfc4eadbd76a7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-10 15:58:43 +0600

    dasf

--- UVa/p100.c
+++ UVa/p100.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+int func(int a,int b);
+
+
 int main() {
-    
+/*     
     int n;
     scanf("%d", &n);
     while(n!=1){
@@ -13,6 +16,35 @@ int main() {
         }
         
     }
+ */
+    int i,j,result;
+    while(scanf("%d %d",&i,&j)!=EOF){
+        result = func(i , j);
+        printf("%d %d %d\n",i,j,result);
+    }
 
+    
 	return 0;
+}
+
+int func(int a,int b){
+    int x,count = 1,max =0;
+    if(a > b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    while(a <= b){
+        x = a;
+        while(x != 1){
+            if(x % 2 == 0) x = x / 2;
+            else x = (3 * x) + 1;
+            count++;
+            if(x == 1)break;
+        }
+        if(count > max)max = count;
+        count = 1;
+        a++;
+    }
+    return max;
 }
