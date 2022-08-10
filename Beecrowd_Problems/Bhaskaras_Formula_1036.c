33f162502e23f5bf11940796f6f39118191e4ced
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-10 13:18:25 +0600

    ff

--- Beecrowd_Problems/Bhaskaras_Formula_1036.c
+++ Beecrowd_Problems/Bhaskaras_Formula_1036.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
- 
+#include <math.h>
 int main(void) {
     
     double a,b,c;
 
     scanf("%lf%lf%lf", &a,&b,&c);
-    
+
     double del = (b*b)-4*a*c;
 
     if(del > 0) {
-
-        printf("R1 = %.5lf\nR2 = %.5lf\n");
+        double r1= (sqrt(del)-b)/2*a;
+        double r2= (-sqrt(del)-b)/2*a;
+        printf("R1 = %.5lf\nR2 = %.5lf\n",r1,r2);
     }else{
 
         printf("Impossivel calcular\n");
