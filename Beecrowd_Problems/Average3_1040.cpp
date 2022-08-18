1bda19991983415b53ba9083609623a42737f3f2
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-18 11:54:46 +0600

    dddd

--- Beecrowd_Problems/Average3_1040.cpp
+++ Beecrowd_Problems/Average3_1040.cpp
@@ -7,35 +7,44 @@ int main(){
     float score;
 
     cin>>n1>>n2>>n3>>n4;
-    cin>>score;
+    //cin>>score;
 
     float average= (n1+n2+n3+n4)/4;
-    float average2 = (average+score)/2;
+    //float average2 = (average+score)/2;
     
     //cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
     //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
 
-    if(average>=7.0 && average2>=5.0){
+    if(average>=7.0){
         cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
         cout<<"Aluno aprovado."<<endl;
-        cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
-        cout<<"Aluno aprovado."<<endl;
-        cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
+        //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
+        //cout<<"Aluno aprovado."<<endl;
+        //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
 
-    }else if(average<5.0 && average2<=4.9){
+    }else if(average<5.0){
         cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
         cout<<"Aluno reprovado."<<endl;
-        cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
-        cout<<"Aluno reprovado."<<endl;
-        cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
+        //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
+        //<<"Aluno reprovado."<<endl;
+        //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
 
-    }else if(average>5.0 && average<6.9 && average2>=5.0){
+    }else if(average>5.0 && average<6.9){
         cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
         cout<<"Aluno em exame."<<endl;
+        //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
+        //cout<<"Aluno aprovado."<<endl;
+        //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
+        cin>>score;
+        float average2 = (average+score)/2;
         cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
-        cout<<"Aluno aprovado."<<endl;
+        if(average2>5.0){
+            cout<<"Aluno aprovado."<<endl;
+        }else{
+            cout<<"Aluno reprovado."<<endl;
+        }
+
         cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
-        
     }
 
     return 0;
