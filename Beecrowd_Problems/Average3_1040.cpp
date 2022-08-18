733d69dbc64ac710e9e394533098d5e803789523
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-18 12:21:28 +0600

    ddd

--- Beecrowd_Problems/Average3_1040.cpp
+++ Beecrowd_Problems/Average3_1040.cpp
@@ -12,28 +12,22 @@ int main(){
     float average= ((n1*2)+(n2*3)+(n3*4)+n4)/10.0;
     //float average2 = (average+score)/2;
     
-    //cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
+    cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
     //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
 
     if(average>=7.0){
-        cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
+        //cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
         cout<<"Aluno aprovado."<<endl;
         //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
         //cout<<"Aluno aprovado."<<endl;
         //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
 
-    }else if(average<5.0){
-        cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
-        cout<<"Aluno reprovado."<<endl;
-        //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
-        //<<"Aluno reprovado."<<endl;
-        //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
-
-    }else if(average>5.0 && average<6.9){
-        cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
+    }else if(average>=5.0 && average<=6.9){
+        //cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
+        //cout<<"Aluno reprovado."<<endl;
         cout<<"Aluno em exame."<<endl;
         //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
-        //cout<<"Aluno aprovado."<<endl;
+        //<<"Aluno reprovado."<<endl;
         //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
         cin>>score;
         float average2 = (average+score)/2.0;
@@ -45,6 +39,15 @@ int main(){
         }
 
         cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
+        
+    }else if(average<5.0){
+        //cout<<"Media: "<<fixed<<setprecision(1)<<average<<endl;
+        //cout<<"Aluno em exame."<<endl;
+        cout<<"Aluno reprovado."<<endl;
+        //cout<<"Nota do exame: "<<fixed<<setprecision(1)<<score<<endl;
+        //cout<<"Aluno aprovado."<<endl;
+        //cout<<"Media final: "<<fixed<<setprecision(1)<<average2<<endl;
+        
     }
 
     return 0;
