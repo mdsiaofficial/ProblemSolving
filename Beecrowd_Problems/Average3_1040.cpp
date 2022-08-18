f28ad8bb60bccccf86d46c8ad7090fe4e1046316
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-18 11:13:17 +0600

    dsfsdf

---
+++ Beecrowd_Problems/Average3_1040.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    float n1,n2,n3,n4;
+
+    cin>>n1>>n2>>n3>>n4;
+
+    float average= (n1+n2+n3+n4)/4;
+
+    cout<<average<<endl;
+
+
+
+    return 0;
+}
