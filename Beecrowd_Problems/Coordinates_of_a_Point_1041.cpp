e742ea4879a6346db057a745679ff605cef182b3
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-21 23:24:47 +0600

    dfdd

---
+++ Beecrowd_Problems/Coordinates_of_a_Point_1041.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+
+    float x,y;
+
+    cin>>x>>y;
+
+    cout<<x<<y;
+    return 0;
+}
