dc428bece36a603b4104b3127a66f4beedf9f372
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-23 22:13:04 +0600

    ddd

--- Beecrowd_Problems/Triangle_Types_1045.cpp
+++ Beecrowd_Problems/Triangle_Types_1045.cpp
@@ -2,17 +2,21 @@
 #include <iomanip> //this library need to use setprecision
 using namespace std;
 int main(){
-    float a,b,c;
+    double a,b,c;
     cin>>a>>b>>c;
-    float a2=a*a;
-    float b2=b*b;
-    float c2=c*c;
+    double a2=a*a;
+    double b2=b*b;
+    double c2=c*c;
 
     if(a>=b+c){
         cout<<"NAO FORMA TRIANGULO"<<endl;
     }else if(a2==b2+c2){
         cout<<"TRIANGULO RETANGULO"<<endl;
-    }else
+    }else if(a2>b2+c2){
+        cout<<"TRIANGULO OBTUSANGULO"<<endl;
+    }else if(a2<b2+c2){
+        cout<<"TRIANGULO OBTUSANGULO"<<endl;
+    }
 
 
     return 0;
