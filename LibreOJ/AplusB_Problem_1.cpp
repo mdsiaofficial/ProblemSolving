d5c74ae0ce8105e2e3da6c2ea4851a28d2c3ddc6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-23 07:41:43 +0600

    dd

---
+++ LibreOJ/AplusB_Problem_1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+    int n,n1;
+    cin>>n>>n1;
+    cout<<n+n1;
+
+    return 0;
+}
