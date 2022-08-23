5e2ddbc5daf92eb6bab49164380db0781e2d54c9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-23 07:15:41 +0600

    g

--- Beecrowd_Problems/Simple_Sort_1042.cpp
+++ Beecrowd_Problems/Simple_Sort_1042.cpp
@@ -5,6 +5,16 @@ int main(){
 
     int n1,n2,n3;
     cin>>n1>>n2>>n3;
+
+
+    if(n1<n2 && n1<n3){
+        cout<<n1<<endl<<endl;
+    }else if(n2<n1 && n2<n3){
+        cout<<n2<<endl<<endl;
+    }else{
+        cout<<n3<<endl<<endl;
+    }
+
     cout<<n1<<endl<<n2<<endl<<n3<<endl;
 
     return 0;
