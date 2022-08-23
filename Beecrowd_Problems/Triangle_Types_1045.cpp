8728b47802b32f0e075a16a0023cc45fe8f29066
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-23 22:51:19 +0600

    sss

--- Beecrowd_Problems/Triangle_Types_1045.cpp
+++ Beecrowd_Problems/Triangle_Types_1045.cpp
@@ -7,28 +7,38 @@ int main(){
     double a2=a*a;
     double b2=b*b;
     double c2=c*c;
-/* 
+ 
     int A,B,C;
     if(a>b && a>c){
         A=a;
         if(b>c){
             B=b;
+            C=c;
         }else{
             B=c;
+            C=b;
         }
-
-
 
     }else if(b>a && b>c){
         A=b;
-        B=a;
-        C=c;
+        if(a>c){
+            B=a;
+            C=c;
+        }else{
+            B=c;
+            C=a;
+        }
     }else if(c>a && c>b){
         A=c;
-        B=a;
-        C=b;
+        if(a>b){
+            B=a;
+            C=b;
+        }else{
+            B=b;
+            C=a;
+        }
     }
- */
+
 
     if(a>=b+c){
         cout<<"NAO FORMA TRIANGULO"<<endl;
