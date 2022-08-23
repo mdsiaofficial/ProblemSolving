c0025b0cca47719a54868fa8176712280979783c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-23 20:58:23 +0600

    ddd

---
+++ LibreOJ/Copycat_3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+    int t;
+    int n;
+    cin>>t;
+    for(n=1; n<=t; n++){
+        cin>>n;
+    }
+
+    return 0;
+}
