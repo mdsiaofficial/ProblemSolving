3fcedef6bea2ec4e3c707029995bad970dd68371
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-23 07:11:22 +0600

    ddd

---
+++ Beecrowd_Problems/Simple_Sort_1042.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    cout<<n1<<endl<<n2<<endl<<n3<<endl;
+
+    return 0;
+}
