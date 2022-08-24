29f4b68ae005096e2868407e57d5dc3d83a59c29
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-24 22:59:21 +0600

    one

---
+++ nickname.py
@@ -0,0 +1,16 @@
+import random
+
+#rodrigo tallar.
+
+firstL=["Super","evil","Pro","Sad","Golden","Best","Beautiful","Green","Blue","Fat","Old","Wise","Young","First","Big","fast","Small","Smart","tiny"]
+
+secondL=["Jumper","Killer","Noob","Man","Hero","devil","Thief","Kid","Dragon","skeleton","Archer","Runner","Jumper","Knight","Adventurer","Warrior","Mage","Druid","Ninja"]
+
+fs1=random.choice(firstL)
+fs2=random.choice(secondL)
+fs=fs1+"_"+fs2
+
+print("Your name is: ")
+print(fs)
+print("")
+print("")
