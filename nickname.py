5dd29a033eee878fb23ef2f5bcc665572f3ae237
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-24 23:01:23 +0600

    two

--- nickname.py
+++ nickname.py
@@ -1,16 +1,71 @@
-import random
 
-#rodrigo tallar.
 
-firstL=["Super","evil","Pro","Sad","Golden","Best","Beautiful","Green","Blue","Fat","Old","Wise","Young","First","Big","fast","Small","Smart","tiny"]
+ime = input("Enter your name: \n\n")
 
-secondL=["Jumper","Killer","Noob","Man","Hero","devil","Thief","Kid","Dragon","skeleton","Archer","Runner","Jumper","Knight","Adventurer","Warrior","Mage","Druid","Ninja"]
+for c in ime:
 
-fs1=random.choice(firstL)
-fs2=random.choice(secondL)
-fs=fs1+"_"+fs2
+    c = c.upper()
+    if (c == "A"):
+        print("..######..\n..#....#..\n..######..\n..#....#..\n..#....#..\n\n")
+    elif (c == "B"):
+        print("..######..\n..#....#..\n..#####...\n..#....#..\n..######..\n\n")
+    elif (c == "C"):
+        print("..######..\n..#.......\n..#.......\n..#.......\n..######..\n\n")
+    elif (c == "D"):
+        print("..#####...\n..#....#..\n..#....#..\n..#....#..\n..#####...\n\n")
+    elif (c == "E"):
+        print("..######..\n..#.......\n..#####...\n..#.......\n..######..\n\n")
+    elif (c == "F"):
+        print("..######..\n..#.......\n..#####...\n..#.......\n..#.......\n\n")
+    elif (c == "G"):
+        print("..######..\n..#.......\n..#####...\n..#....#..\n..#####...\n\n")
+    elif (c == "H"):
+        print("..#....#..\n..#....#..\n..######..\n..#....#..\n..#....#..\n\n")
+    elif (c == "I"):
+        print("..######..\n....##....\n....##....\n....##....\n..######..\n\n")
+    elif (c == "J"):
+        print("..######..\n....##....\n....##....\n..#.##....\n..####....\n\n")
+    elif (c == "K"):
+        print("..#...#...\n..#..#....\n..##......\n..#..#....\n..#...#...\n\n")
+    elif (c == "L"):
+        print("..#.......\n..#.......\n..#.......\n..#.......\n..######..\n\n")
+    elif (c == "M"):
+        print("..#....#..\n..##..##..\n..#.##.#..\n..#....#..\n..#....#..\n\n")
+    elif (c == "N"):
+        print("..#....#..\n..##...#..\n..#.#..#..\n..#..#.#..\n..#...##..\n\n")
+    elif (c == "O"):
+        print("..######..\n..#....#..\n..#....#..\n..#....#..\n..######..\n\n")
+    elif (c == "P"):
+        print("..######..\n..#....#..\n..######..\n..#.......\n..#.......\n\n")
+    elif (c == "Q"):
+        print("..######..\n..#....#..\n..#.#..#..\n..#..#.#..\n..######..\n\n")
+    elif (c == "R"):
+        print("..######..\n..#....#..\n..#.##...\n..#...#...\n..#....#..\n\n")
+    elif (c == "S"):
+        print("..######..\n..#.......\n..######..\n.......#..\n..######..\n\n")
+    elif (c == "T"):
+        print("..######..\n....##....\n....##....\n....##....\n....##....\n\n")
+    elif (c == "U"):
+        print("..#....#..\n..#....#..\n..#....#..\n..#....#..\n..######..\n\n")
+    elif (c == "V"):
+        print("..#....#..\n..#....#..\n..#....#..\n...#..#...\n....##....\n\n")
+    elif (c == "W"):
+        print("..#....#..\n..#....#..\n..#.##.#..\n..##..##..\n..#....#..\n\n")
+    elif (c == "X"):
+        print("..#....#..\n...#..#...\n....##....\n...#..#...\n..#....#..\n\n")
+    elif (c == "Y"):
+        print("..#....#..\n...#..#...\n....##....\n....##....\n....##....\n\n")
+    elif (c == "Z"):
+        print("..######..\n......#...\n.....#....\n....#.....\n..######..\n\n")
+    elif (c == " "):
+        print("..........\n..........\n..........\n..........\n\n")
+    elif (c == "."):
+        print("----..----\n\n")
 
-print("Your name is: ")
-print(fs)
-print("")
-print("")
+print("______________________________________")
+print("If you like it!!! Please hit one like.")
+print("______________________________________")
+print("Thanks for all the likes people! I have made this in Delphi for you")
+print("There is a repo in Bitbucket, so if you want you can compile it yourself")
+print("Or you can download exe just for fun :)")
+print("REPO: https://kordarei@bitbucket.org/kordarei/solo_magic.git")
