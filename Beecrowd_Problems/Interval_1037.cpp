1ccd3462c2d91360ef48e5b347731027b0ff596b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-25 23:03:12 +0600

    b

--- Beecrowd_Problems/Interval_1037.cpp
+++ Beecrowd_Problems/Interval_1037.cpp
@@ -6,15 +6,15 @@ int main(){
 	float n;
 	cin>>n;
 
-	if(n>=0 && n<=25.0){
+	if(n>=0.0000 && n<=25.0000){
 		cout<<"Intervalo (0,25]"<<endl;
-	}else if(n>25.0 && n<=50.0){
+	}else if(n>25.0000 && n<=50.0000){
 		cout<<"Intervalo (25,50]"<<endl;
-	}else if(n>50.0 && n<=75.0){
+	}else if(n>50.0000 && n<=75.0000){
 		cout<<"Intervalo (50,75]"<<endl;
-	}else if(n>75.0 && n<=100.0){
+	}else if(n>75.0000 && n<=100.0000){
 		cout<<"Intervalo (75,100]"<<endl;
-	}else if(n<0 || n>100.0){
+	}else if(n<0.0000 || n>100.0000){
 		cout<<"Fora de intervalo"<<endl;
 	}
     
