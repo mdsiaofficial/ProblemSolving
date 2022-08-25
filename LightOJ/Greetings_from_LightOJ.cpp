7a21c3a20535590664499867ed804ac4a0131de8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-25 08:42:14 +0600

    gg

--- LightOJ/Greetings_from_LightOJ.cpp
+++ LightOJ/Greetings_from_LightOJ.cpp
@@ -6,16 +6,18 @@ int main(){
     int n;
     long int num[1000];
     cin>>t;
+    int sum;
 
     for(n=1; n<=t; n++){
         cin>>num[n]>>num[n+1];
         cin>>num[n+2]>>num[n+3];
-        int sum[n]= num[n]+num[n+1]
-        int sum[n+1]= num[n+2] + num[n+3];
+        sum[n]= num[n]+num[n+1];
+        sum[n+1]= num[n+2] + num[n+3];
     }
 
     for(int m=1; m<=t; m++){
-        cout<<"Case "<<m<<sum[m]<<endl<<"Case "<<m+1<<sum[m+1];
+        sum[1000];
+        cout<<"Case "<< m <<sum[m]<<endl<<"Case "<<m+1<<sum[m+1];
     }
 
     return 0;
