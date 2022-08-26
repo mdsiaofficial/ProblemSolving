4d1c0af17fa501cb1998897085fdbe87e3043e5e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-26 08:42:04 +0600

    done

--- Beecrowd_Problems/Interval_1037.cpp
+++ Beecrowd_Problems/Interval_1037.cpp
@@ -7,7 +7,7 @@ int main(){
 	cin>>n;
 
 	if(n>=0.0000 && n<=25.0000){
-		cout<<"Intervalo (0,25]"<<endl;
+		cout<<"Intervalo [0,25]"<<endl;
 	}else if(n>25.0000 && n<=50.0000){
 		cout<<"Intervalo (25,50]"<<endl;
 	}else if(n>50.0000 && n<=75.0000){
