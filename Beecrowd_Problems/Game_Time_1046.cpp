810ef80c742e2d1d8f86a69008bfdcda936d41e6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-26 10:35:03 +0600

    f

--- Beecrowd_Problems/Game_Time_1046.cpp
+++ Beecrowd_Problems/Game_Time_1046.cpp
@@ -8,11 +8,23 @@ int main(){
 
     int start, end;
     cin>>start>>end;
-
+/* 
     int hour = 24-start;
     int hout = hour + end;
 
     cout<<"O JOGO DUROU "<<hout<<" HORA(S)"<<endl;
-
+ */
+    int count=1;
+    if(start>end){
+        for(int i =start; i<=24; i++){
+            count++;
+            cout<<endl<<count<<endl<<i<<endl<<start<<endl;
+        }
+    }else{
+        for(int i =start; i<=end; i++){
+            count++;
+            cout<<endl<<count<<endl<<i<<endl<<start<<endl;
+        }
+    }
     return 0;
 }
