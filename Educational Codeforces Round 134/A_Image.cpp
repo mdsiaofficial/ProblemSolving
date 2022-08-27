ef60d650bfef4ca535df0463ba0108d2b5fe1dbe
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-27 22:41:37 +0600

    w

--- Educational Codeforces Round 134/A_Image.cpp
+++ Educational Codeforces Round 134/A_Image.cpp
@@ -2,7 +2,12 @@
 #include <iomanip> //this library need to use setprecision
 using namespace std;
 int main(){
+    char a,b,c,d;
+    cin>>a>>b>>c>>d;
 
-    
+    cout<<a<<b<<c<<d;
+
+    if(a!=b)
+
     return 0;
 }
