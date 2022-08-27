c9b61a54c20893e698aecc11dc78d39d9af9106c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-27 16:00:48 +0600

    ff

--- CodeForces/P01_Reverse_an_array.cpp
+++ CodeForces/P01_Reverse_an_array.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    int number[5];
+
+    //cin>>number[0];
+    //cout<<number[0];
+/*     cin>>n;
+    for(int i=0; i<n; i++){
+        //cout<<i<<" ";
+        cin>>number[i-1];
+    }
+    cout<<n[0]<<n[1]<<n[2]<<n[3]<<n[4];
+
+     */
     return 0;
 }
