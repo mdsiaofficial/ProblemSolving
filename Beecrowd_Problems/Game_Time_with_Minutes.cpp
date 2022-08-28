239496bfe83a7579ba86f3e64d62b55452fe6d3e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-28 12:56:34 +0600

    gg

---
+++ Beecrowd_Problems/Game_Time_with_Minutes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+
+    int sh,sm,eh,em;
+
+    cin>>sh>>sm>>eh>>em;
+
+    cout<<"O JOGO DUROU "<<sh<<" HORA(S) E "<<sm<<" MINUTO(S)"<<endl;
+
+    return 0;
+}
