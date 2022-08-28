251af749695b0188dde9f93c46ccf66739ecccaf
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-28 12:59:25 +0600

    ddd

--- Beecrowd_Problems/Game_Time_with_Minutes.cpp
+++ Beecrowd_Problems/Game_Time_with_Minutes.cpp
@@ -7,7 +7,19 @@ int main(){
 
     cin>>sh>>sm>>eh>>em;
 
-    cout<<"O JOGO DUROU "<<sh<<" HORA(S) E "<<sm<<" MINUTO(S)"<<endl;
+    int hour;
+    if(sh<eh){
+        hour = eh-sh;
+        cout<<"O JOGO DUROU "<<time<<" HORA(S)"<<endl;
+    }else if(sh>eh){
+        hour = 24-(sh-eh);
+        cout<<"O JOGO DUROU "<<time<<" HORA(S)"<<endl;
+    }else if(sh==eh){
+        hour = 24;
+        cout<<"O JOGO DUROU "<<time<<" HORA(S)"<<endl;
+    }
+
+    cout<<"O JOGO DUROU "<<hour<<" HORA(S) E "<<sm<<" MINUTO(S)"<<endl;
 
     return 0;
 }
