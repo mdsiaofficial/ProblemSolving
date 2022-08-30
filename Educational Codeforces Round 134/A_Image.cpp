0cd3c0b623cf00ed98a6a0121dfdcba0ec0befcd
Md Shoriful Islam <mdsiaofficial@gmail.com>
2022-08-30 06:59:40 +0600

    Copy korsi....

--- Educational Codeforces Round 134/A_Image.cpp
+++ Educational Codeforces Round 134/A_Image.cpp
@@ -1,13 +1,24 @@
 #include<bits/stdc++.h>
 #include <iomanip> //this library need to use setprecision
 using namespace std;
-int main(){
-    char a,b,c,d;
-    cin>>a>>b>>c>>d;
-
-    cout<<a<<b<<c<<d;
-
-    if(a!=b)
-
-    return 0;
+int t, sum;
+set<int> a;
+void solve() {
+	a.clear();
+	sum = 0;
+	string s1, s2;
+	cin >> s1 >> s2;
+	int x = s1[0] - 'a', y = s1[1] - 'a';
+	int z = s2[0] - 'a', k = s2[1] - 'a';
+	a.insert(x); a.insert(y);
+	a.insert(z); a.insert(k);
+	sum = a.size() - 1;
+	cout<<sum <<endl;
+}
+int main() {
+	cin >> t;
+	while(t --) {
+		solve();
+	}
+	return 0;
 }
