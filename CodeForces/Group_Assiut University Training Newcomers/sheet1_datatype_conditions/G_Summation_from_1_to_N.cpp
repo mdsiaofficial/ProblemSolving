045072e39c018ab758c6bb53b2adf35934f758e4
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-31 12:04:22 +0600

    cc

---
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath> //maths er function gula korar liga ei library 
+using namespace std;
+int main()
+{
+
+    long long int n;
+    cin>>n;
+    cout<<(n*(n+1))/2<<endl;
+
+
+    return 0;
+
+}
