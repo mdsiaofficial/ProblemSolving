b6ce92dde7fd6a37ac385bed88292efbe9873e37
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-31 12:13:49 +0600

    bb

---
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/H_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath> //maths er function gula korar liga ei library 
+using namespace std;
+int main()
+{
+    double a,b;
+
+    cin>>a>>b;
+    cout<<"floor "<<a<<" / "<<b<<" = "<<a/b<<endl;
+    cout<<"ceil "<<a<<" / "<<b<<" = "<<a%b<<endl;
+    cout<<"round "<<a<<" / "<<b<<" = "<<a/b<<endl;
+
+
+    return 0;
+
+}
