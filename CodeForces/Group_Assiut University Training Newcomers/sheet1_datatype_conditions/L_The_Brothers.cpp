4f405cba07f6f64072a0f24b3ac71e74972177bf
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-31 22:06:25 +0600

    hhh

--- CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/L_The_Brothers.cpp
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/L_The_Brothers.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 int main()
 {
-    string n1,n2;
-    cin>>n1>>n2;
-    
-/*
+    //string n1,n2;
+    string f1, s1, f2, s2;
+    //cin>>n1>>n2;
+    cin>>f1>>s1;
+    cin>>f2>>s2;
+/*  
     we often use the getline() function to read a line of text
     getline (cin, fullName);
 */
@@ -16,7 +18,8 @@ int main()
     //getline(cin, n1);
     //getline(cin, n2);
 
-    if(n1==n2){
+    //cout<<f1<<endl<<s1<<endl<<f2<<endl<<s2;
+    if(s1==s2){
         cout<<"ARE Brothers"<<endl;
 
     }else{
