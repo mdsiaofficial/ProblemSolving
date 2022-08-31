1eee326b47c3257d154fa8321197bb16dae7f8b6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-31 10:46:55 +0600

    c

--- CodeForces/Assiut University Training Newcomers/sheet1_datatype_conditions/C_Simple_Calculator.cpp
+++ CodeForces/Assiut University Training Newcomers/sheet1_datatype_conditions/C_Simple_Calculator.cpp
@@ -7,8 +7,8 @@ int main()
 {
     int a,b;
     cin>>a>>b;
-    cout<<a<<"+"<<b<<"="<<a+b<<endl;
-    cout<<a<<"*"<<b<<"="<<a*b<<endl;
-    cout<<a<<"-"<<b<<"="<<a-b<<endl;
+    cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+    cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+    cout<<a<<" - "<<b<<" = "<<a-b<<endl;
 	return 0;
 }
