711dc7cb95b342af95cb37dd76f8cc08405e2c7b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-01 01:23:53 +0600

    kkk

--- CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/O_Calculator.cpp
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/O_Calculator.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 int main()
 {
-    int 
+    int a,b,x;
+    cin>>a>>x>>b;
+    cout<<a<<x<<b;
+    if(x=='+'){
+        cout<<a+b<<endl;
+
+    }else if(x=='-'){
+        cout<<a-b<<endl;
+
+    }else if(x=='*'){
+        cout<<a*b<<endl;
+
+    }else if(x=='/'){
+        cout<<a/b<<endl;
+
+    }
+
+    
     return 0;
 }
