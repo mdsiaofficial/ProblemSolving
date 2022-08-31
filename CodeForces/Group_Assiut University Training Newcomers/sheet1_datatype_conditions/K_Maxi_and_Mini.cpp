18597751f540c8bc16cd5c0587399c58bbcec3d6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-08-31 21:47:12 +0600

    jj

--- CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/K_Maxi_and_Mini.cpp
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/K_Maxi_and_Mini.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 int main()
 {
-    long int a,b,c;
-    long int max, min;
+    long long int a,b,c;
+    long long int max, min;
     cin>>a>>b>>c;
 
-    if(a<b && a<c){
+    if(a<=b && a<=c){
         min=a;
-        if(b<c){
+        if(b<=c){
             max=c;
         }else{
             max=b;
         }
-    }else if(b<a && b<c){
+    }else if(b<=a && b<=c){
         min=b;
-        if(a<c){
+        if(a<=c){
             max=c;
         }else{
             max=a;
         }
     }else{
         min=c;
-        if(a<b){
+        if(a<=b){
             max=b;
         }else{
             max=a;
