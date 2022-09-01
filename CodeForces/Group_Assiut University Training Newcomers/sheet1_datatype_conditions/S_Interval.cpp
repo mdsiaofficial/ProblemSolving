de2a4717170de230c6b853559b92ff74779bf2c8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-01 12:55:25 +0600

    kkk

--- CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/S_Interval.cpp
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/S_Interval.cpp
@@ -6,14 +6,20 @@ int main()
     float x;
     cin>>x;
 
-    if(x>=0 && x<=25){
-        cout<<"Interval [0,25]"<<endl;
-
-    }else if(x>=0 && x<=25){
-        cout<<"Interval [0,25]"<<endl;
+    if(x<0 && x>100){
+        cout<<"Out of INtervals"<<endl;
         
     }else if(x>=0 && x<=25){
         cout<<"Interval [0,25]"<<endl;
+
+    }else if(x>25 && x<=50){
+        cout<<"Interval (25,50]"<<endl;
+        
+    }else if(x>50 && x<=75){
+        cout<<"Interval (50,75]"<<endl;
+        
+    }if(x>75 && x<=100){
+        cout<<"Interval (75,100]"<<endl;
         
     }
 
