afcdfdfd97cbc3ccb22f4326fa4ad9df2c0b0a3b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-01 12:52:46 +0600

    d

---
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/S_Interval.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> //maths er function gula korar liga ei library 
+using namespace std;
+int main()
+{
+    float x;
+    cin>>x;
+
+    if(x>=0 && x<=25){
+        cout<<"Interval [0,25]"<<endl;
+
+    }else if(x>=0 && x<=25){
+        cout<<"Interval [0,25]"<<endl;
+        
+    }else if(x>=0 && x<=25){
+        cout<<"Interval [0,25]"<<endl;
+        
+    }
+
+    return 0;
+}
