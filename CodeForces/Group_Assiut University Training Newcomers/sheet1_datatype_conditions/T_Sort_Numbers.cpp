cdcc4edc404ef3012ffa80fd0b17d7da1cf0fd33
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-01 23:25:58 +0600

    jj

--- CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/T_Sort_Numbers.cpp
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/T_Sort_Numbers.cpp
@@ -5,7 +5,30 @@ int main()
 {
     long int a,b,c;
     cin>>a>>b>>c;
-    
 
+    if(a<b && a<c){
+        if(b<c){
+            cout<<a<<endl<<b<<endl<<c<<endl;
+        }else{
+            
+            cout<<a<<endl<<c<<endl<<b<<endl;
+        }
+    }else if(b<a && b<c){
+        if(a<c){
+            cout<<b<<endl<<a<<endl<<c<<endl;
+        }else{
+            
+            cout<<b<<endl<<c<<endl<<a<<endl;
+        }
+    }else if(c<a && c<b){
+        if(a<b){
+            cout<<c<<endl<<a<<endl<<b<<endl;
+        }else{
+            
+            cout<<c<<endl<<b<<endl<<a<<endl;
+        }
+
+    }
+    
     return 0;
 }
