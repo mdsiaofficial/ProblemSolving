04b8d229988b7c4a5544b09aa063ff0715ddea20
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-01 23:41:53 +0600

    copy solve nimu nah.... nije korum

---
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/U_Float_or_Int.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath> //maths er function gula korar liga ei library 
+using namespace std;
+
+
+
+
+/* 
+// solve 1 part 2
+void retNr(int x)
+{
+    cout << "The entered number is an integer. " << endl;
+}
+void retNr(float x)
+{
+    cout << "The entered number is a float. " << endl;
+}
+ */
+
+
+
+ 
+int main()
+{
+
+/* 
+    // solve 1 part 1
+    int nr;
+    cout << "Please enter a number: " << endl;
+    cin >> nr;
+    retNr(nr);
+    return 0;
+
+ */
+
+
+
+/* 
+    // solve 2
+    int nr = 0; char ch;
+    cout << "Please enter a number: " << endl;
+    cin >> nr;
+    cin.get(ch);
+    if(ch=='.')
+    {
+        cout << "The entered number is a float. " << endl;
+    }
+    else
+    {
+         cout << "The entered number is an integer. " << endl;
+    }
+ */
+
+
+
+    return 0;
+}
