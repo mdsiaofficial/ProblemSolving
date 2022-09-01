adfa93ad2a862e4f554f8b4a7aa41d3e354b2e23
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-01 23:21:16 +0600

    ffg

---
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/T_Sort_Numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath> //maths er function gula korar liga ei library 
+using namespace std;
+int main()
+{
+    long int a,b,c;
+    cin>>a>>b>>c;
+
+    return 0;
+}
