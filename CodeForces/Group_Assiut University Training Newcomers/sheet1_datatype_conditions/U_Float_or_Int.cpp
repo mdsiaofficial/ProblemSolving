655b05474b5df52b2c9e1cac0696ec67b72f3d34
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-02 01:13:06 +0600

    kkk

--- CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/U_Float_or_Int.cpp
+++ CodeForces/Group_Assiut University Training Newcomers/sheet1_datatype_conditions/U_Float_or_Int.cpp
@@ -7,35 +7,35 @@ using namespace std;
 
 /* 
 // solve 1 part 2
-void retNr(int x)
+void Number(int x)
 {
     cout << "The entered number is an integer. " << endl;
 }
-void retNr(float x)
+void Number(float x)
 {
     cout << "The entered number is a float. " << endl;
 }
- */
 
+ */
 
 
 
 int main()
 {
 
-/* 
+/*  // unsolved actually, not providing Answer:
     // solve 1 part 1
     int nr;
     cout << "Please enter a number: " << endl;
     cin >> nr;
-    retNr(nr);
+    Number(nr);
     return 0;
-
- */
+*/
 
 
 
 /* 
+
     // solve 2
     int nr = 0; char ch;
     cout << "Please enter a number: " << endl;
@@ -49,6 +49,7 @@ int main()
     {
          cout << "The entered number is an integer. " << endl;
     }
+
  */
 
 
@@ -62,6 +63,51 @@ int main()
 
     cout<< typeof(number)<<endl;
   */   
+
+
+
+/* 
+
+    // solve 4
+    int number = 0;
+    char ch;
+    int fraction;
+    //cout << "Please enter a number: " << endl;
+    cin >>number; // decimal
+    cin.get(ch); //point
+    cin>>fraction;
+
+    //this conditions finds it a float or not with a point input
+    if(ch=='.'){
+        cout <<"float "<<number<<" 0."<<fraction<<endl;
+    }else{
+        cout << "int " <<number<<endl;
+    }
+
+
+ */
+
+
+
+/* 
+    //solve 5 : https://qr.ae/pvgFUh
+    float number;
+    int decimal = number;
+    float fraction = number-decimal;
+
+    if()
+
+ */
+
+    // SOLVE 6
+    string number;
+    cin>>number;
+    int de=(int)number;
+    float fr=(float)number;
+
+    cout<<number<<endl<<de<<endl<<fr<<endl;
+    //if(de)
+
 
     return 0;
 }
