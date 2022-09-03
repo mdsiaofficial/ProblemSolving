1246a46e2933ceeb0d5912a458bcca4f73e83b68
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-04 00:36:20 +0600

    ff

--- CodeMarshal/A_and_B_and_Operators.cpp
+++ CodeMarshal/A_and_B_and_Operators.cpp
@@ -2,6 +2,26 @@
 #include <iomanip> //this library need to use setprecision
 using namespace std;
 int main(){
+    int a,b;
+    char x;
+    int c;
+    
+    cin>>c;
+    for(int i=1; i<=c; i++){
+        cin>>a>>b>>x;
+        if(x=='+'){
+            cout<<"Case "<<i<<": "<<a+b<<endl;
+        }else if(x=='-'){
+            cout<<"Case "<<i<<": "<<a-b<<endl;
+        }else if(x=='*'){
+            cout<<"Case "<<i<<": "<<a*b<<endl;
+        }else if(x=='/'){
+            cout<<"Case "<<i<<": "<<a/b<<endl;
+        }else if(x=='%'){
+            cout<<"Case "<<i<<": "<<a%b<<endl;
+        }
+        
+    }
 
     
     return 0;
