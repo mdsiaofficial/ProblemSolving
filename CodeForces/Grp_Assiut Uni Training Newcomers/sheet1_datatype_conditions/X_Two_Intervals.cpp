524c28c5a90a35ceaeed9a971a184f9ed6366a6f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-03 22:25:11 +0600

    gg

--- CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/X_Two_Intervals.cpp
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/X_Two_Intervals.cpp
@@ -25,17 +25,17 @@ int main(){
  */
 
     // try 2
-    if(l1<l2 && l1<r2 && r1>l2 && r1<r2){
+    if(l1<=l2 && l1<=r2 && r1>=l2 && r1<=r2){
         cout<<l2<<" "<<r1<<endl;
-    }else if(l1>l2 && l1<r2 && r1>l2 && r1>r2){
+    }else if(l1>=l2 && l1<=r2 && r1>=l2 && r1>=r2){
         cout<<l1<<" "<<r2<<endl;
-    }else if(l1<l2 && l1<r2 && r1>l2 && r1>r2){
+    }else if(l1<=l2 && l1<=r2 && r1>=l2 && r1>=r2){
         cout<<l2<<" "<<r2<<endl;
-    }else if(l1>l2 && l1<r2 && r1>l2 && r1<r2){
+    }else if(l1>=l2 && l1<=r2 && r1>=l2 && r1<=r2){
         cout<<l2<<" "<<r1<<endl;
     }else{
         cout<<"-1"<<endl;
     }
-    
+
     return 0;
 }
