f77cb22b1893904818df5a183406b3dd3be30023
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-03 21:18:56 +0600

    ff

---
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/W_Mathematical_Expression.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+    long int a,b,s;
+    char x,y;
+
+    cin>>a>>x>>b>>y>>s;
+    if(x=='+' && a+b==)
+
+
+
+    return 0;
+}
