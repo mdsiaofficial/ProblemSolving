b55df50b7247cc540389df866c7d52be32b8a3dd
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-03 21:41:40 +0600

    fff

---
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/X_Two_Intervals.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+
+    long int l1,r1, l2,r2;
+    cin>>l1>>r1>>l2>>r2;
+    if(r1>=l2 && r1<=r2){
+        cout<<l2<<" "<<r1<<endl;
+    }else{
+        cout<<"-1"<<endl;
+    }
+
+
+    return 0;
+}
