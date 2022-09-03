6e81bc8ebecc350d07d2aa7a07671d9a85582947
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-04 00:20:22 +0600

    ff

---
+++ CodeMarshal/WelcomeToCSEdu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+using namespace std;
+int main(){
+    int x,y,z,res;
+    cin>>x>>y>>z;
+    res=(x+y)-z;
+    cout<<res<<endl;
+    
+    return 0;
+}
