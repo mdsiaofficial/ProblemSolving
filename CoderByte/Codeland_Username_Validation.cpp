5e7b8c8f9508ef30868e6536c10fa6fb1cda5144
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-05 06:48:14 +0600

    jh

---
+++ CoderByte/Codeland_Username_Validation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string CodelandUsernameValidation(string str) {
+  
+  // code goes here  
+  return str;
+
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  cout << CodelandUsernameValidation(coderbyteInternalStdinFunction(stdin));
+  return 0;
+    
+}
