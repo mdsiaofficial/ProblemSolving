908d29f3b1a7212e43d72d65e3d4bc67a8922c28
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-05 23:50:59 +0600

    fff

--- CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/Y_The_Last_2_digits.cpp
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/Y_The_Last_2_digits.cpp
@@ -2,11 +2,37 @@
 #include <iomanip> //this library need to use setprecision
 using namespace std;
 int main(){
+    unsigned long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    unsigned long long sum = a*b*c*d;
+
+
+     
+    cout<<sum<<endl;
+    //cout<<sum%100;
+    //sum=sum/10;
+    //cout<<sum%10<<endl;
+
+
+    
+    //solve 1: done
+    if(sum<=9){
+        cout<<sum<<endl;
+    }else{
+        cout<<sum%100<<endl;
+    }
 
 
 
 
+/* 
+    //solve 2: done
+    //The last K digits of N is just N%10^K.
 
+    cout<<sum%(10*10)<<endl;
+
+ */
 
 
     return 0;
