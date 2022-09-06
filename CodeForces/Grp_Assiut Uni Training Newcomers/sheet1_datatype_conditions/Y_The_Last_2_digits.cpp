0737286c0525518fea2b78bcd8d7999d495f5e03
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-06 22:11:29 +0600

    hhh

--- CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/Y_The_Last_2_digits.cpp
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/Y_The_Last_2_digits.cpp
@@ -2,14 +2,16 @@
 #include <iomanip> //this library need to use setprecision
 using namespace std;
 int main(){
-     long long a,b,c,d;
+
+
+    long long a,b,c,d;
     cin>>a>>b>>c>>d;
     
     long long sum = a*b*c*d;
 
 
      
-    cout<<sum<<endl;
+    //cout<<sum<<endl;
     //cout<<sum%100;
     //sum=sum/10;
     //cout<<sum%10<<endl;
