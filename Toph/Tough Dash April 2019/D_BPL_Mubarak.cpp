985c6d19f17506e94a32d8946fd65c98fd6c177c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-07 23:38:39 +0600

    hh

---
+++ Toph/Tough Dash April 2019/D_BPL_Mubarak.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    
+
+    return 0;
+}
