3cd5a0169e48ea6cfc90feee85c3c96f06dbb1ab
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-07 23:34:03 +0600

    hh

--- Toph/Tough Dash April 2019/C_Thought_Game.cpp
+++ Toph/Tough Dash April 2019/C_Thought_Game.cpp
@@ -8,10 +8,10 @@ int main(){
     int n;
     cin>>n;
     
-    for(int i=1; i<n; i++){
+    for(int i=0; i<n; i++){
         int a,b;
         cin>>a>>b;
-        
+
         if(((a+b)/2)%2==0){
             cout<<"Sadia will be happy."<<endl;
         }else{
