81fab53bc6004d63a700f04b11b2837abcb98f46
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-07 23:34:35 +0600

    kk

---
+++ Toph/Tough Dash April 2019/tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include <iomanip> //this library need to use setprecision
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    
+    for(int i=0; i<n; i++){
+        int a,b;
+        cin>>a>>b;
+
+        if(((a+b)/2)%2==0){
+            cout<<"Sadia will be happy."<<endl;
+        }else{
+            cout<<"Oops!"<<endl;
+        }
+    }
+
+    return 0;
+}
