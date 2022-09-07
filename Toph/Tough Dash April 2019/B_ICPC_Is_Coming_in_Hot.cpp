8ff787f41ebf147a5a094cc0cd129022bee60d50
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-08 01:27:40 +0600

    hh

--- Toph/Tough Dash April 2019/B_ICPC_Is_Coming_in_Hot.cpp
+++ Toph/Tough Dash April 2019/B_ICPC_Is_Coming_in_Hot.cpp
@@ -3,11 +3,38 @@
 #include <cmath>
 using namespace std;
 
+void find_max(int ans[])
+{
+    int max=ans[0];
+    int index=0;
+    for(int j=1;j<10;j++)
+    {
+        if(max<ans[j])
+        {
+             max=ans[j];
+             index=j;
+        }
+    }
+    cout<<index<<endl;
+}
 int main(){
-
-    long long int n;
-    cin>>n;
-    
+    string a;
+    string b="0123456789";
+    int ans[12];
+    cin>>a;
+    for(int i=0;i<10;i++)
+        ans[i]=0;
+    for(int i=0;i<a.size();i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            if(a[i]==b[j])
+            {
+                ans[j]+=1;
+            }
+        }
+    }
+    find_max(ans);
 
     return 0;
 }
