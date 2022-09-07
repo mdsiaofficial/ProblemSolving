b1e3d6d83f5b01ec4a51a1120cbd36b1e808cc30
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-07 23:21:32 +0600

    hh

--- Toph/Tough Dash April 2019/A_Fair_Distribution.cpp
+++ Toph/Tough Dash April 2019/A_Fair_Distribution.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main(){
+    int x,y;
+    cin>>x>>y;
+
+    //cout<<y%3
+
+    for(int i=0; y%x==0; i++){
+        y=y+1;
+
+    }
+    cout<<i<<endl;
     
     return 0;
 }
