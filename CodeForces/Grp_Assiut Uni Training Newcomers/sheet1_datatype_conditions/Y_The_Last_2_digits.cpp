49910973807e5f33a93a5fd63c7cb65d871d9a44
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-07 08:45:27 +0600

    hhh

--- CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/Y_The_Last_2_digits.cpp
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet1_datatype_conditions/Y_The_Last_2_digits.cpp
@@ -12,7 +12,12 @@ int main(){
     long long sum = a*b*c*d;
     cout<<sum<<endl;
     cout<<a%100<<endl<<b%100<<endl<<c%100<<endl<<d%100<<endl<<endl;
-
+    //solve 1: done
+    if(sum<10){
+        cout<<sum<<endl<<endl;
+    }else{
+        cout<<sum%100<<endl<<endl;
+    }
 
 
 // check 2
@@ -23,7 +28,12 @@ int main(){
     long long sum2 = a*b*c*d;
     cout<<sum2<<endl;
     cout<<a%100<<endl<<b%100<<endl<<c%100<<endl<<d%100<<endl<<endl;
-
+    //solve 1: done
+    if(sum<10){
+        cout<<sum<<endl;
+    }else{
+        cout<<sum%100<<endl;
+    }
 
 
     //cout<<sum%100;
