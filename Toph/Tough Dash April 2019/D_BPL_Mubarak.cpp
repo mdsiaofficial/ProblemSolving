c214388de12f76e9fd494d76d695a8240385387d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-08 00:09:47 +0600

    ,,

--- Toph/Tough Dash April 2019/D_BPL_Mubarak.cpp
+++ Toph/Tough Dash April 2019/D_BPL_Mubarak.cpp
@@ -7,7 +7,39 @@ int main(){
 
     int n;
     cin>>n;
-    
 
+    for(int i=0; i<n; i++){
+        string bowls;
+        cin>>bowls;
+        
+        char b;
+        int leng = str.length();
+        int ballcount;
+        for(int j=0; j<leng; j++){
+            b = str.at(i);
+            
+        }
+        for(int k=0; k<leng; k++){
+            //if(b[k]=='N' || b[k]=='W' || b[k]=='D' || b[k]=='O'){
+            if(b[k]>=0 && b[k]<=6){
+                ballcount++;
+            }else{
+                
+            }
+        }
+        //int overs;
+        if(ballcount<6){
+
+            cout<<ballcount<<" BALLS"<<endl;
+
+        }else if(ballcount==6){
+            cout<<"1 OVER"<<endl;
+        }else if(ballcount>6){
+            cout<<ballcount/6<<" OVER"<<ballcount%6<<" BALL"<<endl;
+        }
+        
+    }
+
+    
     return 0;
 }
