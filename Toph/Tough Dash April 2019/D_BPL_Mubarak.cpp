229ff293a2846aef4daca881f30006540d5f1b54
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-08 00:12:17 +0600

    hh

--- Toph/Tough Dash April 2019/D_BPL_Mubarak.cpp
+++ Toph/Tough Dash April 2019/D_BPL_Mubarak.cpp
@@ -21,7 +21,7 @@ int main(){
         }
         for(int k=0; k<leng; k++){
             //if(b[k]=='N' || b[k]=='W' || b[k]=='D' || b[k]=='O'){
-            if(b[k]>='0' && b[k]<='6'){
+            if(b[k]=='0' || b[k]=='1' || b[k]=='2' || b[k]=='3' || b[k]=='4' || b[k]=='5' || b[k]=='6'){
                 ballcount++;
             }else{
                 
