387c4464a18224b5c9af62580e9d98a2e5e0accf
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-08 16:01:07 +0600

    hh

--- Toph/Formatted_Number.cpp
+++ Toph/Formatted_Number.cpp
@@ -8,7 +8,24 @@ int main(){
     cin>>a;
     string b = to_string(a);
     reverse(b.begin(),b.end());
-    cout<<b<<endl;
+
+    string newdi ="";
+    int c=0;
+
+
+    for(int i=0;i<b.length();i++){
+        if(c==3){
+            newdi=newdi+",";
+            newdi=newdi+b[i];
+            c=0;
+        }else{
+            newdi=newdi+b[i];
+        }
+        c++;
+    }
+
+
+    //cout<<b<<endl;
     
     
 
