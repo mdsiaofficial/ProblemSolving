7b7a12039efdff3f21f86d928978870eb7e5e3da
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-09 09:00:49 +0600

    jj

---
+++ Practice_250+_beginner_Problem/Area_n_Circumference_of_Circle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main{
+
+    const double pi=3.14159;
+    float radius;
+
+    cin>>radius;
+
+    float area= pi*pow(radius,2);
+    float circumference= 2*pi*radius;
+
+    cout<<"Area: "<<area<<" & Circumference: "<<circumference<<endl;
+    return 0;
+}
