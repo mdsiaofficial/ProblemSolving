934917ac2fce10998b43784fad4c0eeb26e6f75f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-09 21:45:59 +0600

    b

---
+++ Practice_250+_beginner_Problem/Print_Ascii_values.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    //To find the ASCII value of a given character in C++, 
+    //we can use the int() function.
+    cin>>ch;
+    cout << "Enter the character: "<<int(ch)<<endl;
+
+    return 0;
+}
