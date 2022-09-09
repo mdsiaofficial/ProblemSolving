7d7b9318180c5429310a76215f1e6669779435a7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-09 23:20:27 +0600

    f

--- Practice_250+_beginner_Problem/Factorial.cpp
+++ Practice_250+_beginner_Problem/Factorial.cpp
@@ -5,11 +5,17 @@ using namespace std;
 int main(){
     long int a;
     cin>>a;
+    int f;
+    int i=1;
 
-    for(int i=1; i<=a; i++){
+    /*
+    for(i=1; i<=a; i++){
+        a=a*i;
+    }*/
+
+    while(i<=a){
         a=a*i;
     }
-
     cout<<a<<endl;
     return 0;
 }
