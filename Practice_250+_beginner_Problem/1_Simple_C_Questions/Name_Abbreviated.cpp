44eebd9e78bad57ec0213147eabb3116f23f6c0f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-11 00:53:59 +0600

    undone

--- Practice_250+_beginner_Problem/1_Simple_C_Questions/Name_Abbreviated.cpp
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Name_Abbreviated.cpp
@@ -8,7 +8,7 @@ int main(){
     cin>>firstname>>lastname;
     string shortcut ="";
     if(firstname.size() >= 5 && lastname.size() >= 5){
-        shortcut = firstname[0]+firstname[1]+ firstname[2]+ lastname[0] + lastname[1]lastname[2];
+        shortcut = firstname[0]+firstname[1]+ firstname[2]+ lastname[0] + lastname[1] + lastname[2];
         
     }else{
         shortcut = firstname[0]+firstname[1]+ lastname[0] + lastname[1];
