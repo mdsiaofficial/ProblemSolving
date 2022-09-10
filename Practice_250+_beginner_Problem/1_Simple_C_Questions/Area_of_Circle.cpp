dc9274a5eadbf360906c961cf4e6d0f7918cacb6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-10 10:20:48 +0600

    bb

---
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Area_of_Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+#define pi 3.14159
+
+int main(){
+
+    float radius;
+    cin>>radius;
+
+    float area= pi*pow(radius,2);
+    return 0;
+}
