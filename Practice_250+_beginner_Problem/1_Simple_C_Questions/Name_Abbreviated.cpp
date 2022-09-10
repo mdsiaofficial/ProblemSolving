f27474e250dfca72e076bff7d988105be7d92f10
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-11 00:52:43 +0600

    g

--- Practice_250+_beginner_Problem/1_Simple_C_Questions/Name_Abbreviated.cpp
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Name_Abbreviated.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main(){
 
-    string name;
+    string firstname, lastname;
+    cin>>firstname>>lastname;
+    string shortcut ="";
+    if(firstname.size() >= 5 && lastname.size() >= 5){
+        shortcut = firstname[0]+firstname[1]+ firstname[2]+ lastname[0] + lastname[1]lastname[2];
+        
+    }else{
+        shortcut = firstname[0]+firstname[1]+ lastname[0] + lastname[1];
+        
+    }
 
-    
+    cout<<shortcut<<endl;
+    return 0;
 }
