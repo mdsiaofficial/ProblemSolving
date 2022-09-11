0207d45bf95682e4711901aab2748d65ce077949
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-12 00:14:53 +0600

    f

---
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Vowel_Consonant.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    char ch;
+    cin>>ch;
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+        cout<<"Character is vowel."<<endl;
+    }else{
+        cout<<"Character is consonant."<<endl;
+
+    }
+    return 0;
+    
+}
