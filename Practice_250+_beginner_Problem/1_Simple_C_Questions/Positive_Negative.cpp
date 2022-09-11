2e6d017488da0f05544672255745d7dbc51e24da
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-12 00:11:27 +0600

    f

---
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Positive_Negative.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    if(n>0){
+        cout<<"Number is positive"<<endl;
+
+    }else{
+        cout<<"Number is negative"<<endl;
+    }
+    return 0;
+}
