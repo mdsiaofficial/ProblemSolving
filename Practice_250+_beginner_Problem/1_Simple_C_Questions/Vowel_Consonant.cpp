5977f88a05b58971fbb837f9f527d1a56f8233dd
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-12 00:15:52 +0600

    f

--- Practice_250+_beginner_Problem/1_Simple_C_Questions/Vowel_Consonant.cpp
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Vowel_Consonant.cpp
@@ -6,12 +6,12 @@ int main(){
     char ch;
     cin>>ch;
 
-    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
         cout<<"Character is vowel."<<endl;
     }else{
         cout<<"Character is consonant."<<endl;
 
     }
     return 0;
-    
+
 }
