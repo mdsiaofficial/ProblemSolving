56372fe08ec59579ffa37b56bffb731528ce910b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-12 09:32:19 +0600

    yy

---
+++ Beecrowd_Problems/Sum_of_consecutive_odd_Numbers_1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>                                                                                                                                          
+using namespace std;
+int main(){
+    
+    int x,y;
+    cin>>x>>y;
+
+    int odds=0;
+    if(x>y){
+        for(int i=x; i<y; i++){
+            if(i%2!=0){
+                odds=odds+i;
+            }
+        }
+        cout<<odds<<endl;
+    }else{
+        for(int i=y; i<x; i++){
+            if(i%2!=0){
+                odds=odds+i;
+
+            }
+        }
+        cout<<odds<<endl;
+    }
+    //cout<<odds<<endl;
+
+    return 0;
+}
