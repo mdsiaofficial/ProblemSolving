59dd976cfc23ea910c5852fb1ec79c2c5a014b23
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-13 16:05:27 +0600

    k

--- Beecrowd_Problems/Sum_of_consecutive_odd_Numbers_1.cpp
+++ Beecrowd_Problems/Sum_of_consecutive_odd_Numbers_1.cpp
@@ -26,12 +26,22 @@ int main(){
         }
         cout<<odds<<endl;
     } 
-    */
+   
+ */
 
+
+    for(int i=y; i<x; i++){
+        if(i%2!=0){
+            odds=odds+i;
+        }
+    }
+    cout<<odds<<endl;
+    
+/* 
     int h=9;
     int j=-4;
-    int l=h-j;
+    int l=h+j;
     cout<<l<<endl;
-
+ */
     return 0;
 }
