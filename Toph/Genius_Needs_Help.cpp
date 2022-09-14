e279ca0df5152428660b1ecf63ddcab4f0ba3252
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-14 22:24:49 +0600

    gggb

---
+++ Toph/Genius_Needs_Help.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    long long n1,n2;
+    int n;
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>n1>>n2;
+        cout<<Case #<<i<<": "<<n1+n2<<endl;
+    }
+    
+
+
+    return 0;
+}
