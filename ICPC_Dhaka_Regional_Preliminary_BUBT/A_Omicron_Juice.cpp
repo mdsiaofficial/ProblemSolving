2a2ab1f5094738167c427f1abe362926c191d948
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-16 10:43:23 +0600

    dh

---
+++ ICPC_Dhaka_Regional_Preliminary_BUBT/A_Omicron_Juice.cpp
@@ -0,0 +1,17 @@
+#inlcude <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int count;
+
+    cin>>count;
+
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+        long int a,b,c;
+        cin>>a>>b>>c;
+
+    }
+    return 0;
+}
