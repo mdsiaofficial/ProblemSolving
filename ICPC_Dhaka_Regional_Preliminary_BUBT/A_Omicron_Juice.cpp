84c92f0e1274e4c56c2a3a9960aece9fef8939eb
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-16 11:42:10 +0600

    c

--- ICPC_Dhaka_Regional_Preliminary_BUBT/A_Omicron_Juice.cpp
+++ ICPC_Dhaka_Regional_Preliminary_BUBT/A_Omicron_Juice.cpp
@@ -6,11 +6,17 @@ int main(){
 
     cin>>count;
 
-    for (int i = 0; i < count; i++)
+    for (int i = 1; i <= count; i++)
     {
         /* code */
         long int a,b,c;
         cin>>a>>b>>c;
+
+        if(a==b && b==c && a==c){
+            cout<<"Case "<<i<<": Peaceful."<<endl;
+        }else if(a<b && a<c && b<c){
+             
+        }
 
     }
     return 0;
