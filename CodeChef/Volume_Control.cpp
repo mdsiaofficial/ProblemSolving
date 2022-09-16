3b64f6af70fe8246084beb8b36957587ede4228b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-16 13:17:43 +0600

    d

---
+++ CodeChef/Volume_Control.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int count;
+
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+        int a,b;
+        cin>>a>>b;
+
+        if(a<b){
+            for(int j=a; j<b; j++){
+                
+            }
+        }
+    }
+    
+    
+    return 0;
+}
