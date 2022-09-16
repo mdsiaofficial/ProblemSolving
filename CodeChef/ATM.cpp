70f06e93da6b83e63b46a95fa0946e2b934d8036
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-16 14:01:28 +0600

    h

--- CodeChef/ATM.cpp
+++ CodeChef/ATM.cpp
@@ -9,16 +9,17 @@ int main(){
         //stod(total);
         //stod(amount);
 
-        float a=(float)amount;
-        float t=(float)total;
-        cout<<fixed<<setprecision(2)<<t-a-0.50<<endl;
+        double a=(double)amount;
+        double t=(double)total;
+        double remain= t-a-0.50;
+        cout<<setprecision(2)<<remain<<endl;
     }else if(amount>total || amount%5!=0){
         /* stof(total);
         stof(amount); */
-        float a=(float)amount;
-        float t=(float)total;
-
-        cout<<fixed<<setprecision(2)<<t<<endl;
+        double a=(double)amount;
+        double t=(double)total;
+        double remain=t;
+        cout<<t<<endl;
     }
 
     return 0;
