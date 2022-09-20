105ff5f554a4ad652814983e9a4c65837731badc
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-20 17:08:47 +0600

    n

---
+++ Beecrowd_Problems/Salary_Increase_1048.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+main(){
+    
+    double salary;
+    cin>>salary;
+    double ext;
+    if(salary >= 0 && salary <=400.00){
+
+        ext = (salary*(15/100));
+        salary=salary+ext;
+        cout<<fixed<<setprecision(2)<<"Novo salario: "<<salary<<endl;
+        cout<<fixed<<setprecision(2)<<"Reajuste ganho: "<<ext<<endl;
+        cout<<fixed<<setprecision(2)<<"Em percentual: 15 %"<<endl;
+
+    }else if(salary >= 400.01 && salary <=800.00){
+
+        ext = (salary*(12/100));
+        salary=salary+ext;
+        cout<<fixed<<setprecision(2)<<"Novo salario: "<<salary<<endl;
+        cout<<fixed<<setprecision(2)<<"Reajuste ganho: "<<ext<<endl;
+        cout<<fixed<<setprecision(2)<<"Em percentual: 12 %"<<endl;
+
+    }else if(salary >= 800.01 && salary <=1200.00){
+
+        ext = (salary*(10/100));
+        salary=salary+ext;
+        cout<<fixed<<setprecision(2)<<"Novo salario: "<<salary<<endl;
+        cout<<fixed<<setprecision(2)<<"Reajuste ganho: "<<ext<<endl;
+        cout<<fixed<<setprecision(2)<<"Em percentual: 10 %"<<endl;
+
+    }else if(salary >= 1200.01 && salary <=2000.00){
+
+        ext = (salary*(7/100));
+        salary=salary+ext;
+        cout<<fixed<<setprecision(2)<<"Novo salario: "<<salary<<endl;
+        cout<<fixed<<setprecision(2)<<"Reajuste ganho: "<<ext<<endl;
+        cout<<fixed<<setprecision(2)<<"Em percentual: 7 %"<<endl;
+
+    }else if(salary > 2000.00){
+
+        ext = (salary*(4/100));
+        salary=salary+ext;
+        cout<<fixed<<setprecision(2)<<"Novo salario: "<<salary<<endl;
+        cout<<fixed<<setprecision(2)<<"Reajuste ganho: "<<ext<<endl;
+        cout<<fixed<<setprecision(2)<<"Em percentual: 4 %"<<endl;
+
+    }
+
+
+    return 0;
+
+}
