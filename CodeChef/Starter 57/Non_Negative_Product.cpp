11c900e8be4251a492787a0cc2cb99125f414f4d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-22 21:13:24 +0600

    h

---
+++ CodeChef/Starter 57/Non_Negative_Product.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    for (int i = 0; i < t; i++)
+    {   
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int j=0; i<n; j++){
+
+            cin>>arr[j];
+            
+        }
+
+    }
+    
+
+}
