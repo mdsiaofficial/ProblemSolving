c498f3748cce9d8570cc265f83949ad747364698
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-22 21:38:57 +0600

    g

--- Beecrowd_Problems/Beginner/Animal_1049.cpp
+++ Beecrowd_Problems/Beginner/Animal_1049.cpp
@@ -6,17 +6,20 @@ int main(){
 
     string spec1, spec2, spec3;
     cin>>spec1;
-    cin>>spec2;
-    cin>>spec3;
+    
+    
 
     if(spec1=="vertebrado"){
+        cin>>spec2;
         if(spec2=="ave"){
+            cin>>spec3;
             if(spec3=="carnivoro"){
                 cout<<"aguia"<<endl;
             }else if(spec3=="onivoro"){
                 cout<<"pomba"<<endl;
             }
         }else if(spec2=="mamifero"){
+            cin>>spec3;
             if(spec3=="onivoro"){
                 cout<<"humem"<<endl;
             }else if(spec3=="herbivoro"){
@@ -24,13 +27,16 @@ int main(){
             }
         }
     }else if(spec1=="invertebrado"){
+        cin>>spec2;
         if(spec2=="inseto"){
+            cin>>spec3;
             if(spec3=="hematofago"){
                 cout<<"pulga"<<endl;
             }else if(spec3=="herbivoro"){
                 cout<<"lagarta"<<endl;
             }
         }else if(spec2=="anelideo"){
+            cin>>spec3;
             if(spec3=="hematofago"){
                 cout<<"sanguessuga"<<endl;
             }else if(spec3=="onivoro"){
