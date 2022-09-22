3d26f82f5d41f908ab30669cb22019de42d5cff8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-23 00:51:47 +0600

    nh

---
+++ Beecrowd_Problems/Beginner/Taxex_1051.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    float salary;
+    cin>>salary;
+    int newsal
+    if(salary >=0.00 && salary <=2000.00){
+        cout<<"Isento"<<endl;
+    }else if(salary >=2000.01 && salary <=3000.00){
+        newsal= (salary*8)/100;
+        cout<<fixed<<setprecision(2)<<"R$ "<<newsal<<endl;
+    }else if(salary >=3000.01 && salary <=4500.00){
+
+        newsal=(salary*18)/100;
+        cout<<fixed<<setprecision(2)<<"R$ "<<newsal<<endl;
+
+    }else if(salary >4500.00){
+        newsal = (salary*28)/100;
+        cout<<fixed<<setprecision(2)<<"R$ "<<newsal<<endl;
+    }
+
+    return 0;
+}
