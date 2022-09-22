abfd0359f53de1f35a0b697d53faf73e82b642f2
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-22 10:09:46 +0600

    f

--- Beecrowd_Problems/Beginner/Output_10.cpp
+++ Beecrowd_Problems/Beginner/Output_10.cpp
@@ -2,17 +2,6 @@
 //#include <iomanip> //this library need to use setprecision
 using namespace std;
 int main(){
-    
-
-    int n;
-    
-    cin>>n;
-
-    for(int i=1; i<=n; i++){
-        if(n%i==0){
-            cout<<i<<endl;
-        }
-    }
-
+        
     return 0;
 }
