9d706d3a6a05c5b00d3fc71c2a803a52d5755795
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-22 10:31:15 +0600

    g

--- Beecrowd_Problems/Beginner/Output_10.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h> // ei library add korle ar kisu add kora lage nah 
-//#include <iomanip> //this library need to use setprecision
-using namespace std;
-int main(){
-        
-    return 0;
-}
---
+++ Beecrowd_Problems/Mathematics/Factorial_Sum_1161.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    long long a,b;
+
+    cin>>a>>b;
+    long long factA=1;
+    long long factB=1;
+
+    
+    for(int i=a; i>=1; i--){
+        // a=a*i;
+        factA*=i;
+        //cout<<a<<endl;
+    }
+    for(int i=b; i>=1; i--){
+        // a=a*i;
+        factB*=i;
+        //cout<<a<<endl;
+    }
+    cout<<factA+factB<<endl;
+
+}
