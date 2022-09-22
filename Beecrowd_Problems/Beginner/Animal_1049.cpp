6c2fb6483cc8451c4dc58e8542de02b666210488
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-22 21:52:24 +0600

    hh

--- Beecrowd_Problems/Beginner/Animal_1049.cpp
+++ Beecrowd_Problems/Beginner/Animal_1049.cpp
@@ -3,12 +3,10 @@ using namespace std;
 
 int main(){
 
+/*  //my solve: 
 
     string spec1, spec2, spec3;
     cin>>spec1;
-    
-    
-
     if(spec1=="vertebrado"){
         cin>>spec2;
         if(spec2=="ave"){
@@ -18,7 +16,7 @@ int main(){
             }else{          // if(spec3=="onivoro"){
                 cout<<"pomba"<<endl;
             }
-        }else if(spec2=="mamifero"){
+        }else{     //if(spec2=="mamifero"){
             cin>>spec3;
             if(spec3=="onivoro"){
                 cout<<"humem"<<endl;
@@ -35,7 +33,7 @@ int main(){
             }else{              // if(spec3=="herbivoro"){
                 cout<<"lagarta"<<endl;
             }
-        }else if(spec2=="anelideo"){
+        }else{      //if(spec2=="anelideo"){
             cin>>spec3;
             if(spec3=="hematofago"){
                 cout<<"sanguessuga"<<endl;
@@ -45,5 +43,54 @@ int main(){
         }
     }
 
+*/
+
+
+    string c;
+
+    cin >> c;
+
+    if(c == "vertebrado"){
+  
+        cin >> c;
+        if(c == "ave"){
+
+            cin >> c;
+            if(c == "carnivoro"){
+                cout << "aguia" << endl;
+            }else{
+                cout << "pomba" << endl;
+            }
+        }else{
+
+            cin >> c;
+            if(c == "onivoro"){
+                cout << "homem" << endl;
+            }else{
+                cout << "vaca" << endl;
+            }
+        }
+    }else{
+
+        cin >> c;
+        if(c == "inseto"){
+
+            cin >> c;
+            if(c == "hematofago"){
+                cout << "pulga" << endl;
+            }else{
+                cout << "lagarta" << endl;
+            }
+        }else{
+
+            cin >> c;
+            if(c == "hematofago"){
+                cout << "sanguessuga" << endl; 
+            }else{
+                cout << "minhoca" << endl;
+            }
+        }
+    }
+    
     return 0;
 }
