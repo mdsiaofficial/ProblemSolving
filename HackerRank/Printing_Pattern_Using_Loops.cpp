19913ab582b395d25f68ce6ccabe40284d6624a8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-23 17:41:14 +0600

    k

--- HackerRank/Printing_Pattern_Using_Loops.cpp
+++ HackerRank/Printing_Pattern_Using_Loops.cpp
@@ -19,14 +19,21 @@ int main(){
     
  */
 
-    for(int i=1; i<=n; i++){
+/*     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             cout<<"* ";
         }
         cout<<endl;
     }
 
-    
+ */
+
+    for(int i=n; i>0; i--){
+        for(int j=i; j>0; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
