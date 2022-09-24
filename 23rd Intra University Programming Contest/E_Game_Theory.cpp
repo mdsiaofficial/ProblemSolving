b52e3ba7b76e7f523b56ca45980f85dc352be1f5
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-24 16:06:12 +0600

    m

--- 23rd Intra University Programming Contest/E_Game_Theory.cpp
+++ 23rd Intra University Programming Contest/E_Game_Theory.cpp
@@ -2,36 +2,36 @@
 using namespace std;
 
 int main(){
-    int t;
+    int t; //test case variable declaration
 
-    cin>>t;
+    cin>>t; //input test case variable
 
 
-    int lc=0;
-    int uc=0;
+    int lc=0; //lower case variable
+    int uc=0; //upper case variable
 //int arr[n]={};
-    for(int h=1; h<=t; h++){
-        int n;
-        cin>>n;
-        string ss;
-        cin>>ss;
+    for(int h=1; h<=t; h++){ //loop for test case
+        int n; //input size of string variable
+        cin>>n; //input size of string variable
+        string ss; //input string variable
+        cin>>ss; //input string variable
 
-        for(int i=0; i<sizeof(ss); i++){
-            char arr[n];
-            arr[i]=ss[i];
+        for(int i=0; i<sizeof(ss); i++){ //loop for string
+            char arr[n]; //array for string variable
+            arr[i]=ss[i]; //array for string variable
         //cout<<arr[i]<<endl;
-            for(int j=0; j<sizeof(ss); j++){
-                if(arr[j]>='a' && arr[j]<='z'){
-                    lc++;
-                }else if(arr[j]>='Z' && arr[j]<='Z'){
-                    uc++;
+            for(int j=0; j<sizeof(ss); j++){ //loop for string
+                if(arr[j]>='a' && arr[j]<='z'){ //condition for lower case
+                    lc++; //increment lower case
+                }else if(arr[j]>='Z' && arr[j]<='Z'){ //condition for upper case
+                    uc++; //increment upper case
                 }
             }
         }
-        if(lc>uc){
-            cout<<"Alice"<<endl;
-        }else if(uc>lc){
-            cout<<"Bob"<<endl;
+        if(lc>uc){ //condition for lower case
+            cout<<"Alice"<<endl; //print alice
+        }else if(uc>lc){ //condition for upper case
+            cout<<"Bob"<<endl; //print bob
         }
 
     }
@@ -39,5 +39,3 @@ int main(){
 
     return 0;
 }
-
-
