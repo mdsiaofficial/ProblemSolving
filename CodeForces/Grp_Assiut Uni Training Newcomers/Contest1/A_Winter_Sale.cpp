16f4e64eb13eafcf7eee321d1ce2cef26b185321
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-24 17:17:07 +0600

    f

--- CodeForces/Grp_Assiut Uni Training Newcomers/Contest1/A_Winter_Sale.cpp
+++ CodeForces/Grp_Assiut Uni Training Newcomers/Contest1/A_Winter_Sale.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main(){
     
+    double per, price;
+    cin>>per>>price;
     
+    double mainprice=((100*price)/(100-per));
+    cout<<fixed<<setprecision(2)<<mainprice<<endl;
+
     return 0;
 }
