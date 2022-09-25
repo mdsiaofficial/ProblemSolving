4708a143509472bc53fda3a27bb8dd2b2d903d88
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-25 10:17:58 +0600

    g

---
+++ CodeForces/Grp_Assiut Uni Training Newcomers/Contest1/E_Interval_Sweep.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    
+    if((a-b)>==1 || (b-a)==1){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
