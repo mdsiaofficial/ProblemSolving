ad14408e5cc337213a608ae9aa230d5df02bc41c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-26 01:47:43 +0600

    b

--- CSESFI/Missing_Number.cpp
+++ CSESFI/Missing_Number.cpp
@@ -22,9 +22,26 @@ int main() {
  */
 
 
-    int n;
+    long long n;
+    long long sumofn=1;
+    long long sumofnum=0;
     cin>>n;
+    long int num[n-1];
+    for(long long i=0; i<n-1; i++){
+        cin>>num[i];
+        sumofn=sumofn+i;
+        sumofnum=sumofnum+num[i];
+        // cout<<num[i]<<endl;
+        // cout<<sumofn<<endl;
+        // cout<<sumofnum<<endl;
+    }
+    long long miss=sumofnum-sumofn;
+    cout<<miss<<endl;
 
-    
+/*     
+    for(long int j=0; j<n; j++){
+        if(num[j])
+    }
+ */
 	return 0;
 }
