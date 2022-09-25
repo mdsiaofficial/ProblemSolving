bd5e4f4f3abd796ade2c0bbb6ea8c3c534b6efb5
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-26 01:57:57 +0600

    s

--- CSESFI/Missing_Number.cpp
+++ CSESFI/Missing_Number.cpp
@@ -2,26 +2,6 @@
 using namespace std;
 
 int main() {
-/* 	
-    long long int n;
-    int t[1000];
-    cin >> n;
-
-    for (long long int i = 0; i < n-1; i++) {
-        cin>>t[i];
-    }
-
-    for(long long int j=0; j<n; j++){
-        cout << t[j] << " ";
-    }
- */
-
-  /*   
-    int sumofnthnumber= (n*(n+1))/2;
-    cout<<sumofnthnumber;
- */
-
-
     long long n;
     long long sumofn=0;
     long long sumofnum=0;
@@ -29,27 +9,12 @@ int main() {
     long int num[n-1];
     for(long long i=0; i<n-1; i++){
         cin>>num[i];
-        //sumofn=sumofn+i;
-        //sumofnum=sumofnum+num[i];
-        // cout<<num[i]<<endl;
-        // cout<<sumofn<<endl;
-        // cout<<sumofnum<<endl;
+        sumofnum=sumofnum+num[i];
     }
-
     for(long long j=1; j<=n; j++){
         sumofn=sumofn+j;
     }
-    for(long long z=0; z<n-1; z++){
-        sumofnum=sumofnum+num[z];
-    }
-
     long long miss=sumofn-sumofnum;
     cout<<miss<<endl;
-
-/*     
-    for(long int j=0; j<n; j++){
-        if(num[j])
-    }
- */
 	return 0;
 }
