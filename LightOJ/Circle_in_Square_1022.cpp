3d902fe0c43cd26c25a49090db5f3fd9fe8b0648
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-25 08:48:55 +0600

    b

--- LightOJ/Circle_in_Square_1022.cpp
+++ LightOJ/Circle_in_Square_1022.cpp
@@ -4,13 +4,17 @@ using namespace std;
 #define pi 3.14159
 int main(){
 
+    //unsolved
     
     int t;
     cin>>t;
+    int c=1;
     while(t--){
         double r;
         cin>>r;
         double area=2*pi*r;
+        cout<<"Case "<<c<<": "<<area<<endl;
+        c++;
     }
 
     return 0;
