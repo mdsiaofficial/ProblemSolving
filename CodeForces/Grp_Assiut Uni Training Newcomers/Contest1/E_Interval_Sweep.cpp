a71931a9ad18c567c49a4eac131471b871f8a142
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-25 10:26:40 +0600

    b

--- CodeForces/Grp_Assiut Uni Training Newcomers/Contest1/E_Interval_Sweep.cpp
+++ CodeForces/Grp_Assiut Uni Training Newcomers/Contest1/E_Interval_Sweep.cpp
@@ -1,11 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+
+    //unsolved
     int a,b;
     cin>>a>>b;
     
-    if((a-b)>==1 || (b-a)==1){
-        cout<<"YES"<<endl;
+    if((a-b)==1 || (b-a)==1 || a==b){
+        if(a==b){
+            cout<<"YES"<<endl;
+        }else if(a==0 && b==0){
+            cout<<"NO"<<endl;
+        }
     }else{
         cout<<"NO"<<endl;
     }
