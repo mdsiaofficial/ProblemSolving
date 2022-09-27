8d4f0c8535c0844e89e13408d6fe262866d0fedb
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-27 17:59:23 +0600

    f

---
+++ CodeForces/Codeforces Round #340 (Div. 2)/A_Elephant.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int d;
+    cin>>d;
+
+    if(d%5==0){
+        cout<<d/5<<endl;
+    }else if(d%4==0){
+        cout<<d/4<<endl;
+    }else if(d%3==0){
+        cout<<d/3<<endl;
+    }else if(d%2==0){
+        cout<<d/2<<endl;
+    }else{
+        cout<<d<<endl;
+    }
+    return 0;
+}
