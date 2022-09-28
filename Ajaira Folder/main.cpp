3413093730541de13581a7ac0b1705b71964d91d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-28 22:08:52 +0600

    f

--- Ajaira Folder/main.cpp
+++ Ajaira Folder/main.cpp
@@ -1,75 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    cout<<"hhh";
-/* 
-    // try 1
-    int x, y; 
-    x = 1; 
-    y = x; 
-    x = 2; 
-    printf("%d", y);
-
-
-    // try 2
-    int x=1; y=x;
-    x=2;
-    printf("x=%d \tJarin er chosma y=%d", x,y);
-
 
     
-    // try 3
-    x = 5; 
-    y = x-2;
-    x=x+5-2;
-    printf("x=%d \tJarin er chosma y=%d", x,y);
 
-
- */
-    
-/*     
-    int n;
-    char h=',';
-    cin>>n;
-    string s= 
-    /* int n1=n%1000;
-
-    int n12=n/1000;
-
-    int n2=n12%1;
- */
-    //cout<<ch<<endl;
-
-    //cout<<n2<<h<<n1;
-
-   /*  for(int i=0;i<n1;i++){
-        
-    } */
-
-
-/*
-    string s;
-    cin>>s;
-    reverse(s.begin(),s.end());
-
-    // 1171123 > 3211711
-
-    string ss="";
-    int count;
-    for(int i=0;i<s,size(); i++){
-        if(count==3){
-            ss+=1;
-            ss+=s[i];
-            count=0;
-        }else{
-            ss+=s[i];
-        }
-        count++;
-    }
-
-    reverse(ss.begin(),ss.end());
-    cout<<ss<<endl;
-
-*/
-
+    return 0;
 }
