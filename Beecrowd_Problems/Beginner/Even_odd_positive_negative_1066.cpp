3c67214b7f5fb272678f3291777cbc8a635bd41c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-28 16:46:30 +0600

    u

---
+++ Beecrowd_Problems/Beginner/Even_odd_positive_negative_1066.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    int even=0,odd=0,pos=0,neg=0;
+    for(int i=1;i<=5;i++){
+        if(a%2==0){
+            if(b%2==0){
+                if(c%2==0){
+                    if(d%2==0){
+                        if(e%2==0){
+
+                        }
+                }
+        }else{
+            
+        }
+    }
+}
