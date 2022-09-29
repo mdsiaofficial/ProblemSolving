ab50fed8985043bb8ffaa2c0570dd5f8c86601a4
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-29 23:25:34 +0600

    f

--- CodeForces/Codeforces Round #340 (Div. 2)/617A_Elephant.cpp
+++ CodeForces/Codeforces Round #340 (Div. 2)/617A_Elephant.cpp
@@ -17,7 +17,31 @@ int main(){
     }
 
  */
+
+
+
+/* 
     int steps[5]={5,4,3,2,1};
+    int c=0;
+    for(int i=0;i<5;i++){
+        if(d>=steps[i]){
+            c=d/steps[i]+c;
+            d=d%steps[i];
+        }
+    }
+    cout<<c<<endl;
+
+*/
+
+
+    int stp=5;
+    int ct=0;
+
+    if(d%5==0){
+        cout<<d/5<<endl;
+    }else{
+        cout<<(d/5)+1<<endl;
+    }
 
     
     
