1b21e2aa6effc00c6be0d63d13f08653830fb208
mdsiaofficial <mdsiaofficial@gmail.com>
2022-09-30 09:05:32 +0600

    f

--- Ajaira Folder/my.cpp
+++ Ajaira Folder/my.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-//using namespace std;
+using namespace std;
+
+average(int n, int m){
+    int avrg=(n+m)/2;
+    return avrg;
+}
+
+
 int main(){
 
     std::cout<<"H";
     std::cout<<"h2";
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<ajaira_average(a,b)<<std::endl;
+
     return 0;
 }
