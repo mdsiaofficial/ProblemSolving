d1a5ae3da7c372eb0f874d84b8e13740e304dec9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-02 00:32:09 +0600

    h

---
+++ LearnCPP/sdfsd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+
+
+    //Best practice
+    //Your lines should be no longer than 80 chars in length.
+
+    
+    std::cout << "This is a really, really, really, really, really, really, really, "
+        "really long line\n"; // one extra indentation for continuation line
+
+    std::cout << "This is another really, really, really, really, really, really, really, "
+                 "really long line\n"; // text aligned with the previous line for continuation line
+
+    std::cout << "This one is short\n";
+
+}
