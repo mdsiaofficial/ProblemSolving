9ed3f9a6f077d6a1ebc857a56f565ebdd79a9b88
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-02 23:06:49 +0600

    nb

--- Beecrowd_Problems/Beginner/Game_Time_with_Minutes.cpp
+++ Beecrowd_Problems/Beginner/Game_Time_with_Minutes.cpp
@@ -86,24 +86,29 @@ int main(){
 
  */
 
-    // solve 2: mine
 
+
+
+
+    // solve 2: mine
     int hs, ms, he, me;
     cin >> hs >> ms >> he >> me;
     int tms = ( hs * 60 )+ ms;
     int tme =( he * 60 )+ me;
     int h;
     int m;
-    if(tms>tme){
-        h = (tms - tme) / 60;
-        m = (tms - tme) % 60;
+    if(tms<tme){
+        h = (tme - tms) / 60;
+        m = (tme - tms) % 60;
         cout << "O JOGO DUROU " << h << " HORA(S) E " << m << " MINUTO(S)" << endl;
-    }else if(tms<tme){
-        h = (1440 - (tme - tms)) / 60;
-        m = (1440 - (tme - tms)) % 60;
+    }else if(tms>tme){
+        h = (1440 - (tms - tme)) / 60;
+        m = (1440 - (tms - tme)) % 60;
         cout << "O JOGO DUROU " << h << " HORA(S) E " << m << " MINUTO(S)" << endl;
     }else{
         cout << "O JOGO DUROU " << 24 << " HORA(S) E " << 0 << " MINUTO(S)" << endl;
     }
-    return 0;
+
+
+    return 0;  
 }
