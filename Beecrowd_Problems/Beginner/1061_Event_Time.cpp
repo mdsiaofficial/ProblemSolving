c834469f7249b6d5553c82065877a1d89f00cff9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-02 22:41:17 +0600

    g

---
+++ Beecrowd_Problems/Beginner/1061_Event_Time.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    cout << "Dia";
+    int day_start;
+    cin >> day_start;
+    int hour_s, minute_s, second_s;
+    cin >> hour_s;
+    cout << ":";
+    cin >> minute_s;
+    cout << ":";
+    cin >> second_s;
+
+    cout << "Dia";
+    int day_end;
+    cin >> day_end;
+    int hour_e, minute_e, second_e;
+    cin >> hour_e;
+    cout << ":";
+    cin >> minute_e;
+    cout << ":";
+    cin >> second_e;
+
+
+
+    
+
+    return 0;
+}
