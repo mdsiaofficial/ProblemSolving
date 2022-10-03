afac60365c8be8db45ba9fcfdc92ea029c632b29
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-04 01:45:49 +0600

    d

---
+++ AIZU_Online_Judge/Course/Intro to Programming/ITP1_1_D.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int s;
+    cin >> s;
+    cout << s / (60 * 60) << ":" << (s % (60 * 60)) / 60 << ":" << (s % (60 * 60)) % 60 << endl;
+    return 0;
+}
