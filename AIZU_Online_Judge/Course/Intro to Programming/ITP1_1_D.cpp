7910773861bb6c476310c22416a0966e806c238d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-05 00:47:03 +0600

    e

--- AIZU_Online_Judge/Course/Intro to Programming/ITP1_1_D.cpp
+++ AIZU_Online_Judge/Course/Intro to Programming/ITP1_1_D.cpp
@@ -3,7 +3,10 @@ using namespace std;
 int main()
 {
     int s;
+    
     cin >> s;
     cout << s / (60 * 60) << ":" << (s % (60 * 60)) / 60 << ":" << (s % (60 * 60)) % 60 << endl;
+
+
     return 0;
 }
