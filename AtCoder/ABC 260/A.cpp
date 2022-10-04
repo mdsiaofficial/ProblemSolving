85dfebbe52a994103414c79df6f16927b225106a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-05 02:17:08 +0600

    c

--- AtCoder/ABC 260/A.cpp
+++ AtCoder/ABC 260/A.cpp
@@ -5,12 +5,15 @@ int main(){
     string ss;
     cin>>ss;
     char st[3];
-    for (int i = 0; i < count; i++)
-    {   
-        char st[i]=ss[i];
-        cout<<st[i]<<endl<<ss[i];
-        /* code */
-    }
+    st[0]=ss[0];
+    st[1]=ss[1];
+    st[2]=ss[2];
+    // for (int i = 0; i < 3; i++)
+    // {   
+    //     char st[i]=ss[i];
+    //     cout<<st[i]<<endl<<ss[i];
+    //     /* code */
+    // }
 
     int c;
 
@@ -25,6 +28,18 @@ int main(){
     }else if(st[0] == st[1] && st[0]==st[2] && st[2]==st[1]){
         cout<<"-1"<<endl;
     }
+
+    // if(st[0]!=st[1] && st[0]!=st[2]){
+    //     cout<<st[0]<<endl;
+    // }else if(st[1]!=st[0] && st[0]!=st[2]){
+    //     cout<<st[1]<<endl;
+    // }else if(st[2]!=st[1] && st[2]!=st[0]){
+    //     cout<<st[2]<<endl;
+    // }else if(st[0]!= st[1] && st[0]!=st[2] && st[2]!=st[1]){
+    //     cout<<st[0]<<endl;
+    // }else if(st[0] == st[1] && st[0]==st[2] && st[2]==st[1]){
+    //     cout<<"-1"<<endl;
+    // }
     
     return 0;
 
