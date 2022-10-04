d9311cd8c2ed0206af4143813db58e5fe8a3e41e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-05 01:56:06 +0600

    kf

--- AtCoder/ABC 260/A.cpp
+++ AtCoder/ABC 260/A.cpp
@@ -1,14 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-
-    int a,b,c;
+    
     string ss;
-    cin>>a;
-    cin>>b>>c;
     cin>>ss;
-
-    cout<<a+b+c<<" "<<ss<<endl;
+    char 
     return 0;
 
 }
