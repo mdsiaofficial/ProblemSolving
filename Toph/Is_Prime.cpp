e8563d8829c26532454388f3e797f4f9376532ad
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-05 23:25:12 +0600

    b

---
+++ Toph/Is_Prime.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n=10;
+    //cin>>n;
+
+    cout<<n<<endl<<++n<<endl<<n++<<endl;
+    printf("%d, %d, %d", n, ++n, n++);
+
+    return 0;
+}
