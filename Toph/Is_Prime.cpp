c886b807b1d56ba32a35c345d6f8adbd19fcc3c8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-05 23:34:53 +0600

    b

--- Toph/Is_Prime.cpp
+++ Toph/Is_Prime.cpp
@@ -5,7 +5,7 @@ int main(){
     int n=10;
     //cin>>n;
 
-    cout<<n<<endl<<++n<<endl<<n++<<endl;
+    //cout<<n<<endl<<++n<<endl<<n++<<endl;
     printf("%d, %d, %d", n, ++n, n++);
 
     return 0;
