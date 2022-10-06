ce7a7c2a1e9dde873ddbf9878282bed781bbad1c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-06 10:33:49 +0600

    j

---
+++ HackerRank/basic_data_type.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+
+    //printf(%d %)
+    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e<<endl;
+
+    return 0;
+}
