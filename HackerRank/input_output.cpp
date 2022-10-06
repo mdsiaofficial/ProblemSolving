e8da301a3cafd15a4f9fef6cb05876528e71ce02
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-06 10:29:16 +0600

    h

---
+++ HackerRank/input_output.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+    //string ss;
+    int n,nn,nnn;
+    //cin>>ss>>nn;
+    cin>>n>>nn>>nnn;
+
+    cout<<n+nn+nnn<<endl;
+
+    return 0;
+}
