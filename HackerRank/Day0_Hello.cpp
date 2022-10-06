089fe7a047e8851c994bcb0b6ae3ca6b1bd4b6cf
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-06 10:22:32 +0600

    f

--- HackerRank/Day0_Hello.cpp
+++ HackerRank/Day0_Hello.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main(){
     string ss;
+    // getline (cin, variable_name) it will get input with spacef
     getline(cin,ss);
     cout<<"Hello, World."<<endl<<ss<<endl;
-    
+
     return 0;
 }
