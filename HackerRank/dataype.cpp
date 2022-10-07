06f21e4237855776c59c3db02b415f2e5fbbc966
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-07 12:26:16 +0600

    h

---
+++ HackerRank/dataype.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    int ii;
+    double dd;
+    string ss;
+
+    cin>>ii>>dd;
+    getline(cin,ss);
+
+    cout<<i+ii<<endl<<d+dd<<endl<<s<<ss<<endl;
+    return 0;
+}
