16d4d0a94b2ce9ba5e13c975e18a88ced695e838
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-07 21:49:54 +0600

    l

--- AIZU_Online_Judge/Course/Intro to Programming/tempCodeRunnerFile.cpp
+++ AIZU_Online_Judge/Course/Intro to Programming/tempCodeRunnerFile.cpp
@@ -4,22 +4,14 @@ int main(){
 
     int width, height, x, y, r;
     cin>>width>>height>>x>>y>>r;
-/*  // failed
+
     if(x>0 && y>0 && x<width && y<height){
         cout<<"Yes"<<endl;
     }else{
         cout<<"No"<<endl;
 
     }
- */
 
-
-    if(x-r>=0 && y-r>=0 && x+r<=width && y+r<=height){
-        cout<<"Yes"<<endl;
-    }else{
-        cout<<"No"<<endl;
-
-    }
     return 0;
 
 }
