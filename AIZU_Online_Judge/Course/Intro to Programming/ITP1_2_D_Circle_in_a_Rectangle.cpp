992ef8f1c497d614bf521ab1d22d607022e3d508
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-07 21:40:10 +0600

    dd

--- AIZU_Online_Judge/Course/Intro to Programming/ITP1_2_D_Circle_in_a_Rectangle.cpp
+++ AIZU_Online_Judge/Course/Intro to Programming/ITP1_2_D_Circle_in_a_Rectangle.cpp
@@ -5,13 +5,13 @@ int main(){
     int width, height, x, y, r;
     cin>>width>>height>>x>>y>>r;
 
-    if(x>0 && y>0 && x<width && x<height){
+    if(x>0 && y>0 && x<width && y<height){
         cout<<"Yes"<<endl;
     }else{
         cout<<"No"<<endl;
 
     }
-    
+
     return 0;
 
-}
+}d
