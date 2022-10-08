2ffcda3a1a763e7fe9f817f9d8192169a1bb7a93
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-08 23:58:36 +0600

    g

--- LearnCPP/funci.cpp
+++ LearnCPP/funci.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 
+int sum(int a,int b){
+    int c=a+b;
+    return c;
+}
+
 int main(){
+    
+    int num1,num2;
+    cin>>num1>>num2;
+    cout<<"Sum="<<sum(num1,num2)<<endl;
     
 
     return 0;
