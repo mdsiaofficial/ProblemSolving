8faec260c57bf64691d237c57e877f9760c9e089
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-11 00:25:18 +0600

    fff

---
+++ manyAplusB.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    long long a,b;
+    
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    
+    return 0;
+}
