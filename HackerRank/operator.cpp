d73f6ae30dc1326e708962de204c3d78fd4bd5b6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-10 21:58:13 +0600

    h

--- HackerRank/operator.cpp
+++ HackerRank/operator.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+mealcost(double base_meal, int tips, int tax){
     
+
+    double total_meal= base_meal + (base_meal*(tips/100) + (base_meal*(tax/100))) ;
+
+    cout<<fixed<<total_meal<<endl;
+
+}
+int main(){
+
+    float a,b,c;
+    cin>>a>>b>>c;
+
+    mealcost(a,b,c);
+
     return 0;
 }
