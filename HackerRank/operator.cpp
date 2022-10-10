4288e62633359f7c517252301dfc610356fba699
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-10 22:09:56 +0600

    h

--- HackerRank/operator.cpp
+++ HackerRank/operator.cpp
@@ -1,20 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+/* 
 float mealcost(float base_meal, int tips, int tax){
     
 
     float total_meal= base_meal + (base_meal*(tips/100) + (base_meal*(tax/100))) ;
 
     cout<<total_meal<<endl;
+    //Math.round
     return total_meal;
 }
+ */
 int main(){
 
-    float a,b,c;
-    cin>>a>>b>>c;
+    float base_meal, tips, tax;
+    cin>>base_meal>>tips>>tax;
 
-    mealcost(a,b,c);
+    cout<<round(base_meal + (base_meal*(tips/100.0) + (base_meal*(tax/100.0))))<<endl;
+
+
+
+    //mealcost(a,b,c);
 
     return 0;
 }
