ade494329df18f3b169c3a588cbf0367a10f7314
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-12 08:06:13 +0600

    ddd

--- LearnCPP/cpbook.c
+++ LearnCPP/cpbook.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 int main(){
-
+/* 
     int a=50, b=60, sum;
     sum=a+b;
-    printf("%d + %d = %d", a, b, sum);
+    printf("%d + %d = %d\n", a, b, sum);
+
+    a=1000;
+    printf("Value: %d", a);
+    a=-20000;
+    printf("Value: %d", a);
+    a=10000000;
+
+     */
+
+
+    int a;
+    printf("\n%d\n",a);
     return 0;
 }
