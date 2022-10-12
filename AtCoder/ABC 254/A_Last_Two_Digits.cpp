7942aebf2850f837bca92e2c7f20adc927d5284d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-12 22:22:15 +0600

    d

---
+++ AtCoder/ABC 254/A_Last_Two_Digits.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+
+    int temp = n%10;
+    int temp2 = n/10;
+    int temp3 = temp2%10;
+    cout<<temp3<<temp3<<endl;
+
+    return 0;
+}
