7910253d235f24a0c5cb8af7e9b98c2d5da0f1b7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-12 08:22:24 +0600

    jj

--- LearnCPP/cpbook.c
+++ LearnCPP/cpbook.c
@@ -13,10 +13,25 @@ int main(){
 
      */
 
-
+/* 
     long long a;
     scanf("%d",&a);
     printf("\n%d\n",a);
+ */
+
+    int a; 
+    a = 1000; 
+    printf("Value of a is %d\n", a); 
+    a = -21000; 
+    printf("Value of a is %d\n", a); 
+    a = 10000000; 
+    printf("Value of a is %d\n", a); 
+    a = -10000000; 
+    printf("Value of a is %d\n", a); 
+    a = 100020004000503; 
+    printf("Value of a is %d\n", a); 
+    a = -4325987632; 
+    printf("Value of a is %d\n", a);
 
     return 0;
 }
