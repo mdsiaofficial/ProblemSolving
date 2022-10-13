aa90ad721986da01ee94ab076d5a873256c2761a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-13 19:47:25 +0600

    bbb

--- lab_final/boro_choto.c
+++ lab_final/boro_choto.c
@@ -1,30 +1,36 @@
-/* #include<stdio.h>
+#include<stdio.h>
 int main(){
+/* 
+    //int number1, number2;
+    //printf("Input two number: ");
 
-    int number1, number2;
-    printf("Input two number: ");
-
-    scanf("%d%d", &number1, number2);
+    //scanf("%d%d", &number1, number2);
     // 2 ta number input nilam
     int a=5;
-    printf("%d", (a > 0? a: -a));
+    printf("%d", (a > 0? a : --a));
     //printf("\nBoro: %d\nChoto: \n", (number1>number2? number1:number2), (number1<number2? number1:number2));
     //printf("\n%d", number1>number2);
-    return 0;
-}
-
  */
 
 
-#include<stdio.h>
 
-int main() {
 
+/* 
     int a = -5, b = 5;
-
     printf("%d\n", (a > 0? a: -a));
     printf("%d\n", (b > 0? b: -b));
+*/
 
+
+
+
+    int a=5;
+    int b=10;
+
+    printf("%d %d", (a>b)&&*(a!=b),)
     return 0;
-
 }
+
+
+
+
