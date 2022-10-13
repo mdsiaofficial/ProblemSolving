65a24d5e9eaa2cc69c2dfe92306bb938c03b3655
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-13 10:12:20 +0600

    f

--- lab_final/area_of_triangle.c
+++ lab_final/area_of_triangle.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
-    
+    float base, height, area;
+
+    printf("Enter the value of Base and Height: ");
+    scanf("%f%f", &base, &height);
+
+    area = (base * height)/2;
+
+    printf("Area of Triangle: %f")
     return 0;
 }
