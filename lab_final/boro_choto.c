18f7b04e5c499a5b1de74c1a238ae55e3e318a84
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-13 11:13:39 +0600

    d

--- lab_final/boro_choto.c
+++ lab_final/boro_choto.c
@@ -6,8 +6,8 @@ int main(){
 
     scanf("%d%d", &number1, number2);
     // 2 ta number input nilam
-    
-    printf("\nBoro: %d\nChoto: \n", (number1>number2));
-    
+
+    //printf("\nBoro: %d\nChoto: \n", (number1>number2));
+    printf("\n%d", number1>number2);
     return 0;
 }
