cf02ded4592adea9a1d1dc13aaf6a3a9a3a2dad5
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-13 20:53:07 +0600

    dd

--- lab_final/coumpound_interest.c
+++ lab_final/coumpound_interest.c
@@ -1,9 +1,29 @@
 #include<stdio.h>
 int main(){
 
-
-
+    /* 
+    A = P(1+(r/n))^nt
+    ci = (P(1+(r/n))^nt) - P
+    A = amount
+    P = principal
+    r = rate of interest
+    n = number of times interest is compounded per year
+    t = time (in years) 
+    */
     
+
+    float principal, rate, time, amount, N;
+
+    printf("Enter the value of Principal, Rate and Time and N(interest counts per year): ");
+    
+    scanf("%f%f%f%f", &principal, &rate, &time, &N);
+
+    int temp = N*time;
+    amount = principal*pow((1+(rate/N)), N*time);
+    float ci = amount - principal;
+
+    printf("Compound Interest: %f", ci);
+
     return 0;
     
 }
