3de63da754773f9ef03848ac72e7e572ef8278dd
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-13 23:04:56 +0600

    ggg

---
+++ CodeForces/Codeforces Round #827 (Div. 4)/A_Sum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        if(a==(b+c) || b==(a+c) || c==(a+b)){
+            cout<<"YES"<<endl;
+
+        }else{
+            cout<<"NO"<<endl;
+        }
+        
+    }
+
+    
+    
+    return 0;
+
+}
