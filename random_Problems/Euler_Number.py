aaae627e2e239de6430e1d6d91a70c85aa404798
Md Shoriful Islam <mdsiaofficial@gmail.com>
2022-10-15 08:16:30 +0600

    Update Euler_Number.py

--- random_Problems/Euler_Number.py
+++ random_Problems/Euler_Number.py
@@ -1,9 +1,9 @@
-/*
+"""
 Write a program to compute the value of 
 Euler's number e, that is used as the base of 
 natural logarithms. Use the following 
 formula. e=1+1/1! +1/2+1/3+...+1/n!
-*/
+"""
 
 def factorial_result(n):
    result = 1
