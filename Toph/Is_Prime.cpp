134340bf69ac442e81478e0e7552bef41bb36104
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-17 20:18:12 +0600

    cc

--- Toph/Is_Prime.cpp
+++ Toph/Is_Prime.cpp
@@ -17,11 +17,28 @@ int main(){
     if(n<2){
         cout<<"No"<<endl;
 
-    }else if(n%(n+1)!=0){
+    }else{
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                prime+=1;
+            }else{
+                prime+=1;
+            }
+        }
+        
+        if(prime==2 ){
+            cout<<"Yes"<<endl;
+
+        }else if(prime>2){
+            cout<<"No"<<endl;
+        }
+    }
+    
+/*     if(n%(n+1)!=0){
         cout<<"No"<<endl;
     }else if(n%n==0 && n%1==0){
         cout<<"Yes"<<endl;
     }
-
+ */
     return 0;
 }
