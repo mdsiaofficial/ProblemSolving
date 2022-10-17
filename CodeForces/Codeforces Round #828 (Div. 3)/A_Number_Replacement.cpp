7e9e69836c7cf3c91c70389352e4d00ae4b45c26
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-17 19:50:02 +0600

    hhh

--- CodeForces/Codeforces Round #828 (Div. 3)/A_Number_Replacement.cpp
+++ CodeForces/Codeforces Round #828 (Div. 3)/A_Number_Replacement.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-
+    //After the transformation all letters are united into a string,
+    //in our example we get the string "cacta".
 
     return 0;
 }
