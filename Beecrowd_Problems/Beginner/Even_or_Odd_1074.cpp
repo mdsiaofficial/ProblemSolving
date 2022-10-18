df3054de54640e8b00d30f3d9dfb17fb0d6e4d58
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 00:27:18 +0600

    ddd

--- Beecrowd_Problems/Beginner/Even_or_Odd_1074.cpp
+++ Beecrowd_Problems/Beginner/Even_or_Odd_1074.cpp
@@ -7,7 +7,26 @@ int main(){
 
     for (int i = 1; i < n; i++)
     {   
-        
+        int a;
+        cin>>a;
+
+        if(a==0){
+            cout<<"NULL"<<endl;
+
+        }else if(n%2==0){
+            if(n>0){
+                cout<<"EVEN POSITIVE"<<endl;
+            }else{
+                cout<<"EVEN NEGATIVE"<<endl;
+            }
+        }else if(n%2!=0){
+
+            if(n>0){
+                cout<<"ODD POSITIVE"<<endl;
+            }else{
+                cout<<"ODD NEGATIVE"<<endl;
+            }
+        }
         /* code */
     }
     
