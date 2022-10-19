b37d5d77436bced97fe93f316db269ea9dd93808
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 10:55:37 +0600

    dd

---
+++ Beecrowd_Problems/Beginner/Weighted_Averages_1079.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    for (int i = 0; i < n; i++)
+    {
+        
+        float x,y,z;
+        cin>>x>>y>>z;
+
+        float avrg = ((x*2)+(y*3)+(z*5))/3;
+        cout<<avrg<<endl;
+        
+    }
+    
+    return 0;
+}
