bab01ee8fdb058eba5858bc15c526aef598b6dea
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 11:41:08 +0600

    vv

--- LearnCPP/1_9.cpp
+++ LearnCPP/1_9.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main(){
 
     int a, b;
-    cin >> a >> b;
-    cout << a * b <<" "<< 2*(a + b) << endl;
-    
+    //cin >> a >> b;
+    //cout << a * b <<" "<< 2*(a + b) << endl;
+    for(int i=1; i<=100;i++){
+
+        cout<<rand()<<endl;
+    }
     return 0;
 }
