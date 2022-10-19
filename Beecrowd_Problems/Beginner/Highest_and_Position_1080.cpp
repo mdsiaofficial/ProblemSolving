754d0e656a25e9ad5fa8718dd62a3a568cd56084
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 16:31:11 +0600

    dd

--- Beecrowd_Problems/Beginner/Highest_and_Position_1080.cpp
+++ Beecrowd_Problems/Beginner/Highest_and_Position_1080.cpp
@@ -6,12 +6,13 @@ int main(){
     // read 100 int
 
     int position;
+    int big;
     int numbers[100];
     for(int i = 0; i<100; i++){
         cin>>numbers[i];
 
     }
-
+/* 
     for(int j=0; j<100; j++){
 
         for(int k=0; k<100; k++){
@@ -21,7 +22,28 @@ int main(){
             }
         }
     }
+ */ 
+/*     for(int z=0; z<100; z++){
+
+        if(numbers[z]>numbers[z+1]){
+            big=numbers[z];
+        }else{
+            big=numbers[z+1];
+        }
+    }
+ */
 
 
+    int num= numbers[0];
+    for(int h=0; h<100; h++){
+        if(num>numbers[h]){
+            big=num;
+            num=big;
+            
+        }else{
+            big=numbers[h];
+            num=numbers[h];
+        }
+    }
     return 0;
 }
