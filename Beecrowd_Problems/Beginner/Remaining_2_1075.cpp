b323862fd20f520d21a5fcf832a2a9dcb5e0525d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 10:29:12 +0600

    dd

---
+++ Beecrowd_Problems/Beginner/Remaining_2_1075.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+
+    for (int i = 1; i < 10000; i++)
+    {   
+
+        if(i%n==2){
+            cout<<i<<endl;
+        }
+        
+        /* code */
+    }
+        
+    return 0;
+}
