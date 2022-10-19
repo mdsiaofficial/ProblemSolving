78835b4c06debc6107d166c53abaebfb44637084
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 10:46:16 +0600

    ggg

--- Beecrowd_Problems/Beginner/Dividing_x_by_y_1116.cpp
+++ Beecrowd_Problems/Beginner/Dividing_x_by_y_1116.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int main(){
 
-    float x,y;
+    
     int n;
-
+    cin>>n;
     for(int i=1; i<=n; i++){
+        float x,y;
         cin>>x>>y;
 
         if(y==0){
             cout<<"divisao impossivel"<<endl;
 
         }else{
-            float div=(x)/(y);
+            float div=x/y;
             cout<<fixed<<setprecision(1)<<div<<endl;
         }
     }
