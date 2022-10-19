8ed8cfd39aadf1e900a90550f976b73e5ebf630a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 10:29:39 +0600

    dd

--- Beecrowd_Problems/Beginner/Remaining_2_1075.cpp
+++ Beecrowd_Problems/Beginner/Remaining_2_1075.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
 
     int n;
-
+    cin>>n;
     for (int i = 1; i < 10000; i++)
     {   
 
         if(i%n==2){
             cout<<i<<endl;
         }
-        
+
         /* code */
     }
         
