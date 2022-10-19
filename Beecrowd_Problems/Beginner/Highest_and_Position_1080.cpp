57edcc1aee5226d82f02b33e55ebc729259fe945
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 11:13:10 +0600

    hh

---
+++ Beecrowd_Problems/Beginner/Highest_and_Position_1080.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    // read 100 int
+
+    int position;
+    int numbers[100];
+    for(int i = 0; i<100; i++){
+        cin>>numbers[i];
+
+    }
+
+    for(int j=0; j<100; j++){
+
+        for(int k=0; k<100; k++){
+
+            if(numbers[j]>numbers[k]){
+                
+            }
+        }
+    }
+
+
+    return 0;
+}
