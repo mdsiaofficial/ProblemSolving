a95e838a4c72685832614989247aab453ecfc375
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-19 10:58:38 +0600

    dd

--- Beecrowd_Problems/Beginner/Weighted_Averages_1079.cpp
+++ Beecrowd_Problems/Beginner/Weighted_Averages_1079.cpp
@@ -9,7 +9,7 @@ int main(){
         float x,y,z;
         cin>>x>>y>>z;
 
-        float avrg = ((x*2)+(y*3)+(z*5))/3;
+        float avrg = ((x*2)+(y*3)+(z*5))/(2+3+5);
         cout<<fixed<<setprecision(1)<<avrg<<endl;
 
     }
