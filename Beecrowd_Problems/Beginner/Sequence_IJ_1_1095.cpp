ad048df0a6ef3ad7c6e243b916188ff1610c248e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-20 08:47:44 +0600

    kkk

---
+++ Beecrowd_Problems/Beginner/Sequence_IJ_1_1095.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    int d=1;
+    for(int i=60; i>=0; i-5){
+        cout<<"I="<<d<<" J="<<i<<endl;
+        d+=3;
+    }
+    return 0;
+}
