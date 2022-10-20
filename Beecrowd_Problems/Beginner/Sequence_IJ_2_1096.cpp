2bbf4577d955a7cb12fdd2d8439ca9bf6ef3fac0
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-20 15:35:41 +0600

    hh

---
+++ Beecrowd_Problems/Beginner/Sequence_IJ_2_1096.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    
+    for(int i=1; i<=9; i=i+2){
+        int j=7;
+        for(int c=1; c<=3; c++){
+            //int j=7;
+            cout<<"I="<<i<<" J="<<j<<endl;
+            j=j-1;
+        }
+    }
+
+    return 0;
+}
+
