65c4a0655439e44700b79bbc982274ac4dcbf49e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 12:20:22 +0600

    dd

--- Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
+++ Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 int main(){
+/* 
 
+    // try 1 failed
     float a,b;
     cin>>a;
     if(a<0.0 && a>10.0){
@@ -42,6 +44,50 @@ int main(){
         float media = (a+b)/2;
         cout<<fixed<<setprecision(2)<<"media = "<<media<<endl;
     }
+
+
+
+
+ */
+
+
+        // try 2 solved
+    float x, y, z, n;
+    y = 0;
+    z = 0;
+    n = 0;
+    while(true){
+                cin >> x;
+                if(n == 1){
+                     
+                if(x == 2){
+                     break;
+                }else if(x == 1){
+                       n = 0;
+                       continue;
+                }
+                else{
+                     cout << "novo calculo (1-sim 2-nao)n";
+                     continue;
+                     }
+                }else{
+                if(x < 0 || x > 10){
+                      cout << "nota invalidan";
+                      }
+                else if(z == 0){
+                      y = x;
+                      z = 1;
+                }
+                else{
+                     printf("media = %.2fn", (x+y)/2.00);
+                     z = 0;
+                     n = 1;
+                }
+                }
+                
+                if(n == 1) cout << "novo calculo (1-sim 2-nao)n";
+                     
+    }    
     return 0;
 
 }
