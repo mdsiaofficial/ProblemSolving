b3024af847eee28dcbc64357076b0da1e4fa0544
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 09:07:50 +0600

    jj

--- Beecrowd_Problems/Beginner/Sequence_ij_3_1097.cpp
+++ Beecrowd_Problems/Beginner/Sequence_ij_3_1097.cpp
@@ -1,18 +1,27 @@
-#include<bits/stdc++.h>
+//#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
-    
-    for(int i=1; i<=9; i=i+2){
-        int j=7;
-        for(int c=1; c<=3; c++){
+int main()
+{
+    int te = 1;
+    for (int i = 1; i <= 9; i = i + 2)
+    {
+        int j = 7;
+        for (int c = 1; c <= 3; c++)
+        {
             //int j=7;
-            cout<<"I="<<i<<" J="<<j<<endl;
-            j=j-1;
+            cout << "I=" << i << " J=" << j << endl;
+            j = j - 1;
+            //te = te + 1;
+        }
+        //  te++;
+        if (te == 4)
+        {
+            j = j + 2;
+            te = 1;
         }
     }
 
     return 0;
 }
-
