d84b0d1d06e81fab06f5b80db6dbf07af93d2450
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 12:12:52 +0600

    d

---
+++ Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    float a,b;
+    cin>>a;
+    if(a<0.0 && a>10.0){
+        cout<<"nota invalida"<<endl;
+    }
+
+    
+    return 0;
+
+}
+
+
