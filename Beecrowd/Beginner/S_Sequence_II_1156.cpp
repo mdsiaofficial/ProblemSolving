7e655a0ce90d64aa3bff4f5b06d7e4591376b080
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 11:29:20 +0600

    ffff

--- Beecrowd/Beginner/S_Sequence_II_1156.cpp
+++ Beecrowd/Beginner/S_Sequence_II_1156.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(){
 
-        
+    int sum=0;
+    int div=1;
+    for(int i=1; i<=39; i=i+2){
+        sum = sum + (i/div);
+
+        div = div*2;
+
+    }
+    cout<<sum<<endl;
+    
     return 0;
 
 }
