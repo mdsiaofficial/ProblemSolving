25f44e2992babdbb78e710852db1dcffbb1fe4a6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 12:18:36 +0600

    cc

--- Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
+++ Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
@@ -9,9 +9,39 @@ int main(){
     cin>>a;
     if(a<0.0 && a>10.0){
         cout<<"nota invalida"<<endl;
+        cin>>a;
     }
 
-    
+    cin>>b;
+    if(b<0.0 && b>10.0){
+        cout<<"nota invalida"<<endl;
+        cin>>b;
+    }
+
+    float media = (a+b)/2;
+    cout<<fixed<<setprecision(2)<<"media = "<<media<<endl;
+
+    cout<<"novo calculo (1-sim 2-nao)"<<endl;
+    int x;
+    cin>>x;
+    if(x!=1 && x!=2){
+        cout<<"novo calculo (1-sim 2-nao)"<<endl;
+    }else{
+        cin>>a;
+        if(a<0.0 && a>10.0){
+            cout<<"nota invalida"<<endl;
+            cin>>a;
+        }
+
+        cin>>b;
+        if(b<0.0 && b>10.0){
+            cout<<"nota invalida"<<endl;
+            cin>>b;
+        }
+
+        float media = (a+b)/2;
+        cout<<fixed<<setprecision(2)<<"media = "<<media<<endl;
+    }
     return 0;
 
 }
