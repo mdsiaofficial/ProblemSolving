6064f2feede4338fec93e1f6331090d21baffb80
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 16:30:15 +0600

    ff

--- Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
+++ Beecrowd/Beginner/Several_Scores_with_Validation_1118.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
-//#include<iostream>
+#include<iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -50,7 +51,7 @@ int main(){
 
  */
 
-
+/* 
         // try 2 solvedggg
     float x, y, z, n;
     y = 0;
@@ -89,6 +90,71 @@ int main(){
         if(n == 1) cout << "novo calculo (1-sim 2-nao)n";
                      
     }    
+
+
+
+ */
+
+
+
+
+    float x = -1, y = -1;
+
+    int value = 1;
+
+ 
+
+    while(value == 1)
+
+    {
+
+        do{
+
+            cin >> x;
+
+            if(x < 0 || x > 10)
+
+                cout << "nota invalida" << endl;
+
+ 
+
+        }while(x < 0 || x > 10);
+
+ 
+
+        do{
+
+            cin >> y;
+
+            if(y < 0 || y > 10)
+
+                cout << "nota invalida" << endl;
+
+ 
+
+        }while(y < 0 || y > 10);
+
+ 
+
+        cout << "media = " << fixed << setprecision(2) << ((x + y) / 2) << endl;
+
+ 
+
+        do{
+
+            cout << "novo calculo (1-sim 2-nao)" << endl;
+
+            cin >> value;
+
+ 
+
+        }while(value != 1 && value != 2);
+
+    }
+
+ 
+
+
     return 0;
 
 }
