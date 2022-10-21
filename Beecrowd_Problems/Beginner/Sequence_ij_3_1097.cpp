000078e2f0e7f5b7f03f9d851e976b00215fb3b8
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 09:15:42 +0600

    hhh

--- Beecrowd_Problems/Beginner/Sequence_ij_3_1097.cpp
+++ Beecrowd_Problems/Beginner/Sequence_ij_3_1097.cpp
@@ -6,15 +6,37 @@ int main()
 {
     
     for (int i = 1; i <= 9; i = i + 2)
-    {
-        int j = 7;
+    {   
+        
+        int j=7;
+        if(i==1){
+            
+            j = 7;
+        }else if(i==3){
+            j = j+2;
+
+        }else if(i==5){
+            
+            j = j+2+2;
+        }else if(i==7){
+            j = j+2+2+2;
+
+        }else if(i==9){
+            
+            j = j+2+2+2+2;
+        }
+        
         for (int c = 1; c <= 3; c++)
         {
             
             cout << "I=" << i << " J=" << j << endl;
             j = j - 1;
+
+
             
         }
+
+
         
     }
 
