a51208889801e6770b7b6773144d80d0196d7644
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 09:16:40 +0600

    ff

--- Beecrowd_Problems/Beginner/Sequence_ij_3_1097.cpp
+++ Beecrowd_Problems/Beginner/Sequence_ij_3_1097.cpp
@@ -7,7 +7,8 @@ int main()
     
     for (int i = 1; i <= 9; i = i + 2)
     {   
-        
+        // ei if diya sequence er j er value change korsi
+
         int j=7;
         if(i==1){
             
@@ -26,14 +27,14 @@ int main()
             j = j+2+2+2+2;
         }
         
+        // ei loop e i er value ar j er value serially print korsi
+        
         for (int c = 1; c <= 3; c++)
         {
             
             cout << "I=" << i << " J=" << j << endl;
             j = j - 1;
 
-
-            
         }
 
 
