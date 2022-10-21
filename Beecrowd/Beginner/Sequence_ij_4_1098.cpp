ba5cbf93b21a00d7288cc4b5078faee6be25939a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 23:42:13 +0600

    hhh

--- Beecrowd/Beginner/Sequence_ij_4_1098.cpp
+++ Beecrowd/Beginner/Sequence_ij_4_1098.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-    for(float i=0; i<=2; i=i+0.2){
+    for(float i=0; i<=2.2; i=i+0.2){
         for (float j = 1; j <= 3; j++)
         {
+            
             cout<<"I="<<i<<" J="<<j<<endl;
         }
         
