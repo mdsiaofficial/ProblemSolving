593fb6a9c931da01eee7ae650f6a280a1d7c9432
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-21 23:59:26 +0600

    bbb

--- Beecrowd/Beginner/Sequence_ij_4_1098.cpp
+++ Beecrowd/Beginner/Sequence_ij_4_1098.cpp
@@ -4,16 +4,38 @@ using namespace std;
 
 int main()
 {
+
+
+    // my solution: try 1
+    int j=1;
     for(float i=0; i<=2.2; i=i+0.2){
-        for (float j = 1; j <= 3; j++)
+        
+        for (float z = 1; z <= 3; z++)
         {
             
             //j=j+i;
             
-            cout<<"I="<<i<<" J="<<j<<endl;
+            //cout<<"I="<<i<<" J="<<j+i<<endl;
         }
-        
+
+        cout<<"I="<<i<<" J="<<j+i<<endl;
+        cout<<"I="<<i<<" J="<<j+1+i<<endl;
+        cout<<"I="<<i<<" J="<<j+2+i<<endl;
+    }
+    
+
+/* 
+
+    float i = 0;
+    float j = 1;
+    while(i <= 2.1){
+            cout << "I=" << i << " " << "J=" << j+i <<endl;
+            cout << "I=" << i << " " << "J=" << j+1+i <<endl;
+            cout << "I=" << i << " " << "J=" << j+2+i <<endl;
+            i+= 0.2;
     }
 
+     */
+    
     return 0;
 }
