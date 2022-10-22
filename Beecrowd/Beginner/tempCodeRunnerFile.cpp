7eaa38521b4affd00c8a07e771898bd17c8d0010
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-23 00:01:08 +0600

    d

--- Beecrowd/Beginner/tempCodeRunnerFile.cpp
+++ Beecrowd/Beginner/tempCodeRunnerFile.cpp
@@ -1,18 +1,40 @@
-#include<bits/stdc++.h> // ei library add korle ar kisu add kora lage nah 
-//#include <iomanip> //this library need to use setprecision
+
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    
+// Print the sum of all odd numbers between X and Y.
 
-    int n;
-    
-    cin>>n;
+int main()
+{
 
-    for(int i=1; i<=6; i++){
-        if(n%i==0){
-            cout<<i<<endl;
+    int count;
+    cin>>count;
+
+    for (int i = 0; i < count; i++)
+    {
+        
+        int a,b;
+        cin>>a>>b;
+        int sum=0;
+
+        if(a<b){
+            for(int i=a+1; i<b; i++){
+                if(i%2!=0){
+                    sum=sum+i;
+                }
+            }
+        }else{
+            for(int i=b+1; i<a; i++){
+                if(i%2!=0){
+                    sum=sum+i;
+                }
+            }
         }
+        
+        
+        cout<<sum<<endl;
+
     }
+    
 
     return 0;
 }
