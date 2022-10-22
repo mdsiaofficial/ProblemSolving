a524d5769634fda4ff2ecbccbefb5847cef63df6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-22 23:45:28 +0600

    hh

--- Beecrowd/Beginner/Sum_of_Consecutive_odd_numbers_ii_1099.cpp
+++ Beecrowd/Beginner/Sum_of_Consecutive_odd_numbers_ii_1099.cpp
@@ -1,20 +1,24 @@
 
 #include<bits/stdc++.h>
-
 using namespace std;
 // Print the sum of all odd numbers between X and Y.
-
 
 int main()
 {
 
     int count;
     cin>>count;
+
     for (int i = 0; i < count; i++)
     {
         
         int a,b;
         cin>>a>>b;
+
+        for(int i=a; i<b; i++){
+            
+        }
+
     }
     
 
