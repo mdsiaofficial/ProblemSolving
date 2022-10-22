6af380ff994d102bf44f303c887073c91a55ae0d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-22 23:47:49 +0600

    ggg

--- Beecrowd/Beginner/Sum_of_Consecutive_odd_numbers_ii_1099.cpp
+++ Beecrowd/Beginner/Sum_of_Consecutive_odd_numbers_ii_1099.cpp
@@ -15,8 +15,11 @@ int main()
         int a,b;
         cin>>a>>b;
 
-        for(int i=a; i<b; i++){
-            
+        int sum=0;
+        for(int i=a+1; i<b; i++){
+            if(i%2!=0){
+                sum=sum+i;
+            }
         }
 
     }
