eeae8b31187148de7ec8223e27d14a68f39a0ffa
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-24 01:13:36 +0600

    hhh

---
+++ Toph/Poltu_and_odd_number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <=n; i++)
+    {
+        long long a, b;
+        int odd_sum=0;
+
+
+        cin >> a >> b;
+        for (int j = a; j <= b; j++)
+        {
+            if (j%2!=0)
+            {
+                odd_sum += j;
+            }
+            
+            
+        }
+        cout << "Case " << i << ": " << odd_sum << endl;
+    }
+    
+    return 0;
+}
