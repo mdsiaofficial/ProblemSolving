5394fdbb811fa6f3b9c8e1f8626b0a82ac88464e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-24 01:00:07 +0600

    dd

---
+++ Toph/Missing_Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+
+    int sum;
+    int a, b, c, d;
+
+    cin >> sum;
+    cin >> a >> b >> c;
+
+    d = sum - (a + b + c);
+    cout << d << endl;
+    
+    return 0;
+}
