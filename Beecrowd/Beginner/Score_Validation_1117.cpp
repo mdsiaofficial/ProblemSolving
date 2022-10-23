8f64b276798ebe32ec77616f73a6318ad7823037
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-23 17:04:52 +0600

    hh

--- Beecrowd/Beginner/Score_Validation_1117.cpp
+++ Beecrowd/Beginner/Score_Validation_1117.cpp
@@ -3,6 +3,24 @@ using namespace std;
 int main()
 {
 
+    float number1, number2, c=0;
+    while (1)
+    {
+        cin >> number1;
+        if(number1>= 0.0 && number1<=10.0){
+            cout << "nota invalida" << endl;
+            continue;
+        }
+
+        cin >> number2;
+        if (number2 >= 0.0 && number1 <= 10.0)
+        {
+            cout << "nota invalida" << endl;
+            continue;
+        }
+
+        /* code */
+    }
     
 
     return 0;
