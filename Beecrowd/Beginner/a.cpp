ce4d28d9034a61acdbeffd35a1e83932811d3b61
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-23 22:52:49 +0600

    ddd

---
+++ Beecrowd/Beginner/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    float s = (float)b / (float)a;
+
+    cout << fixed << setprecision(3) << s << endl;
+
+    return 0;
+}
