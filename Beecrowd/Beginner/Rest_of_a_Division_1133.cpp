7fc4c20f63b9b299a88d5085fbce0bc996ccdd49
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-23 13:34:22 +0600

    gggg

--- Beecrowd/Beginner/Rest_of_a_Division_1133.cpp
+++ Beecrowd/Beginner/Rest_of_a_Division_1133.cpp
@@ -6,9 +6,28 @@ using namespace std;
 int main()
 {
 
-    int x,y;
+    int x, y;
 
-    cin>>x>>y;
+    cin>> x >> y;
+    if(x<y){
+
+        for (int i = x + 1; i < y; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                cout << i << endl;
+            }
+        }
+    }else{
+        for (int i = y + 1; i < x; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                cout << i << endl;
+            }
+        }
+    }
+    
     
 
 
