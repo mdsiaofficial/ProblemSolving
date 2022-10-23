af686b186f0010d06eb98372fb9516507470c256
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-24 02:01:57 +0600

    vv

--- Beecrowd/Beginner/Multiples_of_13_1132.cpp
+++ Beecrowd/Beginner/Multiples_of_13_1132.cpp
@@ -8,16 +8,29 @@ int main()
     int a, b;
     cin >> a >> b;
     int sum = 0;
-    for (int i = a; i <= b; i++)
-    {
 
-        if(i%13!=0){
-            sum += i;
+    if(a<b){
+        for (int i = a; i <= b; i++)
+        {
+
+            if (i % 13 != 0)
+            {
+                sum += i;
+            }
         }
+    }else{
+        for (int i = b; i <= a; i++)
+        {
 
+            if (i % 13 != 0)
+            {
+                sum += i;
+            }
+        }
     }
-    cout << sum << endl;
     
+    cout << sum << endl;
+
 
     return 0;
 }
