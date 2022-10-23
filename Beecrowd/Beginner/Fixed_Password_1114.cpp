32685b27346c348f9c4cb297835d75cbdf042b3b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-23 16:29:49 +0600

    hhh

---
+++ Beecrowd/Beginner/Fixed_Password_1114.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+
+    while (1)
+    {
+        cin >> a;
+
+        if (a == 2002)
+        {
+            cout << "Acesso Permitido" 
+            break;
+        }else
+        {
+            cout << "Senha Invalida" << endl;
+        }
+    }
+
+    return 0;
+}
