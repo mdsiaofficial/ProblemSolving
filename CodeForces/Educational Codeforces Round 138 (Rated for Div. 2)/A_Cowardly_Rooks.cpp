9754580a4bf845bd97e1574611f8c8391565381f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-25 08:03:16 +0600

    fff

--- CodeForces/Educational Codeforces Round 138 (Rated for Div. 2)/A_Cowardly_Rooks.cpp
+++ CodeForces/Educational Codeforces Round 138 (Rated for Div. 2)/A_Cowardly_Rooks.cpp
@@ -6,20 +6,23 @@ int main(){
 
     int c;
     cin>>c;
-    int n[c], m[c];
 
-    int x[c],y[c];
+    int n, m;
+
+    int x,y;
 
     for(int i=0; i<c; i++){
 
-        
-        cin>>n[i]>>m[i];
-        cin>>x[i]>>y[i];
+        cin>>n>>m;
+        cin>>x>>y;
 
-        
-        
-
+        if(m<n){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl;
+        }
     }
+    
     return 0;
 }
 
