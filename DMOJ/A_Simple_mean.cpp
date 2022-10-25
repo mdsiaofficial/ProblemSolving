12021c3961d493b13ec44c841cd2cdbc7c8c4df2
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-26 00:23:40 +0600

    ddd

---
+++ DMOJ/A_Simple_mean.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int mean = (a + b + c) / 3;
+    cout << mean;
+    
+    return 0;
+}
