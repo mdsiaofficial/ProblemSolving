ff9ee7009c5d128e203c4a31b632171ccaa7d878
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-26 11:16:54 +0600

    h

--- CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/B.cpp
+++ CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/B.cpp
@@ -7,6 +7,21 @@ using namespace std;
 int main()
 {
 
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int n, m;
+        int ind[n];
+
+        cin >> n >> m;
+        for (int j = 0; j < n; j++)
+        {
+            cin >> ind[j];
+        }
+
+        
+    }
     
 
     return 0;
