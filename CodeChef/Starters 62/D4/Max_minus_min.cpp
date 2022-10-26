0f9972575e12598fa0ac4ffb5249d5b01b6651d7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-26 20:52:43 +0600

    ss

---
+++ CodeChef/Starters 62/D4/Max_minus_min.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int temp = c - a;
+        cout << temp << endl;
+        
+    }
+    
+
+    return 0;
+}
---
+++ CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/F.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+
+    for (int i = 0; i < a; i++){
+        cin >> b;
+
+        
+    }
+
+    return 0;
+}
