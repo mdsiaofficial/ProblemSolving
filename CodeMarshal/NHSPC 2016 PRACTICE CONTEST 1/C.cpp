552ab8a0b1d643ec8349358f714c4e8bb1fb30b9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-26 11:59:41 +0600

    ddd

--- CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/C.cpp
+++ CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/C.cpp
@@ -14,12 +14,12 @@ int main()
     
     for (int i = 1; i <=n; i++)
     {
-        int dia;
+        double dia;
         cin >> dia;
-        float length = sqrt((dia*dia) / 2);
+        double length = sqrt((dia*dia) / 2);
 
-        float area = length * sqrt((dia * dia) - (length * length));
-        cout <<fixed<<setprecision(5)<< "Case :" << i << " " << area << endl;
+        double area = length * sqrt((dia * dia) - (length * length));
+        cout <<fixed<<setprecision(5)<< "Land " << i << ": " << area << endl;
     }
     
     return 0;
