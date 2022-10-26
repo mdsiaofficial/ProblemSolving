5d2b3687f78f200e5b6c3c1eb4bf17783951195d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-26 20:59:07 +0600

    l

---
+++ CodeChef/Starters 62/D4/Test_Score.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+        
+    }
+    
+
+    return 0;
+}
