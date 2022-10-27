d97751e07a8796f0e121c6d7e54679e5d175b353
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-27 22:41:19 +0600

    tt

--- CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/E.cpp
+++ CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/E.cpp
@@ -8,35 +8,7 @@ int main()
 {
     int n;
     cin >> n;
-
-/*     for (int i = 1; i <= n; i++){
-        int x;
-        cin >> x;
-        for (int j = 1; j <= x; j++){
-
-            if(x%j==0)
-        }
-    }
- */
-        /*
-            for (int i = 1; i <=n; i++){
-                long long x;
-                cin >> x;
-                int divisior;
-
-                for (int j = 1; j <=x/2; j++)
-                {
-                    if(x%j==0){
-                        //divisior = x / j;
-                        cout << "Case " << i << ": " << j << endl;
-                    }
-                }
-            }
-
-
-         */
-
-        
+    
     for (int i = 1; i <=n; i++){
         long long x;
         cin >> x;
