83a8f82397fbd98761fb34076ed8e4b8a8b48604
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-27 22:39:31 +0600

    ss

--- CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/E.cpp
+++ CodeMarshal/NHSPC 2016 PRACTICE CONTEST 1/E.cpp
@@ -9,47 +9,56 @@ int main()
     int n;
     cin >> n;
 
-/* 
-    for (int i = 1; i <=n; i++){
-        long long x;
+/*     for (int i = 1; i <= n; i++){
+        int x;
         cin >> x;
-        int divisior;
+        for (int j = 1; j <= x; j++){
 
-        for (int j = 1; j <=x/2; j++)
-        {
-            if(x%j==0){
-                //divisior = x / j;
-                cout << "Case " << i << ": " << j << endl;
-            }
+            if(x%j==0)
         }
     }
-
-
  */
+        /*
+            for (int i = 1; i <=n; i++){
+                long long x;
+                cin >> x;
+                int divisior;
+
+                for (int j = 1; j <=x/2; j++)
+                {
+                    if(x%j==0){
+                        //divisior = x / j;
+                        cout << "Case " << i << ": " << j << endl;
+                    }
+                }
+            }
 
 
+         */
+
+        
     for (int i = 1; i <=n; i++){
         long long x;
         cin >> x;
         int div;
 
         for(int j = 1; j <= x; j++) {
-            if(n % j == 0){
+            if(x % j == 0){
                 //int lel = n % j;
-                //printf("%d\t\n", j); 
-                
+                //printf("%d\t\n", j);
+
                 if(j%2!=0){
                     div = j;
                 }
-                    
+
             }
-            
+
         }
         cout << "Case " << i << ": " << div << endl;
 
     }
 
-
+        
 
     return 0;
 }
