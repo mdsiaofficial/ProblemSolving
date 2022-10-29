8475dace80bac477e557bb4d0371046e2001b0f7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-29 23:23:07 +0600

    gg

--- Beecrowd/Beginner/Squared_and_Cubic_1143.cpp
+++ Beecrowd/Beginner/Squared_and_Cubic_1143.cpp
@@ -8,6 +8,11 @@ int main()
 
     int n;
     cin >> n;
-    
+
+    for (int i = 1; i <= n; i++){
+        cout << i << " " << pow(i, 2) << " " << pow(i, 3) << endl;
+        
+    }
+
     return 0;
 }
