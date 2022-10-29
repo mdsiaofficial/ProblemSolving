f34d32211ec61dbd069b216bd3cf2835a04c3bf9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-30 01:29:25 +0600

    dd

--- Toph/GPA_Calclator.cpp
+++ Toph/GPA_Calclator.cpp
@@ -7,17 +7,18 @@ int main()
 {
 
     int t,n;
-    cin >> t>>n;
+    cin >> t;
     
     
     for (int i = 1; i <= t; i++){
         float total = 0;
+        cin >> n;
         for (int j = 1; i <= n; j++){
             float x;
             cin >> x;
             total = total + x;
         }
-        cout << fixed << setprecision(3) << total / n << endl;
+        cout << fixed << setprecision(3) <<"Case "<<i<<": " total / n << endl;
     }
     
     return 0;
