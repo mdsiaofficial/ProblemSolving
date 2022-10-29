197fceef2673933f3fa2da44cdf119e6aa9f125a
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-29 23:44:44 +0600

    s

--- Beecrowd/Beginner/Logical_Sequence_1144.cpp
+++ Beecrowd/Beginner/Logical_Sequence_1144.cpp
@@ -11,7 +11,7 @@ int main()
 
     for (long long i = 1; i <= n; i++){
         cout << i << " " << pow(i, 2) << " " << pow(i, 3) << endl;
-        cout << i << " " << pow(i, 2)+1 << " " << pow(i, 3)+1 << endl;
+        cout << i << " " << (i*i)+1 << " " << (i*i*i)+1 << endl;
         
     }
 
