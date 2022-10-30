9fc7653e0365dc8c2633bccac0417563537eb0e4
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-30 15:19:36 +0600

    s

--- Toph/NHSPC 2022 Mock/D_Future_Red_Coder.cpp
+++ Toph/NHSPC 2022 Mock/D_Future_Red_Coder.cpp
@@ -6,7 +6,27 @@ using namespace std;
 int main()
 {
 
-    
+    int x;
+    cin >> x;
+    int c = 0;
+    for (int i = 1; i < 1000000; i++){
 
+       //int temp = x / i;
+
+        if(x%i==0){
+            c++;
+        }else if(c>2){
+            break;
+        }else{
+            continue;
+        }
+    }
+    
+    if(c>2){
+        cout << "Well done" << endl;
+    }else{
+        
+        cout << "Never give up" << endl;
+    }
     return 0;
 }
