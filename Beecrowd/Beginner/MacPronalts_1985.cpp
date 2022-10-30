a21baac039bbcdfd8b3e6561af24936354083e74
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-30 23:24:45 +0600

    d

--- Beecrowd/Beginner/MacPronalts_1985.cpp
+++ Beecrowd/Beginner/MacPronalts_1985.cpp
@@ -8,7 +8,33 @@
 using namespace std;
 int main(){
 
+    int n;
+    float p1001 = 1.50;
+    float p1002 = 2.50;
+    float p1003 = 3.50;
+    float p1004 = 4.50;
+    float p1005 = 5.50;
 
-    
+    cin >> n;
+    float sum = 0;
+    for (int i = 1; i <= n; i++){
+        int x, y;
+        cin>>x>>y;
+
+        if(x==1001){
+            sum = sum + (p1001 * y);
+        }else if(x==1002){
+            sum =  sum + (p1002 * y);
+        }else if(x==1003){
+            sum =  sum + (p1003 * y);    
+        }else if(x==1004){
+            sum =  sum + (p1004 * y);
+        }else if(x==1005){
+            sum =  sum + (p1005 * y);
+        }
+
+    }
+    cout << fixed << setprecision(2) << sum << endl;
+
     return 0;
 }
