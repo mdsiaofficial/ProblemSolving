314e5bd920991acc13bfc7c45834a254a094eba1
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-30 22:47:05 +0600

    xx

---
+++ Beecrowd/Beginner/MacPronalts_1985.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+
+#define ll long long
+
+using namespace std;
+int main(){
+
+
+    
+    return 0;
+}
