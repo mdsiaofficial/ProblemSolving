9aa3362303586ee6711129238b1e21d4259c869e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-10-31 22:38:45 +0600

    d

---
+++ Beecrowd/Beginner/Array_Replacement_I_1172.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+
+#define ll long long
+
+using namespace std;
+int main(){
+
+    int x[10];
+    for (int i = 0; i < sizeof(x); i++){
+        cin >> x[i];
+        if (x[i] < 1){
+            x[i] = 1;
+        }
+    }
+
+    for (int j = 0; j < sizeof(x); j++){
+        cout << "X[j] = " << x[j];
+    }
+    return 0;
+}
