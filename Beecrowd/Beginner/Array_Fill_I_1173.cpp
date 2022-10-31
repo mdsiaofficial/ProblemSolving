5f2f6b07b2a8d0769cbab3d21719ffba3669cdc9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-01 00:15:13 +0600

    iii

--- Beecrowd/Beginner/Array_Fill_I_1173.cpp
+++ Beecrowd/Beginner/Array_Fill_I_1173.cpp
@@ -25,9 +25,9 @@ int main(){
     int n;
     cin >> n;
     for(int i = 0; i < 10; i++){
-        cout << "N[" << i << "] = " << n << "n";
+        cout << "N[" << i << "] = " << n << "\n";
         n *= 2;
     }
-    
+
     return 0;
 }
