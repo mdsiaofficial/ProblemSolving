1b114b598c3bb093d9684ad70c37f024943f6eb5
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-01 10:37:55 +0600

    jjj

---
+++ Toph/Leap_Year.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+
+#define ll long long
+
+double sp(int n){
+    return setprecision(n);
+}
+using namespace std;
+int main(){
+
+
+
+    return 0;
+}
