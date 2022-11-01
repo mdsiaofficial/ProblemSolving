ecaf2a873bd285502e4023d63caaf12ab03035ea
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-01 10:55:10 +0600

    d

--- Toph/Leap_Year.cpp
+++ Toph/Leap_Year.cpp
@@ -6,14 +6,27 @@
 #define ll long long
 using namespace std;
 int main(){
-
     int year;
+    //cout << "Enter a year: ";
     cin >> year;
-    if(year%4==0){
-        cout << "Yes" << endl;
-    }else{
-        cout << "No" << endl;
-    }
 
+    // leap year if perfectly divisible by 400
+    if (year % 400 == 0) {
+        cout << "Yes";
+    }
+    // not a leap year if divisible by 100
+    // but not divisible by 400
+    else if (year % 100 == 0) {
+        cout << "No";
+    }
+    // leap year if not divisible by 100
+    // but divisible by 4
+    else if (year % 4 == 0) {
+        cout << "Yes";
+    }
+    // all other years are not leap years
+    else {
+        cout << "No";
+    }
     return 0;
 }
