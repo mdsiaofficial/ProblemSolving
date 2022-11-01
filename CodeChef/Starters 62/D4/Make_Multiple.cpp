b182c3fdf11b2ed0f8d83aec183fa7f0acc99406
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-01 11:42:57 +0600

    e

---
+++ CodeChef/Starters 62/D4/Make_Multiple.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+
+#define ll long long
+using namespace std;
+int main(){
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        int a, b;
+        cin >> a >> b;
+        
+    }
+    return 0;
+}
