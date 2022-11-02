c6c3f234a85ccb2d8017862dab355d8dd0a31753
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-02 12:27:47 +0600

    5

---
+++ CodeForces/Codeforces Round #827 (Div. 4)/B_Increasing.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+
+
+#define ll long long
+#define pi 3.14159
+#define sp(n) setprecision(int (n))
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+    forn(i, n){
+        int tt;
+        forn(j, tt){
+            int arrr[tt];
+            cin >> arrr[j];
+        }
+            
+    }
+    
+    return 0;
+}
