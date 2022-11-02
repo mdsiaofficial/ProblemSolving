48f41a878c79a4259d63e904797ca183cedec2b4
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-02 11:23:50 +0600

    ss

--- Beecrowd/Strings/Encryption_1024.cpp
+++ Beecrowd/Strings/Encryption_1024.cpp
@@ -7,10 +7,16 @@
 #define ll long long
 #define pi 3.14159
 #define sp(n) setprecision(int (n))
+#define forn(i, n) for (int i = 0; i < int(n); i++)
 
 using namespace std;
 int main(){
 
-    
+    int n;
+    cin >> n;
+    forn(i, n){
+        
+        
+    }
     return 0;
 }
