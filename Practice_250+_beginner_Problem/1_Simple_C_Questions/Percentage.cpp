14b0f155bf7034a7bcf7144c1ed8cdaa274a6b59
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-03 16:01:23 +0600

    cc

--- Practice_250+_beginner_Problem/1_Simple_C_Questions/Percentage.cpp
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Percentage.cpp
@@ -4,6 +4,6 @@ int main(){
     float a,b,c,d,e;
     cin>>a>>b>>c>>d>>e;
     float percentage = ((a+b+c+d+e)/500)*100;
-    cout<<percentage<<endl;
+    cout<<percentage<<"%"<<endl;
     return 0;
 }
