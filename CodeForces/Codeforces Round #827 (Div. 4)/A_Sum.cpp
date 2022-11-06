ac10b923e320fc540bc155bf18672a24cc2d381b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-06 23:04:25 +0600

    d

--- CodeForces/Codeforces Round #827 (Div. 4)/A_Sum.cpp
+++ CodeForces/Codeforces Round #827 (Div. 4)/A_Sum.cpp
@@ -3,16 +3,15 @@ using namespace std;
 
 int main(){
     
-    int n;
-    cin>>n;
+    int testcase;
+    cin>>testcase;
 
-    for(int i=1; i<=n; i++){
+    for(int i=0; i<testcase; i++){
         int a,b,c;
         cin>>a>>b>>c;
 
         if(a==(b+c) || b==(a+c) || c==(a+b)){
             cout<<"YES"<<endl;
-
         }else{
             cout<<"NO"<<endl;
         }
