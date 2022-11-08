319dca2a25fba5a8507755ceb12614594481c58e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-09 00:38:14 +0600

    hhhhhh

--- LearnCPP/sdfsdf.cpp
+++ LearnCPP/sdfsdf.cpp
@@ -7,8 +7,11 @@ int main()
 
     // int aaa{10};
     // int df{-300};
-    signed int aaa{10};
-    signed int df{-300};
+    // signed int aaa{10};
+    // signed int df{-300};
+
+    unsigned int aaa{10};
+    unsigned int df{-300};
 
     cout << aaa << endl;
     cout << df << endl;
