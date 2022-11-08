1cbea6498a9bcd30c0351de80b4eec1ff0c14650
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-08 14:17:29 +0600

    dd

---
+++ CodeForces/271A_Beautiful_Year.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+
+
+#define ll long long
+#define pi 3.14159
+#define sp(n) setprecision(int (n))
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+using namespace std;
+int main(){
+    int year;
+    cin>>year;
+
+    int one,two,three,four;
+    one = year % 10;
+    int temp = year / 10;
+
+    two = temp % 10;
+    three 
+    return 0;
+}
