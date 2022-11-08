51b17e5392d0eeeb048ba4bd8ce5ddc7688a279b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-08 23:32:24 +0600

    ss

---
+++ LearnCPP/sdfsdf.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // int aaa{10};
+    // int df{-300};
+    signed int aaa{10};
+    signed int df{-300};
+
+    cout << aaa << endl;
+    cout << df << endl;
+    cout << sizeof(aaa) << endl;
+    cout << sizeof(df) << endl;
+    
+    return 0;
+}
