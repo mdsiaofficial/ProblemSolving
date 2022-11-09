dee9f52afca78a71677aba88d5dad6d384844b95
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-09 11:00:13 +0600

    jjj

--- LearnCPP/sdfsdf.cpp
+++ LearnCPP/sdfsdf.cpp
@@ -10,13 +10,24 @@ int main()
     // signed int aaa{10};
     // signed int df{-300};
 
-    unsigned int aaa{10};
-    unsigned int df{-300};
+    // unsigned int aaa{10};
+    // unsigned int df{-300};
 
-    cout << aaa << endl;
-    cout << df << endl;
-    cout << sizeof(aaa) << endl;
-    cout << sizeof(df) << endl;
+    // cout << aaa << endl;
+    // cout << df << endl;
+    // cout << sizeof(aaa) << endl;
+    // cout << sizeof(df) << endl;
+
+    char charrr6{'a'};
     
+    char charrr5{'a'};
+    
+    char charrr4{'a'};
+    
+    char charrr3{'a'};
+    
+    char charrr2{'a'};
+    
+    char charrr1{'a'};
     return 0;
 }
