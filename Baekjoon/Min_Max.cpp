bba4c2295fdf47afe81bc32e4db67110aae8ece7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-10 12:20:54 +0600

    yy

--- Baekjoon/Min_Max.cpp
+++ Baekjoon/Min_Max.cpp
@@ -6,8 +6,8 @@ int main()
 {
     int t;
     cin >> t;
-    int max = 0;
-    int min = 0;
+    // int max = 0;
+    // int min = 0;
     for (int i = 0; i < t;i++){
         int nn[t];
         cin >> nn[t];
@@ -15,7 +15,7 @@ int main()
     }
     int max = nn[0];
     int min = nn[0];
-    for (int j = 0; i < t;i++){
+    for (int j = 0; j < t;j++){
         if(nn[j]<min){
             min = nn[j];
         }
