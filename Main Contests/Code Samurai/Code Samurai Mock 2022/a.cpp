664da7f308109ccab32302cd17ee87801a766f79
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-15 23:26:24 +0600

    sss

--- Main Contests/Code Samurai/Code Samurai Mock 2022/a.cpp
+++ Main Contests/Code Samurai/Code Samurai Mock 2022/a.cpp
@@ -17,40 +17,39 @@
 
 using namespace std;
 
-string addBigNumbers(string number1, string number2) {
-   if (number1.length() > number2.length())
-    swap(number1, number2);
-   string sum = "";
-   int len1 = number1.length();
-   int len2 = number2.length();
-   int digitDiff = len2 - len1;
-   int carry = 0;
-   int intSum;
-   for (int i=len1-1; i>=0; i--) {
-      intSum = ((number1[i]-'0') + (number2[i+digitDiff]- '0') + carry);
-      sum.push_back(intSum%10 + '0');
-      carry = intSum/10;
-   }
-   for (int i=digitDiff-1; i>=0; i--) {
-      intSum = ((number2[i]-'0')+carry);
-      sum.push_back(intSum%10 + '0');
-      carry = intSum/10;
-   }
-   if (carry)
-    sum.push_back(carry+'0');
-   reverse(sum.begin(), sum.end());
-   return sum;
+string sumof(string number1, string number2) {
+    if (number1.length() > number2.length())
+        swap(number1, number2);
+        
+    string sum = "";
+    int len1 = number1.length();
+    int len2 = number2.length();
+    int digitDiff = len2 - len1;
+    int carry = 0;
+    int intSum;
+    for (int i=len1-1; i>=0; i--) {
+        intSum = ((number1[i]-'0') + (number2[i+digitDiff]- '0') + carry);
+        sum.push_back(intSum%10 + '0');
+        carry = intSum/10;
+    }
+    for (int i=digitDiff-1; i>=0; i--) {
+        intSum = ((number2[i]-'0')+carry);
+        sum.push_back(intSum%10 + '0');
+        carry = intSum/10;
+    }
+    if (carry)
+        sum.push_back(carry+'0');
+    reverse(sum.begin(), sum.end());
+    return sum;
 }
 int main() {
    
-   //string number1 = "259058909701382810435441643464652692076";
-   //string number2 = "75306452065121335498200664981021461578";
-   string number1, number2;
+   //string n1 = "259058909701382810435441643464652692076";
+   //string n2 = "75306452065121335498200664981021461578";
+   string n1, n2;
    char x;
-   cin >> number1 >> x >> number2;
-
-   //cout << "Sum of two large numbers is " << addBigNumbers(number1, number2);
-   cout << addBigNumbers(number1, number2) << endl;
+   cin >> n1 >> x >> n2;
+   cout << sumof(n1, n2) << endl;
    
 
    return 0;
