d7c76201b476ac040aedb80bddbda8f21bacfa2e
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-17 20:46:10 +0600

    dd

---
+++ Toph/DIU Take-Off Programming Contest Fall 2022 Mock/c.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <cstring>
+
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define ll long long
+#define ld long double
+#define fsp(n) fixed<<setprecision(int(n))
+#define sp(n) setprecision(int(n))
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    if(n%3==0){
+
+        cout << "Equilateral" << endl;
+    }else{
+        cout << "Isosceles" << endl;
+    }
+    
+    return 0;
+}
