c0b22cba088e44b8ce3e4a68ca39dc9bdb84067f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-17 13:10:14 +0600

    xx

---
+++ Beecrowd/Strings/LED_1168.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+#include <string>
+
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define ll long long
+#define ld long double
+#define fsp(n) fixed<<setprecision(int(n))
+#define sp(n) setprecision(int(n))
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+    int nums[10]={6,2,5,5,4,5,6,3,7,6}
+    for(int i=0;i<t;i++){
+        string number;
+        cin>>number;
+    }
+
+
+
+    return 0;
+}
