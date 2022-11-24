d4e1f01abd804681f57eae63ddf5e63a96201e33
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-24 16:28:53 +0600

    ss

---
+++ Beecrowd/Beginner/1158.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+#include <string>
+
+#define ll long long
+#define pi 3.14159
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+using namespace std;
+int main(){
+
+    int t;
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+        int x, y;
+        cin >> x >> y;
+
+        int sum = 0;
+        int c = 0;
+        int temp = x;
+        while (true)
+        {
+            if(x%2!=0){
+                sum += x;
+            }
+            if(c==y){
+                break;
+            }
+            c++;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
+
