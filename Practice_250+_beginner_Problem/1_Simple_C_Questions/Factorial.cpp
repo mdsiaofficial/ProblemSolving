ce6512356f541e548d8148b447c151fc2eb13a7b
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-24 12:29:02 +0600

    cc

--- Practice_250+_beginner_Problem/1_Simple_C_Questions/Factorial.cpp
+++ Practice_250+_beginner_Problem/1_Simple_C_Questions/Factorial.cpp
@@ -7,15 +7,15 @@ int main(){
     cin>>a;
     //int f;
     //int i=1;
-
+    long long f=1;
     if(a==0){
         cout<<"No Factorial !!!"<<endl;
     }else{
-        for(long long i=a; i>=1; i--){
-            a=a*i;
+        for(long long i=1; i<=a; i++){
+            f=f*i;
             //a*=i;
         }
-        cout<<a<<endl;
+        cout<<f<<endl;
     }
 /*
     while(i<=a){
