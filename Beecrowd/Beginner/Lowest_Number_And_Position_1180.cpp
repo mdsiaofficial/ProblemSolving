ef0812de4eb8ead8af020dc93ad82825c3ef3630
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-25 17:27:59 +0600

    xx

--- Beecrowd/Beginner/Lowest_Number_And_Position_1180.cpp
+++ Beecrowd/Beginner/Lowest_Number_And_Position_1180.cpp
@@ -23,8 +23,21 @@ int main(){
     int num[t];
     for(int i=0; i<t; i++){
         cin >> num[t];
-        
-
     }
+    lowest = num[0];
+    pos = 0;
+    for(int j=0; j<t; j++){
+
+        for (int k = 0; k < t; k++){
+
+            if(num[j]<num[k]){
+                lowest = num[j];
+                pos = j;
+            }
+        }
+    }
+    cout << "Menor valor: " << lowest << endl;
+    cout << "Posicao: " << pos << endl;
+    
     return 0;
 }
