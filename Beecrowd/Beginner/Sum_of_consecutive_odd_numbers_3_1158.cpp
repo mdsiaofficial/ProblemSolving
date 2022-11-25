0160d6f3f6f15d7950250a3e68ecf3bd8982c332
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-25 11:50:25 +0600

    xxx

--- Beecrowd/Beginner/Sum_of_consecutive_odd_numbers_3_1158.cpp
+++ Beecrowd/Beginner/Sum_of_consecutive_odd_numbers_3_1158.cpp
@@ -11,12 +11,23 @@ int main()
     for (int i = 1; i <= n; i++){
         int x, y;
         cin >> x >> y;
-        int sum;
-        for (int j = x; j <= y; j++){
+
+        int sum=0;
+        int c = 0;
+
+        // for (int j = x; j <= y; j++){
+        //     if(j%2!=0){
+        //         sum =sum + j;
+        //     }
+        // }
+
+        while (c<=y)
+        {
             if(j%2!=0){
                 sum =sum + j;
             }
         }
+        
         cout << sum << endl;
     }
         return 0;
