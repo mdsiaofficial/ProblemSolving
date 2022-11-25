936148d99d42157b2e6f8f7d1eae25eb0b993aff
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-25 12:55:38 +0600

    aa

---
+++ Beecrowd/Beginner/Prime_Number_1165.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+using namespace std;
+int main(){
+
+    ll t;
+    cin >> t;
+    for(ll i=0; i<t; i++){
+        ll x;
+        cin >> x;
+
+        if(x<=1){
+            cout << x<<" nao eh primo" << endl;
+        }
+        
+        if(x%(x+1)!=0){
+            cout << x<<" nao eh primo" << endl;
+        }else{
+            cout << x<<" eh primo" << endl;
+        }
+        
+        
+    }
+    return 0;
+}
+
