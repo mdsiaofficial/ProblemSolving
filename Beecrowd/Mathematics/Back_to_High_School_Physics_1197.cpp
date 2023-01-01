afdf6670f4f1c5b20e80a45c3c86fccb7ca8d802
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-01 22:29:02 +0600

    jjj

---
+++ Beecrowd/Mathematics/Back_to_High_School_Physics_1197.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+using namespace std;
+
+int main(){
+    int v,t;
+    while (cin>>v>>t)
+    {
+        cout<<(v*t)*2<<endl;
+    }
+/* 
+    while (EOF)
+    {
+        cin>>v>>t;
+        cout<<(v*t)*2<<endl;
+    }
+ */    
+    return 0;
+}
---
+++ Beecrowd/Mathematics/Hasmat_the_brave_warrior_1198.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+using namespace std;
+int main(){
+    int a,b;
+    
+    return 0;
+}
