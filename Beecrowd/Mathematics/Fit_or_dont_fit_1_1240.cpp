7a2cdf120df17e6afbcb9de99dcae84257a05344
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-01 23:54:32 +0600

    ;

--- Beecrowd/Mathematics/Fit_or_dont_fit_1_1240.cpp
+++ Beecrowd/Mathematics/Fit_or_dont_fit_1_1240.cpp
@@ -15,10 +15,14 @@
 
 using namespace std;
 int main(){
-    ll a,b;
-    while (cin>>a>>b)
-    {
-        cout<< abs(a-b) <<endl;
+
+    int t;
+    cin>>t;
+
+    for(int i=0; i<t; i++){
+        int a,b;
+        cin>>a>>b;
+        
     }
     return 0;
 }
