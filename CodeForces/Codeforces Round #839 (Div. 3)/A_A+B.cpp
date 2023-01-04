f7539030bc1480a355c0f1b3b5d469e044473016
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-04 11:54:40 +0600

    dd

---
+++ CodeForces/Codeforces Round #839 (Div. 3)/A_A+B.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
+#include <iostream>
+#include <iomanip>//this library need to use setprecision
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    forn(i, t){
+        int a,b;
+        char x;
+        cin>>a>>x>>b;
+
+        cout<<a+b<<endl;
+    }
+    
+    return 0;
+}
