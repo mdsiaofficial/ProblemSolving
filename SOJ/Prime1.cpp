03756b47bd80db60c49ad6727b9ce2f1987acaef
mdsiaofficial <mdsiaofficial@gmail.com>
2023-02-01 10:32:59 +0600

    mm

--- SOJ/Prime1.cpp
+++ SOJ/Prime1.cpp
@@ -16,33 +16,16 @@
 #define s(n) setprecision(int(n))
 using namespace std;
 
+int prime(int n){
 
-ll n=90000000;
-bool p[90000001];
-vector<ll>pp;
-void sieve(){
-    for(ll i=2; i<=sqrt(n); i++){
-        if(p[i]==false){
-            for(ll j=i+i; j<=n; j+=i){
-                p[j]=true;
-            }
-        }
-    }
-    for(ll i=2; i<=n; i++){
-        if(p[i]==false){
-            pp.push_back(i);
-        }
-    }
+    
 }
-
 int main(){
-    sieve();
+    
     ll t;
     cin>>t;
     for(ll i=0; i<t; i++){
-        ll x;
-        cin>>x;
-        cout<<pp[x-1]<<endl;
+
     }
 
     return 0;
--- Toph/proper_leap_year.cpp
+++ Toph/proper_leap_year.cpp
@@ -18,29 +18,20 @@ using namespace std;
 int main(){
     int year;
     cin>>year;
-/*
-    if((y%400==0 || y%4==0) && y%100!=0){
-        cout<<"Yes"<<endl;
-    }else{
-        cout<<"No"<<endl;
-    }
-*/
+
     if (year % 400 == 0) {
         printf("Yes");
-    }
-    // not a leap year if divisible by 100
-    // but not divisible by 400
-    else if (year % 100 == 0) {
+    }else if (year % 100 == 0) {
         printf("No");
-    }
-    // leap year if not divisible by 100
-    // but divisible by 4
-    else if (year % 4 == 0) {
+        // not a leap year if divisible by 100
+        // but not divisible by 400
+    }else if (year % 4 == 0) {
         printf("Yes");
-    }
-    // all other years are not leap years
-    else {
+        // leap year if not divisible by 100
+        // but divisible by 4
+    }else {
         printf("No");
+        // all other years are not leap years
     }
     return 0;
 }
