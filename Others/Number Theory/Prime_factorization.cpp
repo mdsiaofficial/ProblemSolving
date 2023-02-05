16959eeab7c680584d89e55b04c6dd6a3d6cca58
mdsiaofficial <mdsiaofficial@gmail.com>
2023-02-05 17:49:22 +0600

    ffff

---
+++ Others/Number Theory/Prime_factorization.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void prime_factorization(int n){
+    
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0){
+            int c=0;
+            while (n%i==0)
+            {
+                c++;
+                n=n/i;
+            }
+            cout<<i<<"^"<<c<<" ";
+        }
+    }
+    if(n>1) cout<<n<<"^"<<1<<endl;
+}
+int main(){
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        prime_factorization(n);
+    }
+    return 0;
+
+}
