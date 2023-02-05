59d6c1ac3e4086cdc0c1f26e9ab5c7dcadcf559e
mdsiaofficial <mdsiaofficial@gmail.com>
2023-02-05 22:28:33 +0600

    ddd

---
+++ CodeForces/742A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define llu unsigned long long
+#define mod 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+using namespace std;
+
+// binary exponantiatioin to solve this.
+
+int last_digit_of_pwr(int b, int p){
+
+    int r=1;
+    while (p)
+    {
+        if(p%2==0){
+            b=b*b;
+            p=p/2;
+        }else{
+            r=r*b;
+            p=p-1;
+        }
+
+    }
+    
+    return r%10;
+}
+
+int main(int argc, char *argv[])
+{
+    int p;
+    cin>>p;
+    cout<<last_digit_of_pwr(1378, p)<<endl;
+	return 0;
+}
