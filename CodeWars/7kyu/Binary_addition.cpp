f1646a892efdb7c696092bdf9a338e0a998f6a95
mdsiaofficial <mdsiaofficial@gmail.com>
2023-02-16 13:23:51 +0600

    jj

--- CodeWars/7kyu/Binary_addition.cpp
+++ CodeWars/7kyu/Binary_addition.cpp
@@ -1,8 +1,18 @@
 #include<bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
 
 
 int main() {
-
+    vector<int>binary;
+    int n=2;
+    while(n){
+        binary.push_back(n%2);
+        n=n/2;
+    }
+    for(auto i:binary){
+        cout<<i;
+    }
+    cout<<endl;
     return 0;
 }
