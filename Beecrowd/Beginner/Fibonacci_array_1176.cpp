ba4c9907faebf358399f92b74fe5ce5fcb4784ed
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-02 12:40:07 +0600

    ddd

---
+++ Beecrowd/Beginner/Fibonacci_array_1176.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+
+vector<ull>fibos;
+void fibo(ull n){
+    ull f=0;
+    ull s=1;
+    fibos.push_back(f);
+    fibos.push_back(s);
+    while (n--)
+    {
+        ull nt=f+s;
+        fibos.push_back(nt);
+        f=s;
+        s=nt;
+    }
+}
+int main()
+{
+    unsigned long long t;
+    cin>>t;
+    while (t--)
+    {
+        ull n;
+        cin>>n;
+        fibo(n);
+        cout<<fibos[n]<<endl;
+
+    }
+    return 0;
+}
+
+
+
+
+
+
