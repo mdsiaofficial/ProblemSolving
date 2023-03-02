d4ee786e03206b3f2c4cc426d1299ec78d3de55b
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-03 00:33:09 +0600

    sss

--- Beecrowd/Mathematics/Fit_or_dont_fit_1_1240.cpp
+++ Beecrowd/Mathematics/Fit_or_dont_fit_1_1240.cpp
@@ -22,6 +22,19 @@ int main(){
     for(int i=0; i<t; i++){
         int a,b;
         cin>>a>>b;
+
+        if(a>b){
+            for(int j=0; j<sizeof(b); j++){
+                if((a%10)==(b%10)){
+                    a=a/10;
+                    b=b/10;
+                }else{
+                    
+                }
+            }
+        }else{
+            cout<<"nao encaixa"<<endl;
+        }
         
     }
     return 0;
