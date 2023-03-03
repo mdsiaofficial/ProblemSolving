b42e2d342e1b94cf580604c22e1e703f36d3cc7a
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-03 12:44:08 +0600

    vvvv

--- CodeForces/Codeforces Round #855 (Div. 3)/a.cpp
+++ CodeForces/Codeforces Round #855 (Div. 3)/a.cpp
@@ -2,31 +2,11 @@
 using namespace std;
 #define ull unsigned long long
 
-bool check_meow(string meow){
-    int c=0;
-    string mw = strlwr(meow);
-    for(int i=0; i<meow.length(); i++){
-        if(meow[i]=='m' || meow[i]=='M'){
-            c++;
-        }
-    }
+void check_meow(string meow){
+    
 
 }
 int main()
 {
-
-    int t;
-    cin>>t;
-    while (t--)
-    {
-        int n;
-        cin>>n;
-        string meow;
-        cin>>meow;
-
-
-    }
-    
-    
     return 0;
 }
