e0122ee35f6203221a9f9f2df3c1960700e7454a
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-03 11:46:41 +0600

    jj

--- CodeForces/Codeforces Round #855 (Div. 3)/a.cpp
+++ CodeForces/Codeforces Round #855 (Div. 3)/a.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 bool check_meow(string meow){
     int c=0;
+    string mw = strlwr(meow);
     for(int i=0; i<meow.length(); i++){
         if(meow[i]=='m' || meow[i]=='M'){
             c++;
