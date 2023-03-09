6d3dbc4b11fe4914d7c0ea02e19de967c9d5ec06
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-09 11:53:54 +0600

    aaa

---
+++ Beecrowd/Mathematics/The_trip_1220.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    long long n;
+    cin>>n;
+    vector<double>costs;
+    while (n--)
+    {
+        int x;
+        cin>>x;
+        costs.push_back(x);
+    }
+    
+    
+    
+    return 0;
+}
