034581375c2cae9d90261ce00d396e20e44d1e57
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-10 10:43:18 +0600

    breakstick

---
+++ CodeChef/Break_the_stick.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        // soln 1:
+        // If x is odd, we can always find a solution 
+        // (for example we may split the stick to 1-length sticks)
+        // If x is even, n should also be an even number. 
+        // (we always face at least one even length stick no matter how we split)
+        
+        
+        // n - x = y
+        // even-even= even, satisfied
+        // odd-even= odd, not satisfied
+        // even-odd= odd, satisfied
+        // odd-odd= even= odd+odd, satisfied
+        
+        if(x%2==0){
+            if(n%2==0) cout<<"YES\n";
+            else cout<<"NO\n";
+        }else{
+            cout<<"YES"<<endl;
+        }
+
+        
+    }
+
+    return 0;
+}
+
+
