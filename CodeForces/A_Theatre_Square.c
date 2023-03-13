3c8401f224f4007f982011d007d78c739f236829
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-14 00:03:11 +0600

    aaa

---
+++ CodeForces/A_Theatre_Square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double n, m, a;
+    scanf("%lf %lf %lf", &n, &m, &a);
+    unsigned long long int s=ceil(n/a)*ceil(m/a);
+    printf("%.llu\n", s);
+
+    return 0;
+}
