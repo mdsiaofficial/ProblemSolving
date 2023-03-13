40769ff8a02bba4dfbf84ab6b2bc92a16c071d37
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-14 01:34:28 +0600

    aaa

---
+++ CodeForces/A_Domino_piling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define ll long long
+#define ld long double
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+using namespace std;
+int main(){
+    ll m,n;
+    cin>>m>>n;
+    
+    
+    return 0;
+}
