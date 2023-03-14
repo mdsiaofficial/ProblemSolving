0c3c1b46c42457daf53e0117ec02280ec68e041b
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-14 16:10:23 +0600

    aaa

--- CodeForces/A_Theatre_Square.cpp
+++ CodeForces/A_Theatre_Square.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main(){
     double n, m, a;
     cin>>n>>m>>a;
-    cout<<fixed<<setprecision(0)<<ceil(n/a)*ceil(m/a);
+    cout<<fs(0)<<ceil(n/a)*ceil(m/a);
 
     return 0;
 }
