da1e051e9f914bc7057c135ed2f027b3354add97
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-15 13:53:14 +0600

    aaa

---
+++ CodeForces/281A_Word_Capitalization.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define llu unsigned long long
+#define mod 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+#define el endl
+using namespace std;
+
+int main()
+{
+    string word;
+    cin>>word;
+    word[0]=toupper(word[0]);;
+    cout<<word<<el;
+	return 0;
+}
