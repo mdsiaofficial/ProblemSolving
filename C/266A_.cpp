54abfa83cec17508eecf994542ffda56150f60da
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-21 23:04:03 +0600

    aaa

--- C/266A_.cpp
+++ C/266A_.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 int main(){
+/* 
     long long n;
     string stone;
     cin>>n>>stone;
@@ -13,5 +14,29 @@ int main(){
 		}		
 	}
     cout<<count<<endl;
+     */
+
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int r=0;
+    int g=0;
+    int b=0;
+    for(int i=1;i<n;i++){
+        if(s[i-1]==s[i]){
+            if(s[i]=='R'){
+                r++;
+            }else if(s[i]=='G'){
+                g++;
+            }
+            else if(s[i]=='B'){
+                b++;
+            }
+        }
+    }
+    cout<<r+g+b<<endl;
+
     return 0;    
 }
+
