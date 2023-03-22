f87ddcb769ac75f35a26c1641563ad89bc7f6046
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-22 14:34:08 +0600

    aa

---
+++ HackerRank/Minimum_Height_Triangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define ll long long
+#define ld long double
+#define llu unsigned long long
+#define mod 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+#define el endl
+using namespace std;
+
+int main()
+{
+    double b, a;
+    cin>>b>>a;
+    long long x= ceil((2*a)/b);
+    cout<<ceil((2*a)/b)<<endl;
+	return 0;
+}
