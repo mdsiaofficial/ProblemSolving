c7411f2a9619d5bed1521c51da30d82e876c0f92
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-27 17:52:26 +0600

    aa

---
+++ CodeWars/8kyu/reverssed_words.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+#define ll long long
+
+string str_in(string str){
+    ll l=str.length();
+    string newstr;
+    while (l--)
+    {
+        if(str[])
+        
+        
+    }
+    
+
+}
+using namespace std;
+int main(){
+
+    return 0;
+}
