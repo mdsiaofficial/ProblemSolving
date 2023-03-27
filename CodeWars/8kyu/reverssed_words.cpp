399d2bb18878f4df5ed69f8c0dab6169a2804d6b
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-27 17:53:25 +0600

    aaaa

--- CodeWars/8kyu/reverssed_words.cpp
+++ CodeWars/8kyu/reverssed_words.cpp
@@ -1,21 +1,44 @@
-#include<bits/stdc++.h>
-
-#define ll long long
-
-string str_in(string str){
-    ll l=str.length();
-    string newstr;
-    while (l--)
-    {
-        if(str[])
-        
-        
-    }
-    
-
-}
+// C++ program to reverse a string
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
 
-    return 0;
+// Function to reverse words*/
+void reverseWords(string s)
+{
+	// temporary vector to store all words
+	vector<string> tmp;
+	string str = "";
+	for (int i = 0; i < s.length(); i++) {
+
+		// Check if we encounter space
+		// push word(str) to vector
+		// and make str NULL
+		if (s[i] == ' ') {
+			tmp.push_back(str);
+			str = "";
+		}
+
+		// Else add character to
+		// str to form current word
+		else
+			str += s[i];
+	}
+
+	// Last word remaining,add it to vector
+	tmp.push_back(str);
+
+	// Now print from last to first in vector
+	int i;
+	for (i = tmp.size() - 1; i > 0; i--)
+		cout << tmp[i] << " ";
+	// Last word remaining,print it
+	cout << tmp[0] << endl;
+}
+
+// Driver Code
+int main()
+{
+	string s = "i like this program very much";
+	reverseWords(s);
+	return 0;
 }
