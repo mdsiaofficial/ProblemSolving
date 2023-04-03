3845b67d1b468a26299767038bfb0ff81a3ef650
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-03 23:35:48 +0600

    pari nah

---
+++ CodeForces/Grp_100_Easy_Problems_Bootcamp/Day1/P06_Helpfull_maths.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+
+    string n;
+    cin>>n;
+    vector<int>r;
+    string ne;
+    vector<char>c;
+    for(int i=0; i<n.length(); i++){
+        if(n[i]>47 && n[i]<57){
+            r.push_back(n[i]);
+        }
+    }
+    sort(r.begin(), r.end());
+    for(auto i:r){
+        c.push_back(char(i));
+        if((n[i+1])!='\0') c.push_back('+');
+    }
+    for(auto i:c){
+        cout<<i;
+    }
+    return 0;
+}
