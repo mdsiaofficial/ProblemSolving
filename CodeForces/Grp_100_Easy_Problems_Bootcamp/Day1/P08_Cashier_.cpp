1cc93fe20b3e1bbc0a28d197d62beb92fcd166e4
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-07 01:08:42 +0600

    new 8

---
+++ CodeForces/Grp_100_Easy_Problems_Bootcamp/Day1/P08_Cashier_.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long l,n,a;
+    cin>>n>>l>>a;
+
+    while (n--)
+    {
+        
+        /* code */
+    }
+    
+    return 0;
+}
