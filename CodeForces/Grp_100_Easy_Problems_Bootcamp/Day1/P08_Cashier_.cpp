a08b6046b76f2a84f5d4eda41271f130ba061038
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-07 12:54:49 +0600

    solve. but bujhinaika

--- CodeForces/Grp_100_Easy_Problems_Bootcamp/Day1/P08_Cashier_.cpp
+++ CodeForces/Grp_100_Easy_Problems_Bootcamp/Day1/P08_Cashier_.cpp
@@ -3,12 +3,15 @@ using namespace std;
 int main(){
     long long l,n,a;
     cin>>n>>l>>a;
-
-    while (n--)
+    int t[n];
+    int lt[n];
+    int total=l;
+    for (int i = 0; i < n; i++)
     {
-        
-        /* code */
+        cin>>t[i]>>lt[i];
+        total=total-lt[i];
     }
-    
+    cout<<total/a;
+
     return 0;
 }
