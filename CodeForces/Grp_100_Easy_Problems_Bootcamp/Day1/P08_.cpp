740e60e5632271619b2fd3caff1ceb749831768a
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-07 12:55:38 +0600

    ss

--- CodeForces/Grp_100_Easy_Problems_Bootcamp/Day1/P08_.cpp
+++ CodeForces/Grp_100_Easy_Problems_Bootcamp/Day1/P08_.cpp
@@ -4,43 +4,43 @@
 using namespace std;
 
 int main() {
-  // Read input values
-  int n, L, a;
-  cin >> n >> L >> a;
-  
-  // Create vector of pairs to store arrival times and service times
-  vector<pair<int,int>> customers(n);
-  
-  // Read input values for each customer
-  for (int i = 0; i < n; i++) {
-    cin >> customers[i].first >> customers[i].second;
-  }
-  
-  // Sort vector by arrival times
-  sort(customers.begin(), customers.end());
-  
-  // Initialize current time and number of breaks
-  int curr_time = 0;
-  int breaks = 0;
-  
-  // Loop through customers
-  for (int i = 0; i < n; i++) {
-    // If current time is less than arrival time, take breaks
-    if (curr_time < customers[i].first) {
-      breaks += (customers[i].first - curr_time) / a;
-      curr_time = customers[i].first;
+    // Read input values
+    int n, L, a;
+    cin >> n >> L >> a;
+    
+    // Create vector of pairs to store arrival times and service times
+    vector<pair<int,int>> customers(n);
+    
+    // Read input values for each customer
+    for (int i = 0; i < n; i++) {
+      cin >> customers[i].first >> customers[i].second;
     }
-    // Add service time to current time
-    curr_time += customers[i].second;
-  }
-  
-  // Check remaining time and take breaks
-  if (curr_time < L) {
-    breaks += (L - curr_time) / a;
-  }
-  
-  // Print number of breaks
-  cout << breaks << endl;
-  
-  return 0;
+    
+    // Sort vector by arrival times
+    sort(customers.begin(), customers.end());
+    
+    // Initialize current time and number of breaks
+    int curr_time = 0;
+    int breaks = 0;
+    
+    // Loop through customers
+    for (int i = 0; i < n; i++) {
+      // If current time is less than arrival time, take breaks
+      if (curr_time < customers[i].first) {
+        breaks += (customers[i].first - curr_time) / a;
+        curr_time = customers[i].first;
+      }
+      // Add service time to current time
+      curr_time += customers[i].second;
+    }
+    
+    // Check remaining time and take breaks
+    if (curr_time < L) {
+      breaks += (L - curr_time) / a;
+    }
+    
+    // Print number of breaks
+    cout << breaks << endl;
+    
+    return 0;
 }
