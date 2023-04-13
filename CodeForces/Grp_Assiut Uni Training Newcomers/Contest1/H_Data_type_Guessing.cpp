dc3884d1d1403de2a17fa49e8b916c8489d539b1
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-13 18:08:13 +0600

    unsolved

---
+++ CodeForces/Grp_Assiut Uni Training Newcomers/Contest1/H_Data_type_Guessing.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    auto sum = (a*b)/c;
+    if((a*b)/c < 2147483647) cout<<"int"<<endl;
+    else if((a*b)/c >)
+    else if(((a*b)/c)%2!=0) cout<<"double"<<endl;
+    else
+
+    return 0;
+}
