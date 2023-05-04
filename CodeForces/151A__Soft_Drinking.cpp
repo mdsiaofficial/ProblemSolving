7e4aee6531e55616903eb750240f87878945b5df
mdsiaofficial <mdsiaofficial@gmail.com>
2023-05-04 17:01:47 +0600

    DONE

---
+++ CodeForces/151A__Soft_Drinking.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, k, l, c, d, p, nl, np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+
+    int td = k*l;
+    int ts = c*d;
+    int count=0;
+    while (td>=nl && ts>=1 && p>=np)
+    {
+        td = td - nl;
+        ts = ts - 1;
+        p = p - np;
+        count++;
+    }
+    cout<<count/n<<endl;
+    
+}
