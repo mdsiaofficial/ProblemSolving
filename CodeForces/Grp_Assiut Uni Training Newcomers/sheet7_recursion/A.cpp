3fd6e17f8ecf7842c946d33889c67aed2833e22d
mdsiaofficial <mdsiaofficial@gmail.com>
2023-05-08 22:34:40 +0600

    love recursion

---
+++ CodeForces/Grp_Assiut Uni Training Newcomers/sheet7_recursion/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void x(int n){
+    if(n==0) return;
+    cout<<"I love recursion\n"; x(n-1);
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    x(n);
+}
