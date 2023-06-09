d58c0ffa61684927e13b96c39929a946fc891f5f
mdsiaofficial <mdsiaofficial@gmail.com>
2023-06-09 09:57:55 +0600

    aa

---
+++ UVa/p11614.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        long long x=1;
+        long long c=0;
+        long long y=1;
+        for(int i = 1 i<=11; i++){
+            for(int j=1; j<y; j++){
+                y=y+i;
+            }
+        }
+    }
+}
