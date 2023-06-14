dce9654794f356d6672ff881fce63dc6a72e949a
mdsiaofficial <mdsiaofficial@gmail.com>
2023-06-15 00:02:18 +0600

    aa

---
+++ UVa/p11614_new.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        int c=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+
+            }
+            
+        }
+    }
+    return 0;
+}
