f58d9d1508d49eac8f6a2b5f54109740c6e1b0ff
mdsiaofficial <mdsiaofficial@gmail.com>
2023-06-14 23:11:23 +0600

    aaa

--- UVa/p12578.cpp
+++ UVa/p12578.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
+
 int main() {
     int t;
     cin>>t;
     while(t--){
-        int n;
+        double n;
         cin>>n;
 
         double l = n;
         double w = n*0.6;
-
         double a = l*w;
 
+
+        double pi = acos(-1);
         
+        double r = n/5;
+        double aa = pi * r * r;
+        
+        cout<<fixed<<setprecision(2)<<aa<<" "<<a-aa<<endl;
     }
     return 0;
 }
