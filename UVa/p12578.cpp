e629f3d797c7a4c978157ee539e5a573dcb71d73
mdsiaofficial <mdsiaofficial@gmail.com>
2023-06-14 23:03:55 +0600

    aaa

---
+++ UVa/p12578.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        double l = n;
+        double w = n*0.6;
+
+        double a = l*w;
+
+        
+    }
+    return 0;
+}
