a30cf8781ec0ddbdd3aaa0821e5b2713b40d11ca
mdsiaofficial <mdsiaofficial@gmail.com>
2023-09-20 09:00:35 +0600

    nn

---
+++ Beecrowd/Beginner/Fast_Fibonacci_2164.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+// solved
+int main() {
+    
+    long double n;
+    cin>>n;
+    long double sp = pow(((1 + sqrt(5))/2),n);
+    long double sm = pow(((1 - sqrt(5))/2),n);
+    long double fibo = (sp-sm)/sqrt(5);
+    long double f = (double)fibo;
+    cout<<fixed<<setprecision(1)<<f<<endl;
+
+    return 0;
+}
---
+++ Beecrowd/Beginner/Twitting_2165.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+// solved
+int main() {
+    string t;
+    cin>>t;
+    cout<<t.size();
+    return 0;
+}
