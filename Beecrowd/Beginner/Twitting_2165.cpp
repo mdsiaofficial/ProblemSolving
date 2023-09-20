5336e239b6e021bd8f5b2630661d5fa50eff4f47
mdsiaofficial <mdsiaofficial@gmail.com>
2023-09-20 13:14:02 +0600

    bb

--- Beecrowd/Beginner/Twitting_2165.cpp
+++ Beecrowd/Beginner/Twitting_2165.cpp
@@ -5,7 +5,12 @@ using namespace std;
 // solved
 int main() {
     string t;
-    cin>>t;
+    //cin>>t;
+    //gets(t);
+    getline (cin, t);
     cout<<t.size();
+    cout<<t.length();
+    if(t.size()<=140) cout<<"TWEET"<<endl;
+    else cout<<"MUTE"<<endl;
     return 0;
 }
