b21954e26d45daf4255aa108946cc231cdc06093
mdsiaofficial <mdsiaofficial@gmail.com>
2023-09-28 11:28:48 +0600

    a

--- CodeForces/116A_Tram.cpp
+++ CodeForces/116A_Tram.cpp
@@ -2,44 +2,19 @@
 using namespace std;
 int main()
 {
-	int n,t,ans=-1,d,a,b,i;
-	cin>>n;
-	cin>>a>>b;
-	t=b;
 
-	for(i=1; i<n; i++){
-		ans=max(ans,t);
-		cin>>a>>b;
-		t=t-a+b;
-	}
+	// int n,t,ans=-1,d,a,b,i;
+	// cin>>n;
+	// cin>>a>>b;
+	// t=b;
 
-	cout<<ans;
+	// for(i=1; i<n; i++){
+	// 	ans=max(ans,t);
+	// 	cin>>a>>b;
+	// 	t=t-a+b;
+	// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	// cout<<ans;
 
 
     int n;
