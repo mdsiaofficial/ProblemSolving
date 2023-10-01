6f9a3e535b813d55d47aee445411d08322367f53
Md Shoriful Islam <mdsiaofficial@gmail.com>
2023-10-01 23:57:29 +0600

    aaa

--- CodeForces/ACMSGURU/105_Div_3.cpp
+++ CodeForces/ACMSGURU/105_Div_3.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+int countDivisibleBy3(int N) {
+    int count = 0;
+    int currentNumber = 0;
+
+    for (int i = 1; i <= N; i++) {
+        currentNumber = currentNumber * 10 + i;
+        if (currentNumber % 3 == 0) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 int main() {
-    
+    int N;
+    cin >> N;
+
+    int result = countDivisibleBy3(N);
+    cout << result << endl;
 
     return 0;
 }
