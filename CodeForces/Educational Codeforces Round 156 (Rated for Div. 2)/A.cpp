a050f9f8bdfe9eee795bdaa9191de5c01a81a91c
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-09 22:16:56 +0600

    a

---
+++ CodeForces/Educational Codeforces Round 156 (Rated for Div. 2)/A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        if (n < 3) {
+            cout << "NO" << endl;
+        } else if (n % 3 == 0) {
+            cout << "YES" << endl;
+            cout << "1 2 " << n - 3 << endl;
+        } else {
+            cout << "YES" << endl;
+            cout << "1 1 " << n - 2 << endl;
+        }
+    }
+    
+    return 0;
+}
