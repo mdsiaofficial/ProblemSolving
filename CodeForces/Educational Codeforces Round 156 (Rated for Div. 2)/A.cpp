0dee0b665a795a6858c59bc8510829e6eaea4b4b
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-09 22:39:25 +0600

    aa

--- CodeForces/Educational Codeforces Round 156 (Rated for Div. 2)/A.cpp
+++ CodeForces/Educational Codeforces Round 156 (Rated for Div. 2)/A.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+vector<int> find_triplet(int n) {
+    if (n < 3) {
+        return vector<int>();
+    }
+
+    // Try to find a triplet that sums to n and none of the numbers are divisible by 3
+    for (int x = 1; x < n - 1; ++x) {
+        for (int y = 1; y < n - x; ++y) {
+            int z = n - x - y;
+            if (x % 3 != 0 && y % 3 != 0 && z % 3 != 0 && x != y && x != z && y != z) {
+                return {x, y, z};
+            }
+        }
+    }
+    return vector<int>();
+}
 
 int main() {
     int t;
     cin >> t;
-    
-    while (t--) {
+
+    for (int i = 0; i < t; ++i) {
         int n;
         cin >> n;
-        
-        if (n < 3) {
-            cout << "NO" << endl;
-        } else if (n % 3 == 0) {
+        vector<int> triplet = find_triplet(n);
+
+        if (!triplet.empty()) {
             cout << "YES" << endl;
-            cout << "1 2 " << n - 3 << endl;
+            for (int j : triplet) {
+                cout << j << ' ';
+            }
+            cout << endl;
         } else {
-            cout << "YES" << endl;
-            cout << "1 2 " << n - 3 << endl;
+            cout << "NO" << endl;
         }
     }
-    
+
     return 0;
 }
