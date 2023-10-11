cdff6c78fab58f7f7eb9a503d44637d7e49a5474
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-11 17:12:48 +0600

    aaa

--- CodeForces/271A_Beautiful_Year.cpp
+++ CodeForces/271A_Beautiful_Year.cpp
@@ -1,8 +1,4 @@
-#include<bits/stdc++.h>// ei library add korle ar kisu add kora lage nah 
-#include <iostream>
-#include <iomanip>//this library need to use setprecision
-#include <cmath>
-
+#include<bits/stdc++.h>
 
 #define ll long long
 #define pi 3.14159
@@ -14,19 +10,21 @@ int main(){
     int year;
     cin>>year;
 
-    int one,two,three,four;
-    one = year % 10;
-    int temp = year / 10;
+    
+    while(1){
+        year++;
+        int one,two,three,four;
+        one = year % 10;
+        int temp = year / 10;
 
-    two = temp % 10;
-    temp = temp / 10;
-    three = temp % 10;
-    temp = temp / 10;
-    four = temp % 10;
-
-    for (int i = 0; i < 4; i++){
-        if()
+        two = temp % 10;
+        temp = temp / 10;
+        three = temp % 10;
+        temp = temp / 10;
+        four = temp % 10;
+        if(one!=two && one!=three && one!=four && two!=three && two!=four && three!=four) break;
     }
-        cout << one << two << three << four;
+    cout<<year<<endl;
+    
     return 0;
 }
