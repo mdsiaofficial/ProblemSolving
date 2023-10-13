38ae62c1d8d69c29e0585e40e353caa7f5a26443
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-13 09:02:17 +0600

    a

--- CodeForces/104636A_Oath_of_the_nights_watch.cpp
+++ CodeForces/104636A_Oath_of_the_nights_watch.cpp
@@ -16,38 +16,29 @@ int main() {
     cin >> n;
 
     vector<int> strengths(n);
-    vector<int> less(n, 0);  // To store the count of stewards with less strength.
-    vector<int> greater(n, 0);  // To store the count of stewards with greater strength.
-    
-    set<int> lessSet;  // To keep track of stewards with less strength.
-    set<int> greaterSet;  // To keep track of stewards with greater strength.
 
     for (int i = 0; i < n; ++i) {
         cin >> strengths[i];
     }
 
-    // Build the 'less' and 'greater' arrays and sets.
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if (strengths[i] > strengths[j]) {
-                greater[i]++;
-                less[j]++;
-                greaterSet.insert(i);
-                lessSet.insert(j);
-            } else if (strengths[i] < strengths[j]) {
-                less[i]++;
-                greater[j]++;
-                lessSet.insert(i);
-                greaterSet.insert(j);
-            }
-        }
-    }
-
     int count = 0;
 
-    // Count the stewards that meet the criteria.
     for (int i = 0; i < n; ++i) {
-        if (greater[i] > 0 && less[i] > 0) {
+        bool hasLess = false;
+        bool hasGreater = false;
+
+        for (int j = 0; j < n; ++j) {
+            if (i != j) {
+                if (strengths[j] < strengths[i]) {
+                    hasLess = true;
+                }
+                if (strengths[j] > strengths[i]) {
+                    hasGreater = true;
+                }
+            }
+        }
+
+        if (hasLess && hasGreater) {
             count++;
         }
     }
