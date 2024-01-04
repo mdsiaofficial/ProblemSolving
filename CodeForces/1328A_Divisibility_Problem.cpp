c23c4333e00beb48da1d14123d2f5682413b5f34
mdsiaofficial <mdsiaofficial@gmail.com>
2024-01-05 01:00:39 +0600

    solved

--- CodeForces/1328A_Divisibility_Problem.cpp
+++ CodeForces/1328A_Divisibility_Problem.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main()
 {
     int t;
+    cin>>t;
     while(t--){
         int a,b;
         cin>>a>>b;
+
         if(a%b==0) cout<<0<<endl;
         else cout<<b-(a%b)<<endl;
     }
