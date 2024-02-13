676a4b8bfb53904e07892a863729db603f549015
mdsiaofficial <mdsiaofficial@gmail.com>
2024-02-13 22:19:10 +0600

    a

--- CodeForces/Codeforces Round 925 (Div. 3)/1931A_Recovering_a_small_string.cpp
+++ CodeForces/Codeforces Round 925 (Div. 3)/1931A_Recovering_a_small_string.cpp
@@ -14,7 +14,9 @@
 #define fs(n) fixed<<setprecision(int(n))
 #define s(n) setprecision(int(n))
 using namespace std;
-
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+// a b c d e f g h i j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+//
 string smallestEncodedWord(int n) {
     // Convert the encoded number into three letters
     char first = 'a' + (n - 1) / 676; // 676 = 26 * 26
