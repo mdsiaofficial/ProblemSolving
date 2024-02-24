5142500966f152631efed13a98e26a339abb0226
mdsiaofficial <mdsiaofficial@gmail.com>
2024-02-25 01:53:34 +0600

    s

--- CodeWars/8kyu/Multiphy_the_number.cpp
+++ CodeWars/8kyu/Multiphy_the_number.cpp
@@ -1,1 +1,14 @@
 // solved 
+#include <math.h>
+#include <stdio.h>
+int multiply(int number) {
+
+    int count = 0;
+
+// 	  count = (number == 0) ? 1 : log10(number)+1;
+    count = log10(abs(number))+1;
+    int p = pow(5, count);
+    
+    return (number*p);
+
+}
