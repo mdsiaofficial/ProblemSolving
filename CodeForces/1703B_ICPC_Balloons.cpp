c92e9ca7a2620c82fe5716633e43eda7fc75888c
mdsiaofficial <mdsiaofficial@gmail.com>
2024-03-11 23:34:01 +0600

    Ramadan_Night_1_solved

---
+++ CodeForces/1703B_ICPC_Balloons.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define nl endl
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define mod 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+using namespace std;
+
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+// a b c d e f g h i j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+
+
+// solved //
+
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        int balloons = n;
+
+        set<char>unq;
+        for(char c:s){
+            unq.insert(c);
+        }
+        balloons +=unq.size();
+        /* code */
+        cout<<balloons<<nl;
+    }
+    
+
+    return 0;
+}
