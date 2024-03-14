b5c8634648c29eefad2f6334080d6f1774bb2a32
mdsiaofficial <mdsiaofficial@gmail.com>
2024-03-15 00:28:12 +0600

    solved on

--- CodeForces/1703C_Cypher.cpp
+++ CodeForces/1703C_Cypher.cpp
@@ -22,7 +22,58 @@ using namespace std;
 
 // solved //
 
+std::vector<int> decoder(std::vector<int> current, int n, std::vector<std::pair<int, std::string>> move) {
+    std::vector<int> ans;
+    std::vector<int> arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    for (int i = 0; i < n; i++) {
+        int up = 0, down = 0;
+        for (char c : move[i].second) {
+            if (c == 'U') {
+                up++;
+            }
+        }
+        down = move[i].first - up;
+        int role = down - up;
+        int position = (current[i] + role + 10) % 10; // Add 10 to handle negative values
+        ans.push_back(arr[position]);
+    }
+
+    return ans;
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (int _ = 0; _ < t; _++) {
+        int n;
+        std::cin >> n;
+
+        std::vector<int> current(n);
+        for (int i = 0; i < n; i++) {
+            std::cin >> current[i];
+        }
+
+        std::vector<std::pair<int, std::string>> move(n);
+        for (int i = 0; i < n; i++) {
+            int x;
+            std::string y;
+            std::cin >> x >> y;
+            move[i] = {x, y};
+        }
+
+        std::vector<int> result = decoder(current, n, move);
+        for (int x : result) {
+            std::cout << x << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+/* 
 int main() {
     int t;
     cin>>t;
@@ -52,15 +103,26 @@ int main() {
             for (int i = 0; i <ss.length() ; i++)
             {
                 if(ss[i]=='U'){
-                    count--;
+
+                    if(pos[i]==0){
+                        pos[i]=9;
+                    }else{
+                        // count--;
+                        pos[i]--;
+                    }
 
                 }else if(ss[i]=='D'){
-                     count++;
-                }
-                pos[i] +=count;
-            }
+                    
+                    if(pos[i]==9){
+                        pos[i]=0;
+                    }else{
+                        // count++;
+                        pos[i]++;
+                    }
 
-            
+                }
+                
+            }
         }
 
         for(int x: pos){
@@ -73,3 +135,6 @@ int main() {
 
     return 0;
 }
+
+
+ */
