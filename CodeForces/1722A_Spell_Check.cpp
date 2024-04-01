6f4588ec7dde5fa659b945d1c4cdbf141ee01999
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-02 01:05:36 +0600

    day 23 solved problem

---
+++ CodeForces/1722A_Spell_Check.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#include <stdlib.h>
+
+#define nl endl
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define PLL pair<ll,ll>
+#define pii pair<int,int>
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) (x & (1LL << k))
+#define scn(n) scanf("%d",&n)
+#define scnll(n) scanf("%lld",&n)
+#define nll cout<<"\n"
+#define YES cout<<"YES\n"
+#define Yes cout<<"Yes\n"
+#define yes cout<<"yes\n"
+#define NO cout<<"NO\n"
+#define No cout<<"No\n"
+#define no cout<<"no\n"
+#define newl cout<<"\n"
+#define mod 1000000007LL
+#define mod1 1000000007LL
+#define mod2 1000000009LL
+#define inf 1000000000000000LL
+#define N 200000
+#define pi 3.14159
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define mod9 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+
+#define io std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+//  g++ temp.cpp -o temp
+//          or
+//  g++ temp.cpp -o temp.exe
+//  .\temp.exe
+//  ./temp.exe
+
+
+int main(){
+    io;  
+
+    int t;
+    cin>>t;
+    while (t--){
+        int x;
+        string name;
+        cin>>x>>name;
+        string s = "Timur";
+        sort(name.begin(), name.end());
+        sort(s.begin(), s.end());
+        // cout<<s;
+        // nll;
+        // cout<<name;
+        (s==name)?YES:NO;
+    }
+    return 0;
+}
+
+ 
+ 
