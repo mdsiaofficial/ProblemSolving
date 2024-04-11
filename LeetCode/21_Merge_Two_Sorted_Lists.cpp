22008c864f45a29f60f29cdefffd0f0e5bdbf213
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-12 00:06:24 +0600

    solved

--- LeetCode/21_Merge_Two_Sorted_Lists.cpp
+++ LeetCode/21_Merge_Two_Sorted_Lists.cpp
@@ -37,15 +37,39 @@ public:
         if(list1 == NULL) return list2;
         if(list2 == NULL) return list1;
 
-        ListNode*result;
+        ListNode*result = new ListNode(-1);
+        ListNode*Tail = result;
 
-        if(list1.val <= list2.val){
+        if(list1->val <= list2->val){
             result = list1;
-            list1 = list1.next;
+            Tail = list1;
+            list1 = list1->next;
         }else{
             result = list2;
-            list2 = list2.next;
+            Tail = list2;
+            list2 = list2->next;
         }
+
+        while (list1!=NULL && list2!=NULL)
+        {
+            if(list1->val <= list2->val){
+                tail->next = list1;
+                tail = list1;
+                list1 = list1->next;
+            }else{
+                tail->next = list2;
+                tail = list2;
+                list2 = list2->next;
+            }
+
+
+        }
+
+        if(list1 == NULL) tail->next = list2;
+        else tail->next = list1;
+
+        return result->next;
+        
     }
     // using recursion
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -53,10 +77,10 @@ public:
         if(list2 == NULL) return list1;
 
         if(list1->val <= list2->val){
-            list1->next = merge(list1->next, list2);
+            list1->next = mergeTwoLists(list1->next, list2);
             return list1;
         }else{
-            list2->next = merge(list1, list2->next);
+            list2->next = mergeTwoLists(list1, list2->next);
             return list2;
         }
     }
