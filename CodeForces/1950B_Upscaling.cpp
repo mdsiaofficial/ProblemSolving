51dae284c8cdc1b7a9d4f8c1a7c63b05ae7e79f2
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-17 23:04:59 +0600

    solved

---
+++ CodeForces/1950B_Upscaling.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+#define pi 3.14159
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define nl endl
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define mod 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        // if(n==1) return true;
+        unsigned long long temp = n;
+
+        while (temp!=1 && temp!=4)
+        {
+
+            unsigned long long sum = 0;
+
+            while (temp > 0) {
+                sum += (temp % 10) * (temp % 10);
+                temp = (temp / 10);
+                cout << "sum: " << sum << endl;
+            }
+
+            temp = sum;
+
+        }
+        
+        return (temp == 1);
+    }
+
+
+};
+
+void checker(int n) {
+    int line = 2 * n;
+    int hash_count = 0;
+    int dot_count = 0;
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < line; j++) {
+            cout << (i / 2 + j / 2 & 1 ? "." : "#");
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+    Solution s;
+    
+    // int t; cin >> t;
+
+    // while(t--){
+    //     int n;string s;cin>>n>>s;
+    //     vector<char>ch;
+
+    // }
+
+    // cout<<s.isHappy(2);
+
+    int t;cin >> t;
+    while (t--) {
+        int n;cin >> n;
+        checker(n);
+    }
+    return 0;
+} 
---
+++ LeetCode/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+= 0; i < capacity.size(); i++) {
+            if (sum > capacity[i]) size += capacity[i]; c++;
+            else c++;
+        }
