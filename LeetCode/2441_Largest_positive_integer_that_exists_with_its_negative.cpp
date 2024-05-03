dea114c4b685137d9280920376a4de003ed1ab53
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-03 21:55:10 +0600

    leetcode

--- LeetCode/2441_Largest_positive_integer_that_exists_with_its_negative.cpp
+++ LeetCode/2441_Largest_positive_integer_that_exists_with_its_negative.cpp
@@ -19,7 +19,7 @@ public:
         std::sort(nums.begin(), nums.end());
         int n = nums.size();
         for (int i = n-1; i >= 0; i--) {
-            if (nums[i] > 0 && std::binary_search(nums.begin(), nums.end(), -nums[i])) {
+            if (nums[i] > 0 && binary_search(nums.begin(), nums.end(), -nums[i])) {
                 return nums[i];
             }
         }
