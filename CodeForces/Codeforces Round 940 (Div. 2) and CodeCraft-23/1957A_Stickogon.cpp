6bed0305bdbfe57f75e76b75143e9a2c7e536f3d
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-03 08:46:17 +0600

    done

--- CodeForces/Codeforces Round 940 (Div. 2) and CodeCraft-23/1957A_Stickogon.cpp
+++ CodeForces/Codeforces Round 940 (Div. 2) and CodeCraft-23/1957A_Stickogon.cpp
@@ -5,24 +5,24 @@
 // unsolved
 
 int main() {
-    int t;
-    std::cin >> t;
-    while (t--) {
-        int n;
-        std::cin >> n;
-        std::map<int, int> stick_counts;
-        for (int i = 0; i < n; ++i) {
-            int length;
-            std::cin >> length;
-            ++stick_counts[length];
-        }
+	int t;
+	std::cin >> t;
+	while (t--) {
+		int n;
+		std::cin >> n;
+		std::map<int, int> stick_counts;
+		for (int i = 0; i < n; ++i) {
+			int length;
+			std::cin >> length;
+			++stick_counts[length];
+		}
 
-        int polygons = 0;
-        for (auto &x : stick_counts) {
-            polygons += x.second / x.first;
-        }
+		int polygons = 0;
+		for (auto& x : stick_counts) {
+			polygons += x.second / x.first;
+		}
 
-        std::cout << polygons << std::endl;
-    }
-    return 0;
+		std::cout << polygons << std::endl;
+	}
+	return 0;
 }
