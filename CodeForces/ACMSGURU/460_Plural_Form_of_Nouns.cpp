ce2a0e5a847adf7cb7a146355269c1656863554d
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-28 21:13:33 +0600

    cf

---
+++ CodeForces/ACMSGURU/460_Plural_Form_of_Nouns.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define nl endl
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define PLL pair<ll,ll>
+#define pii pair<int,int>
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) (x & (1LL << k))
+#define scn(n) scanf("%d",&n)
+#define scnll(n) scanf("%lld",&n)
+#define nll cout<<"\n"
+#define YES cout<<"YES\n"
+#define Yes cout<<"Yes\n"
+#define yes cout<<"yes\n"
+#define NO cout<<"NO\n"
+#define No cout<<"No\n"
+#define no cout<<"no\n"
+#define newl cout<<"\n"
+#define mod 1000000007LL
+#define mod1 1000000007LL
+#define mod2 1000000009LL
+#define inf 1000000000000000LL
+#define N 200000
+#define pi 3.14159
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+#define mod9 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+
+#define io std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+//  g++ temp.cpp -o temp
+//          or
+//  g++ temp.cpp -o temp.exe
+//  .\temp.exe
+//  ./temp.exe
+
+int main()
+{
+    
+
+  // io;
+  int t;cin >> t;
+  while (t--) {
+    string s;cin >> s;
+    int n = s.size();
+    // int n = s.length();
+    // cout << n;
+    if (s[n - 1] == 'o' || s[n - 1] == 's' || s[n - 1] == 'x' || (s[n - 2] == 'c' && s[n - 1] == 'h')) {
+      s += "es";
+    }
+    else if (s[n - 1] == 'f' || (s[n - 2] == 'f' && s[n - 1] == 'e')) {
+      (s[n - 2] == 'f' && s[n - 1] == 'e') ? s = s.substr(0, n - 2) : s = s.substr(0, n - 1);
+      s += "ves";
+    }
+    else if (s[n - 1] == 'y') {
+      s = s.substr(0, n - 1);
+      s += "ies";
+    }
+    else {
+      s += "s";
+    }
+    cout << s << endl;
+  }
+
+	return 0;
+}
