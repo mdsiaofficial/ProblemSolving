b0e14d10a0b9b269ec107edd8ef676b29317412c
mdsiaofficial <mdsiaofficial@gmail.com>
2024-10-02 11:39:59 +0600

    "Modified Fibonacci_array_1176.cpp to use recursive function and updated main function; removed while loop and added base case for recursion"

--- Beecrowd/Beginner/Fibonacci_array_1176.cpp
+++ Beecrowd/Beginner/Fibonacci_array_1176.cpp
@@ -18,28 +18,33 @@ void fibo(ull n){
 }
 
 void fibo_Recursion(ull n){
+    if(n <= 1) return;
     ull f=0;
     ull s=1;
     fibos.push_back(f);
     fibos.push_back(s);
-    while (n--)
-    {
-        ull nt=f+s;
-        fibos.push_back(nt);
-        f=s;
-        s=nt;
-    }
+    fibo_Recursion(n-2);
+    ull nt=f+s;
+    fibos.push_back(nt);
+    f=s;
+    s=nt;
 }
+
 int main()
 {
+    // solved
     unsigned long long t;
     cin>>t;
     while (t--)
     {
+        fibos.clear();
         ull n;
         cin>>n;
         fibo(n);
-        cout<<fibos[n]<<endl;
+        // for(int a:fibos){
+        //     printf("%d ", a);
+        // }
+        cout<< "Fib("<<n<<") = "<<fibos[n]<<endl;
 
     }
     return 0;
