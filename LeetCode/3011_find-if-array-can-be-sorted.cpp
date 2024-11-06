a7f4159f03c09b84c63d6639242976d169a9121f
mdsiaofficial <mdsiaofficial@gmail.com>
2024-11-06 12:07:51 +0600

    Removed unnecessary comments and code lines in the `canSortArray` function.

--- LeetCode/3011_find-if-array-can-be-sorted.cpp
+++ LeetCode/3011_find-if-array-can-be-sorted.cpp
@@ -75,18 +75,13 @@ public:
   bool canSortArray(vector<int> &nums)
   {
     int n = nums.size();
-
-    // Avoid modifying the input directly
-    // Create a copy of the input array
     vector<int> values = nums;
-
     for (int i = 0; i < n; i++)
     {
       for (int j = 0; j < n - i - 1; j++)
       {
         if (values[j] <= values[j + 1])
         {
-          // No swap needed
           continue;
         }
         else
@@ -94,7 +89,6 @@ public:
           if (__builtin_popcount(nums[j]) ==
               __builtin_popcount(nums[j + 1]))
           {
-            // Swap the elements
             swap(values[j], values[j + 1]);
           }
           else
